c4b072183733c8a94ebf585889a84c77db4a4173
Angel Stoyanov <atstojanov@gmail.com>
2020-09-03 16:18:22 +0300

    Extend connection string configuration with: MYSQL_DB_PORT and ENABLE_ORM_LOGGING

--- src/connection-factory.ts
+++ src/connection-factory.ts
@@ -1,25 +1,27 @@
 import { createConnection } from 'typeorm';
 
 export class ConnectionFactory {
-  public GetConnection() {
-    return createConnection(
-      {
-        "type": "mysql",
-        "host": process.env.DATABASE_HOST,
-        "port": 3306,
-        "username": "root",
-        "password": process.env.MYSQL_ROOT_PASSWORD,
-        "database": process.env.MYSQL_DATABASE,
-        "insecureAuth": true,
-        "synchronize": true,
-        // "logging": true,
-        "entities": [
-          "./src/models/index.ts"
-        ],
-        "migrations": [
-          "src/migrations/**/*.ts"
-        ]
-      }
-    );
-  }
+    public GetConnection() {
+
+        const dbPort = process.env.MYSQL_DB_PORT ? Number(process.env.MYSQL_DB_PORT) : 3306;
+        return createConnection(
+            {
+                "type": "mysql",
+                "host": process.env.DATABASE_HOST,
+                "port": dbPort,
+                "username": "root",
+                "password": process.env.MYSQL_ROOT_PASSWORD,
+                "database": process.env.MYSQL_DATABASE,
+                "insecureAuth": true,
+                "synchronize": true,
+                "logging": process.env.ENABLE_ORM_LOGGING === 'true',
+                "entities": [
+                    "./src/models/index.ts"
+                ],
+                "migrations": [
+                    "src/migrations/**/*.ts"
+                ]
+            }
+        );
+    }
 }
