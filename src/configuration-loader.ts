96b52038f044d1d01e1af0a48dbf3273844e4cc9
Angel Stoyanov <atstojanov@gmail.com>
2020-09-04 12:56:55 +0300

    Rename .env.sample to .env.default

--- src/configuration-loader.ts
+++ src/configuration-loader.ts
@@ -4,7 +4,7 @@ import { ConfigurationValidator } from './validators/configuration';
 export function LoadConfiguration() {
     
     config();
-    config({ path: '.env.sample' });
+    config({ path: '.env.default' });
 
     const configurationValidator = new ConfigurationValidator();
     configurationValidator.validate();
