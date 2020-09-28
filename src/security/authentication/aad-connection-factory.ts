222c0454b9ab701325deaac8e85f78c114ff0746
Valentin <valentin.yanakiev@gmail.com>
2020-09-28 16:30:35 +0300

    Fixed auth env variable loading + added handling of missing env variables.

--- src/security/authentication/aad-connection-factory.ts
+++ src/security/authentication/aad-connection-factory.ts
@@ -1,21 +1,29 @@
 import { IBearerStrategyOptionWithRequest } from 'passport-azure-ad';
-import { defaultConfig } from './config'
+import { LoadConfiguration } from '../../configuration-loader';
 
 export class AADConnectionFactory {
     public static GetOptions(): IBearerStrategyOptionWithRequest {
 
-        const config : IBearerStrategyOptionWithRequest = process.env.AAD_CLIENT ? {
-          identityMetadata: 'https://login.microsoftonline.com/' + process.env.AAD_TENANT + '/v2.0/.well-known/openid-configuration',
-          clientID: process.env.AAD_CLIENT as string,
+        LoadConfiguration();
+
+        if(!process.env.AAD_CLIENT)
+          throw new Error('No AAD_CLIENT environment variable set!');
+
+        if(!process.env.AAD_TENANT)
+          throw new Error('No AAD_TENANT environment variable set!');
+
+        const config : IBearerStrategyOptionWithRequest = {
+          identityMetadata: `https://login.microsoftonline.com/${process.env.AAD_TENANT}/v2.0/.well-known/openid-configuration`,
+          clientID: process.env.AAD_CLIENT || '',
           validateIssuer: true,
           passReqToCallback: true,
-          issuer: 'https://login.microsoftonline.com/' + process.env.AAD_TENANT + '/v2.0',
-          audience: process.env.AAD_CLIENT,
+          issuer: `https://login.microsoftonline.com/${process.env.AAD_TENANT}/v2.0`,
+          audience: process.env.AAD_CLIENT || '',
           allowMultiAudiencesInToken: false,
           loggingLevel: 'info',
           scope: ['Cherrytwist-GraphQL'],
           loggingNoPII: false
-        } : defaultConfig;
+        };
 
         return config;
     }
