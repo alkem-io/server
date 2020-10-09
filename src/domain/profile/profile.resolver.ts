94490ec15a2ad1a1cd262d66286933f437264b3c
Neil Smyth <neil@thesmyths.eu>
2020-10-09 11:55:18 +0200

    Added mutations related to tagsets

--- src/domain/profile/profile.resolver.ts
+++ src/domain/profile/profile.resolver.ts
@@ -1,4 +1,24 @@
+import { Mutation, Args } from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
+import { Tagset } from '../tagset/tagset.entity';
+import { ProfileService } from './profile.service';
 
 @Resolver()
-export class ProfileResolver {}
+export class ProfileResolver {
+  constructor(private profileService: ProfileService) {}
+
+  @Mutation(() => Tagset, {
+    description:
+      'Creates a new tagset with the specified name for the profile with given id',
+  })
+  async createTagsetOnProfile(
+    @Args('profileID') profileID: number,
+    @Args('tagsetName') tagsetName: string
+  ): Promise<Tagset> {
+    const tagset = await this.profileService.createTagset(
+      profileID,
+      tagsetName
+    );
+    return tagset as Tagset;
+  }
+}
--- src/domain/tagset/tagset.resolver.ts
+++ src/domain/tagset/tagset.resolver.ts
@@ -1,6 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
-
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { TagsInput } from './tagset.dto';
+import { Tagset } from './tagset.entity';
+import { TagsetService } from './tagset.service';
 
 @Resolver()
 export class TagsetResolver {
+  constructor(private tagsetService: TagsetService) {}
+
+  @Mutation(() => Tagset, {
+    description: 'Replace the set of tags in a tagset with the provided tags',
+  })
+  async replaceTagsOnTagset(
+    @Args('tagsetID') tagsetID: number,
+    @Args('tags') newTags: TagsInput
+  ): Promise<Tagset> {
+    if (!newTags.tags)
+      throw new Error(`Unable to replace tags on tagset(${tagsetID}`);
+
+    const tagset = await this.tagsetService.replaceTags(tagsetID, newTags.tags);
+
+    return tagset as Tagset;
+  }
 }
