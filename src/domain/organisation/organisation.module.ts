f4414ea060da8ad7f7b38e79133c4b7a952d5dd4
Neil Smyth <neil@thesmyths.eu>
2020-10-13 15:18:45 +0200

    fixed module imports

--- src/domain/organisation/organisation.module.ts
+++ src/domain/organisation/organisation.module.ts
@@ -3,9 +3,15 @@ import { UserGroupModule } from '../user-group/user-group.module';
 import { OrganisationService } from './organisation.service';
 import { OrganisationResolver } from './organisation.resolver';
 import { TagsetModule } from '../tagset/tagset.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Organisation } from './organisation.entity';
 
 @Module({
-  imports: [UserGroupModule, TagsetModule],
+  imports: [
+    UserGroupModule,
+    TagsetModule,
+    TypeOrmModule.forFeature([Organisation]),
+  ],
   providers: [OrganisationService, OrganisationResolver],
   exports: [OrganisationService],
 })
