fce0c767b31954a2b7307006576b999ce4e74828
Valentin <valentin.yanakiev@gmail.com>
2020-10-19 19:38:13 +0300

    Re-integrate AUTHENTICATION_ENABLED flag.

--- src/utils/authentication/graphql.guard.ts
+++ src/utils/authentication/graphql.guard.ts
@@ -3,9 +3,14 @@ import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 import { AuthenticationError } from 'apollo-server-core';
+import { ConfigService } from '@nestjs/config';
+import { IServiceConfig } from 'src/interfaces/service.config.interface';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('azure-ad') {
+  constructor(private configService: ConfigService) {
+    super();
+  }
   canActivate(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     const { req } = ctx.getContext();
@@ -15,6 +20,11 @@ export class GqlAuthGuard extends AuthGuard('azure-ad') {
   }
 
   handleRequest(err: any, user: any) {
+    if (
+      this.configService.get<IServiceConfig>('service')
+        ?.authenticationEnabled === 'false'
+    )
+      return true;
     if (err || !user) {
       throw err || new AuthenticationError('GqlAuthGuard');
     }
