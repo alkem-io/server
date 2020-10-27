dd008df35ce249e68c190e05521cbd51097c7196
Valentin <valentin.yanakiev@gmail.com>
2020-10-27 11:31:23 +0200

    Removed commented code.

--- test/utils/graphql.request.ts
+++ test/utils/graphql.request.ts
@@ -16,39 +16,3 @@ export const graphqlRequest = async (
     .send({ ...requestParams })
     .set('Accept', 'application/json');
 };
-
-// declare global {
-//   // eslint-disable-next-line @typescript-eslint/no-namespace
-//   namespace jest {
-//     interface Matchers<R> {
-//       toContainObject(argument: any): R;
-//     }
-//   }
-// }
-
-// expect.extend({
-//   toContainObject(received, argument) {
-//     const pass = this.equals(
-//       received,
-//       expect.arrayContaining([expect.objectContaining(argument)])
-//     );
-
-//     if (pass) {
-//       return {
-//         message: () =>
-//           `expected ${this.utils.printReceived(
-//             received
-//           )} not to contain object ${this.utils.printExpected(argument)}`,
-//         pass: true,
-//       };
-//     } else {
-//       return {
-//         message: () =>
-//           `expected ${this.utils.printReceived(
-//             received
-//           )} to contain object ${this.utils.printExpected(argument)}`,
-//         pass: false,
-//       };
-//     }
-//   },
-// });
