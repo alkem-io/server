f49c0fcc050b41c7a0a0a33fe549bd316ea889dc
Neil Smyth <neil@thesmyths.eu>
2020-11-05 16:19:39 +0100

    remove created user if account creation fails

--- src/domain/ecoverse/ecoverse.service.ts
+++ src/domain/ecoverse/ecoverse.service.ts
@@ -309,7 +309,16 @@ export class EcoverseService {
     // Ok to proceed to creating profile and optionally account
     const user = await this.createUserProfile(userData);
     if (this.accountService.authenticationEnabled()) {
-      await this.accountService.createUserAccount(userData);
+      try {
+        const result = await this.accountService.createUserAccount(userData);
+        if (!result) throw new Error('unable to create account for user');
+      } catch (e) {
+        // Account creation failed; need to remove the user
+        await this.userService.removeUser(user);
+        throw new Error(
+          `unable to create account for user; removing created user profile: ${user.name}`
+        );
+      }
     }
     return user;
   }
