ded63b7038f64059ef90238d9f01e86fdebeabfd
Valentin <valentin.yanakiev@gmail.com>
2020-11-26 11:06:18 +0200

    Exception filters WIP

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './exceptions.filter';
 import { IServiceConfig } from './interfaces/service.config.interface';
 import { BootstrapService } from './utils/bootstrap/bootstrap.service';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new AllExceptionsFilter());
   const bootstrapService: BootstrapService = app.get(BootstrapService);
   await bootstrapService.bootstrapEcoverse();
   const configService: ConfigService = app.get(ConfigService);
