24675d4ad17627d1625964c4691506c7522930be
Evgeni Dimitrov <comoque@gmail.com>
2020-12-03 16:32:11 +0200

    Added anonymous tests for: - context - user/s - groups - challenge/s

--- test/auth/anonymous.e2e-spec.ts
+++ test/auth/anonymous.e2e-spec.ts
@@ -1,11 +1,61 @@
 import { graphqlRequestAuth } from '../utils/graphql.request';
 import '../utils/array.matcher';
 import { appSingleton } from '../utils/app.singleton';
-import { name, hostMembers, hostGroups, hostProfile } from '../utils/queries';
+import {
+  name,
+  hostMembers,
+  hostGroups,
+  hostProfile,
+  contextTagline,
+  contextBackground,
+  contextVision,
+  contextImpact,
+  contextWho,
+  contextReferencesId,
+  usersId,
+  usersAccountUPN,
+  usersProfileId,
+  usersMemberofGroupsId,
+  usersMemberofChallengesId,
+  usersMemberofOrganisationsId,
+  userId,
+  userAccountUPN,
+  userProfileId,
+  userMemberofGroupsId,
+  userMemberofChallengesId,
+  userMemberofOrganisationsId,
+  groupsId,
+  groupsFocalPointId,
+  groupsProfileId,
+  groupsMembersId,
+  groupsParentChallenge,
+  groupsParentEcoverse,
+  groupsParentOpportunity,
+  challengesId,
+  challengesTextId,
+  challengesState,
+  challengesContexId,
+  challengesLeadOrganisation,
+  challengesLeadOrganisationGroups,
+  challengesContributors,
+  challengesTagsets,
+  challengesGroups,
+  challengesOpportunities,
+  challengeId,
+  challengeTextId,
+  challengeState,
+  challengeContexId,
+  challengeLeadOrganisation,
+  challengeLeadOrganisationGroups,
+  challengeTagsets,
+  challengeGroups,
+  challengeContributors,
+  challengeOpportunities,
+} from '../utils/queries';
 
 let profileId = '6';
 
-const notAuthorizedMessage = `"message":"You are not authorized to access this resource. "`;
+const notAuthorizedCode = `"code":"UNAUTHENTICATED"`;
 
 beforeAll(async () => {
   if (!appSingleton.Instance.app) await appSingleton.Instance.initServer();
@@ -18,11 +68,56 @@ afterAll(async () => {
 describe('DDT anonymous user - queries', () => {
   // Arrange
   test.each`
-    query          | expected
-    ${name}        | ${`{"data":{"name":"Cherrytwist"}}`}
-    ${hostGroups}  | ${notAuthorizedMessage}
-    ${hostMembers} | ${notAuthorizedMessage}
-    ${hostProfile} | ${`{"data":{"host":{"profile":{"id":"${profileId}"}}}}`}
+    query                               | expected
+    ${name}                             | ${`{"data":{"name":"Cherrytwist"}}`}
+    ${hostGroups}                       | ${notAuthorizedCode}
+    ${hostMembers}                      | ${notAuthorizedCode}
+    ${hostProfile}                      | ${`{"data":{"host":{"profile":{"id":"${profileId}"}}}}`}
+    ${contextTagline}                   | ${`{"data":{"context":{"tagline":"Open`}
+    ${contextBackground}                | ${`{"data":{"context":{"background":"The`}
+    ${contextVision}                    | ${`{"data":{"context":{"vision":"The`}
+    ${contextWho}                       | ${`{"data":{"context":{"who":"Everyone."}}}`}
+    ${contextImpact}                    | ${`{"data":{"context":{"impact":"A`}
+    ${contextReferencesId}              | ${`{"data":{"context":{"references":[{"id":"2"}`}
+    ${usersId}                          | ${notAuthorizedCode}
+    ${usersAccountUPN}                  | ${notAuthorizedCode}
+    ${usersProfileId}                   | ${notAuthorizedCode}
+    ${usersMemberofGroupsId}            | ${notAuthorizedCode}
+    ${usersMemberofChallengesId}        | ${notAuthorizedCode}
+    ${usersMemberofOrganisationsId}     | ${notAuthorizedCode}
+    ${userId}                           | ${notAuthorizedCode}
+    ${userAccountUPN}                   | ${notAuthorizedCode}
+    ${userProfileId}                    | ${notAuthorizedCode}
+    ${userMemberofGroupsId}             | ${notAuthorizedCode}
+    ${userMemberofChallengesId}         | ${notAuthorizedCode}
+    ${userMemberofOrganisationsId}      | ${notAuthorizedCode}
+    ${groupsId}                         | ${notAuthorizedCode}
+    ${groupsFocalPointId}               | ${notAuthorizedCode}
+    ${groupsProfileId}                  | ${notAuthorizedCode}
+    ${groupsMembersId}                  | ${notAuthorizedCode}
+    ${groupsParentChallenge}            | ${notAuthorizedCode}
+    ${groupsParentEcoverse}             | ${notAuthorizedCode}
+    ${groupsParentOpportunity}          | ${notAuthorizedCode}
+    ${challengesId}                     | ${`{"data":{"challenges":[{"id":"1"}`}
+    ${challengesTextId}                 | ${`{"data":{"challenges":[{"textID":"balance`}
+    ${challengesState}                  | ${`{"data":{"challenges":[{"state":"Defined`}
+    ${challengesContexId}               | ${`{"data":{"challenges":[{"context":{"id":"2"}`}
+    ${challengesLeadOrganisation}       | ${`{"data":{"challenges":[{"leadOrganisations":[{"id":"1"}`}
+    ${challengesLeadOrganisationGroups} | ${notAuthorizedCode}
+    ${challengesContributors}           | ${notAuthorizedCode}
+    ${challengesTagsets}                | ${`{"data":{"challenges":[{"tagset":{"id":"2`}
+    ${challengesGroups}                 | ${notAuthorizedCode}
+    ${challengesOpportunities}          | ${`{"data":{"challenges":[{"opportunities":[{"id":"2"}`}
+    ${challengeId}                     | ${`{"data":{"challenge":{"id":"1"}`}
+    ${challengeTextId}                 | ${`{"data":{"challenge":{"textID":"balance`}
+    ${challengeState}                  | ${`{"data":{"challenge":{"state":"Defined`}
+    ${challengeContexId}               | ${`{"data":{"challenge":{"context":{"id":"2"}`}
+    ${challengeLeadOrganisation}       | ${`{"data":{"challenge":{"leadOrganisations":[{"id":"1"}`}
+    ${challengeLeadOrganisationGroups} | ${notAuthorizedCode}
+    ${challengeContributors}           | ${notAuthorizedCode}
+    ${challengeTagsets}                | ${`{"data":{"challenge":{"tagset":{"id":"2`}
+    ${challengeGroups}                 | ${notAuthorizedCode}
+    ${challengeOpportunities}          | ${`{"data":{"challenge":{"opportunities":[{"id":"2"}`}
   `(
     "should expect: '$expected' for query: '$query'",
     async ({ query, expected }) => {
--- test/utils/queries.ts
+++ test/utils/queries.ts
@@ -1,21 +1,151 @@
-const name = `{
-                name
-               }`;
-//export default name;
+const name = `{name}`;
 
-const hostMembers = `{
-                        host {members{id}}
-                      }`;
+const hostMembers = `{host {members{id}}}`;
 
-const hostGroups = `{
-                        host {members{id}}
-                    }`;
+const hostGroups = `{host {members{id}}}`;
 
-const hostProfile = `{
-                        host{profile{id}}
-                      }`;
+const hostProfile = `{host{profile{id}}}`;
 
-const contextTagline = `{
-                        context{tagline{id}}
-                      }`;
-export { name, hostMembers, hostGroups, hostProfile, contextTagline };
+const contextTagline = `{context{tagline}}`;
+
+const contextBackground = `{context{background}}`;
+
+const contextVision = `{context{vision}}`;
+
+const contextImpact = `{context{impact}}`;
+
+const contextWho = `{context{who}}`;
+
+const contextReferencesId = `{context{references{id}}}`;
+
+const usersId = `{users{id}}`;
+
+const usersAccountUPN = `{users{accountUpn}}`;
+
+const usersProfileId = `{users{profile{id}}}`;
+
+const usersMemberofGroupsId = `{users{memberof{groups{id}}}}`;
+
+const usersMemberofChallengesId = `{users{memberof{challenges{id}}}}`;
+
+const usersMemberofOrganisationsId = `{users{memberof{organisations{id}}}}`;
+
+const userId = `{users{id}}`;
+
+const userAccountUPN = `{user(ID: "1"){accountUpn}}`;
+
+const userProfileId = `{user(ID: "1"){profile{id}}}`;
+
+const userMemberofGroupsId = `{user(ID: "1"){memberof{groups{id}}}}`;
+
+const userMemberofChallengesId = `{user(ID: "1"){memberof{challenges{id}}}}`;
+
+const userMemberofOrganisationsId = `{user(ID: "1"){memberof{organisations{id}}}}`;
+
+const groupsId = `{groups{id}}`;
+
+const groupsFocalPointId = `{groups{focalPoint{id}}}`;
+
+const groupsMembersId = `{groups{members{id}}}`;
+
+const groupsProfileId = `{groups{profile{id}}}`;
+
+const groupsParentChallenge = `{groups{parent{__typename ... on Challenge {id}}}}`;
+
+const groupsParentOpportunity = `{groups{parent{__typename ... on Opportunity {id}}}}`;
+
+const groupsParentEcoverse = `{groups{parent{__typename ... on Challenge {id}}}}`;
+
+const challengesId = `{ challenges { id }}`;
+
+const challengesTextId = `{ challenges { textID }}`;
+
+const challengesState = `{ challenges { state }}`;
+
+const challengesContexId = `{ challenges { context { id }}}`;
+
+const challengesLeadOrganisation = `{ challenges { leadOrganisations { id }}}`;
+
+const challengesLeadOrganisationGroups = `{ challenges { leadOrganisations { id groups { id }}}}`;
+
+const challengesTagsets = `{ challenges { tagset { id }}}`;
+
+const challengesGroups = `{ challenges { groups { id }}}`;
+
+const challengesContributors = `{ challenges { contributors { id }}}`;
+
+const challengesOpportunities = `{ challenges { opportunities { id }}}`;
+
+const challengeId = `{ challenge(ID: 1) { id }}`;
+
+const challengeTextId = `{ challenge(ID: 1) { textID }}`;
+
+const challengeState = `{ challenge(ID: 1) { state }}`;
+
+const challengeContexId = `{ challenge(ID: 1) { context { id }}}`;
+
+const challengeLeadOrganisation = `{ challenge(ID: 1) { leadOrganisations { id }}}`;
+
+const challengeLeadOrganisationGroups = `{ challenge(ID: 1) { leadOrganisations { id groups { id }}}}`;
+
+const challengeTagsets = `{ challenge(ID: 1) { tagset { id }}}`;
+
+const challengeGroups = `{ challenge(ID: 1) { groups { id }}}`;
+
+const challengeContributors = `{ challenge(ID: 1) { contributors { id }}}`;
+
+const challengeOpportunities = `{ challenge(ID: 1) { opportunities { id }}}`;
+
+const challenges = `{challenges{}}`;
+
+export {
+  name,
+  hostMembers,
+  hostGroups,
+  hostProfile,
+  contextTagline,
+  contextBackground,
+  contextVision,
+  contextImpact,
+  contextWho,
+  contextReferencesId,
+  usersId,
+  usersAccountUPN,
+  usersProfileId,
+  usersMemberofGroupsId,
+  usersMemberofChallengesId,
+  usersMemberofOrganisationsId,
+  userId,
+  userAccountUPN,
+  userProfileId,
+  userMemberofGroupsId,
+  userMemberofChallengesId,
+  userMemberofOrganisationsId,
+  groupsId,
+  groupsFocalPointId,
+  groupsProfileId,
+  groupsMembersId,
+  groupsParentChallenge,
+  groupsParentEcoverse,
+  groupsParentOpportunity,
+  challengesId,
+  challengesTextId,
+  challengesState,
+  challengesContexId,
+  challengesLeadOrganisation,
+  challengesLeadOrganisationGroups,
+  challengesContributors,
+  challengesTagsets,
+  challengesGroups,
+  challengesOpportunities,
+  challengeId,
+  challengeTextId,
+  challengeState,
+  challengeContexId,
+  challengeLeadOrganisation,
+  challengeLeadOrganisationGroups,
+  challengeTagsets,
+  challengeGroups,
+  challengeContributors,
+  challengeOpportunities,
+};
