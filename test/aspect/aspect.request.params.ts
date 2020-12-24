2f3c0f6897d9412c80746a45452f44096f187899
Evgeni Dimitrov <comoque@gmail.com>
2020-12-24 02:53:07 +0200

    Added aspect e2e tests

---
+++ test/aspect/aspect.request.params.ts
@@ -0,0 +1,126 @@
+import { TestUser } from '../utils/token.helper';
+import { graphqlRequestAuth } from '../utils/graphql.request';
+
+export const createAspectOnProjectMutation = async (
+  projectId: any,
+  aspectTitle: any,
+  aspectFraming?: any,
+  aspectExplenation?: any
+) => {
+  const requestParams = {
+    operationName: null,
+    query: `mutation CreateAspectOnProject($aspectData: AspectInput!, $projectID: Float!) {
+      createAspectOnProject(aspectData: $aspectData, projectID: $projectID) {
+        id,
+        title,
+        framing,
+        explanation
+      }
+    }`,
+    variables: {
+      projectID: parseFloat(projectId),
+      aspectData: {
+        title: `${aspectTitle}`,
+        framing: `${aspectFraming}`,
+        explanation: `${aspectExplenation}`,
+      },
+    },
+  };
+
+  return await graphqlRequestAuth(requestParams, TestUser.GLOBAL_ADMIN);
+};
+
+export const createAspectOnOpportunityMutation = async (
+  opportunityId: any,
+  aspectTitle: any,
+  aspectFraming?: any,
+  aspectExplenation?: any
+) => {
+  const requestParams = {
+    operationName: null,
+    query: `mutation CreateAspect($aspectData: AspectInput!, $opportunityID: Float!) {
+      createAspect(aspectData: $aspectData, opportunityID: $opportunityID) {
+        id,
+        title,
+        framing,
+        explanation
+      }
+    }`,
+    variables: {
+      opportunityID: parseFloat(opportunityId),
+      aspectData: {
+        title: `${aspectTitle}`,
+        framing: `${aspectFraming}`,
+        explanation: `${aspectExplenation}`,
+      },
+    },
+  };
+
+  return await graphqlRequestAuth(requestParams, TestUser.GLOBAL_ADMIN);
+};
+
+export const updateAspectMutation = async (
+  aspectId: any,
+  aspectTitle: any,
+  aspectFraming?: any,
+  aspectExplenation?: any
+) => {
+  const requestParams = {
+    operationName: null,
+    query: `mutation updateAspect($aspectData: AspectInput!, $ID: Float!) {
+      updateAspect(aspectData: $aspectData, ID: $ID) {
+        id, title, framing, explanation
+      }
+    }`,
+    variables: {
+      ID: parseFloat(aspectId),
+      aspectData: {
+        title: `${aspectTitle}`,
+        framing: `${aspectFraming}`,
+        explanation: `${aspectExplenation}`,
+      },
+    },
+  };
+
+  return await graphqlRequestAuth(requestParams, TestUser.GLOBAL_ADMIN);
+};
+
+export const removeAspectMutation = async (aspectId: any) => {
+  const requestParams = {
+    operationName: null,
+    query: `mutation removeAspect($ID: Float!) {
+      removeAspect(ID: $ID)
+    }`,
+    variables: {
+      ID: parseFloat(aspectId),
+    },
+  };
+
+  return await graphqlRequestAuth(requestParams, TestUser.GLOBAL_ADMIN);
+};
+
+export const getAspectPerOpportunity = async (opportunityId: any) => {
+  const requestParams = {
+    operationName: null,
+    query: `query {opportunity(ID: ${parseFloat(opportunityId)}) {
+        aspects { id title framing explanation }}}`,
+  };
+
+  return await graphqlRequestAuth(requestParams, TestUser.GLOBAL_ADMIN);
+};
+
+export const getAspectPerProject = async (opportunityId: any) => {
+  const requestParams = {
+    operationName: null,
+    query: `query {opportunity(ID: ${parseFloat(opportunityId)}) {
+      projects{
+        aspects{
+          id title framing explanation
+        }
+      }
+    }
+  }`,
+  };
+
+  return await graphqlRequestAuth(requestParams, TestUser.GLOBAL_ADMIN);
+};
---
+++ test/project/project.request.params.ts
@@ -0,0 +1,34 @@
+import { TestUser } from '../utils/token.helper';
+import { graphqlRequestAuth } from '../utils/graphql.request';
+
+export const createProjectMutation = async (
+  opportunityId: any,
+  projectName: any,
+  textId: any,
+  projectDescritpion?: any,
+  projectState?: any
+) => {
+  const requestParams = {
+    operationName: null,
+    query: `mutation CreateProject($projectData: ProjectInput!, $opportunityID: Float!) {
+      createProject(projectData: $projectData, opportunityID: $opportunityID) {
+          id,
+        name,
+        textID,
+        description,
+        state
+      }
+    }`,
+    variables: {
+      opportunityID: parseFloat(opportunityId),
+      projectData: {
+        name: `${projectName}`,
+        textID: `${textId}`,
+        description: `${projectDescritpion}`,
+        state: `${projectState}`,
+      },
+    },
+  };
+
+  return await graphqlRequestAuth(requestParams, TestUser.GLOBAL_ADMIN);
+};
