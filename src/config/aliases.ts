199c514dff7b2b907c537ea6bbd4f8a670071d8a
Angel <atstojanov@gmail.com>
2020-12-29 02:26:22 +0200

    Aliases for interfaces, domain, config, utils and all

---
+++ src/config/aliases.ts
@@ -0,0 +1,11 @@
+import moduleAlias from 'module-alias';
+import path from 'path';
+
+const rootPath = path.resolve(__dirname, '..', '..', 'dist');
+moduleAlias.addAliases({
+  '@intefaces': path.join(rootPath, 'interfaces'),
+  '@domain': path.join(rootPath, 'domain'),
+  '@config': path.join(rootPath, 'config'),
+  '@utils': path.join(rootPath, 'utils'),
+  '@src': rootPath,
+});
