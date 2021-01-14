9f09b4862d4e608ce85cf930044438567982496c
Neil Smyth <neil@thesmyths.eu>
2021-01-14 10:00:34 +0100

    added user field resolver to module

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -4,10 +4,11 @@ import { UserResolver } from './user.resolver';
 import { ProfileModule } from '@domain/profile/profile.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { UserResolverFields } from './user.resolver.fields';
 
 @Module({
   imports: [ProfileModule, TypeOrmModule.forFeature([User])],
-  providers: [UserService, UserResolver],
+  providers: [UserService, UserResolver, UserResolverFields],
   exports: [UserService],
 })
 export class UserModule {}
