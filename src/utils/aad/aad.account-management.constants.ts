ccce2a9f05949ea416259c7af1ca183ba955375f
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-02-05 19:01:53 +0200

    Fixed nest AadAccountManagement tokens.

--- src/utils/aad/aad.account-management.constants.ts
+++ src/utils/aad/aad.account-management.constants.ts
@@ -1,4 +1,7 @@
-export const AAD_ACCOUNT_MANAGEMENT_MODULE_OPTIONS = 'aadModuleOptions';
-export const AAD_ACCOUNT_MANAGEMENT_MODULE_PROVIDER = 'aadModule';
+export const AAD_ACCOUNT_MANAGEMENT_MODULE_OPTIONS =
+  'aadAccountManagementModuleOptions';
+export const AAD_ACCOUNT_MANAGEMENT_MODULE_PROVIDER =
+  'aadAccountManagementModule';
 export const AAD_GRAPH_MODULE_PROVIDER = 'aadGraphModule';
-export const AAD_ACCOUNT_MANAGEMENT_MODULE_NEST_PROVIDER = 'nestAadModule';
+export const AAD_ACCOUNT_MANAGEMENT_MODULE_NEST_PROVIDER =
+  'nestAadAccountManagementModule';
