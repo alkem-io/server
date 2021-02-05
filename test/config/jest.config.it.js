8e182f2c673f10647b60bd82db58f4e66ece455a
Evgeni Dimitrov <comoque@gmail.com>
2021-02-05 12:31:31 +0200

    Fixed jest.config.it

--- test/config/jest.config.it.js
+++ test/config/jest.config.it.js
@@ -1,5 +1,5 @@
 module.exports = {
   ...require('./jest.config'),
-  testMatch: ['**/?(*.)+(test-update.it-spec).ts'],
+  testMatch: ['**/?(*.)+(it-spec).ts'],
   coverageDirectory: '<rootDir>/coverage-it',
 };
