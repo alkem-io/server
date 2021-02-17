4bc98f26159c33a7fa143c2836cb7b01feaf7546
Angel Stoyanov <atstojanov@gmail.com>
2021-02-17 15:25:41 +0200

    More PR comments resolved 2

--- src/utils/config/template/template.entity.ts
+++ src/utils/config/template/template.entity.ts
@@ -1,12 +1,11 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { ChallengeTemplate } from '@utils/config/template/challenge.template.entity';
 import { EcoverseTemplate } from '@utils/config/template/ecoverse.template.entity';
 import { OpportunityTemplate } from './opportunity.template.entity';
 import { IOpportunityTemplate } from './opportunity.template.interface';
 import { ITemplate } from './template.interface';
 import { UserTemplate } from './user.template.entity';
 import { IUserTemplate } from './user.template.interface';
-import { ChallengeTemplate } from '@utils/config/template/challenge.template.entity';
-import { Ecoverse } from '@domain/ecoverse/ecoverse.entity';
 
 @ObjectType()
 export class Template implements ITemplate {
@@ -34,7 +33,7 @@ export class Template implements ITemplate {
   })
   opportunities?: IOpportunityTemplate[];
 
-  @Field(() => [Ecoverse], {
+  @Field(() => [EcoverseTemplate], {
     nullable: false,
     description: 'Ecoverse templates.',
   })
