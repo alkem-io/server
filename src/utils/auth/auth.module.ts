c26a4601d01fc236c9083b74d3537f6780d21a1b
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-03-02 16:44:59 +0200

    Reference cleanup

--- src/utils/auth/auth.module.ts
+++ src/utils/auth/auth.module.ts
@@ -6,12 +6,10 @@ import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UserGroupModule } from '@domain/user-group/user-group.module';
 @Module({
   imports: [
     PassportModule.register({ session: false, defaultStrategy: 'bearer' }),
     forwardRef(() => UserModule),
-    UserGroupModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
