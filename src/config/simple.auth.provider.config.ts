dfd08d2cec2d27928881a397dd28feb993024a88
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-03-08 18:37:40 +0200

    Fixed JWT empty secret failure

--- src/config/simple.auth.provider.config.ts
+++ src/config/simple.auth.provider.config.ts
@@ -3,5 +3,5 @@ import { registerAs } from '@nestjs/config/dist/utils/register-as.util';
 export default registerAs('simple_auth_provider', () => ({
   issuer: process.env.AUTH_SIMPLE_AUTH_ISSUER || '',
   tokenEndpoint: process.env.AUTH_SIMPLE_AUTH_TOKEN_ENDPOINT || '',
-  clientSecret: process.env.AUTH_SIMPLE_AUTH_CLIENT_SECRET || '',
+  clientSecret: process.env.AUTH_SIMPLE_AUTH_CLIENT_SECRET || '$3cr3T',
 }));
