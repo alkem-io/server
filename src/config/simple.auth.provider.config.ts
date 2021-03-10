7d9cfcf254691f3447736024532fec1928dce6bd
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-03-10 17:21:25 +0200

    Added defaults for Simple Auth

--- src/config/simple.auth.provider.config.ts
+++ src/config/simple.auth.provider.config.ts
@@ -1,7 +1,9 @@
 import { registerAs } from '@nestjs/config/dist/utils/register-as.util';
 
 export default registerAs('simple_auth_provider', () => ({
-  issuer: process.env.AUTH_SIMPLE_AUTH_ISSUER || '',
-  tokenEndpoint: process.env.AUTH_SIMPLE_AUTH_TOKEN_ENDPOINT || '',
+  issuer: process.env.AUTH_SIMPLE_AUTH_ISSUER || 'localhost',
+  tokenEndpoint:
+    process.env.AUTH_SIMPLE_AUTH_TOKEN_ENDPOINT ||
+    'http://localhost:3002/auth/login',
   clientSecret: process.env.AUTH_SIMPLE_AUTH_CLIENT_SECRET || '$3cr3T',
 }));
