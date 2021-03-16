cb7b9339e9af561f1f90d0c8833ed3a2294cfb72
Angel <atstojanov@gmail.com>
2021-03-16 16:56:50 +0200

    Added UserNotRegisteredException

--- src/domain/community/user/user.resolver.queries.ts
+++ src/domain/community/user/user.resolver.queries.ts
@@ -10,6 +10,7 @@ import { AuthenticationException } from '@common/exceptions';
 import { AuthorizationRoles } from '@src/core/authorization/authorization.roles';
 import { CurrentUser } from '@common/decorators/current-user.decorator';
 import { UserInfo } from '@src/core/authentication/user-info';
+import { UserNotRegistredException } from '@common/exceptions/registration.exception';
 
 @Resolver(() => User)
 export class UserResolverQueries {
@@ -68,7 +69,7 @@ export class UserResolverQueries {
       );
     }
     if (!userInfo.user) {
-      throw new AuthenticationException('Unable to retrieve user profile.');
+      throw new UserNotRegistredException();
     }
     return userInfo.user;
   }
