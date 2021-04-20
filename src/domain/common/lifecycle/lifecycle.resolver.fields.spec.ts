8fb617643015f19713ce154d808f9dc6adb6a8fd
Neil Smyth <neil@thesmyths.eu>
2021-04-20 21:37:17 +0200

    updated lifecycle tests to address failing build

--- src/domain/common/lifecycle/lifecycle.resolver.fields.spec.ts
+++ src/domain/common/lifecycle/lifecycle.resolver.fields.spec.ts
@@ -1,16 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '@src/app.module';
-import { LifecycleResolver } from './lifecycle.resolver';
+import { LifecycleResolverFields } from './lifecycle.resolver.fields';
 
-describe('LifecycleResolver', () => {
-  let resolver: LifecycleResolver;
+describe('LifecycleResolverFields', () => {
+  let resolver: LifecycleResolverFields;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    resolver = module.get<LifecycleResolver>(LifecycleResolver);
+    resolver = module.get<LifecycleResolverFields>(LifecycleResolverFields);
   });
 
   it('should be defined', () => {
