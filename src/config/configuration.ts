774392f8254056427028994fedb29af59b71c464
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-04-27 13:37:29 +0300

    YAML Config WIP
    Structure is in place, needs testing and moving the rest of the config to the yaml structure

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,11 +1,50 @@
 import { readFileSync } from 'fs';
-import * as yaml from 'js-yaml';
-import { join } from 'path';
+import path, { join } from 'path';
+import * as dotenv from 'dotenv';
+import YAML from 'yaml';
 
-const YAML_CONFIG_FILENAME = 'config.yml';
+const YAML_CONFIG_FILENAME = 'cherrytwist.yml';
 
 export default () => {
-  return yaml.load(
-    readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8')
-  ) as Record<string, any>;
+  const rawYaml = readFileSync(
+    join(__dirname, '../../', YAML_CONFIG_FILENAME),
+    'utf8'
+  );
+
+  const doc = YAML.parseDocument(rawYaml);
+  const envConfig = getEnvConfig();
+
+  YAML.visit(doc, {
+    Scalar(key, node) {
+      if (node.type === 'PLAIN') {
+        node.value = buildYamlNodeValue(node.value, envConfig);
+      }
+    },
+  });
+
+  const config = doc.toJSON() as Record<string, any>;
+  return config;
 };
+
+function getEnvConfig() {
+  const filePath = '.env';
+  const envFile = path.resolve(__dirname, '../../', filePath);
+  const envConfig = dotenv.parse(readFileSync(envFile));
+
+  return envConfig;
+}
+
+function buildYamlNodeValue(nodeValue: any, envConfig: any) {
+  let updatedNodeValue = nodeValue;
+  const key = `${nodeValue}`;
+  const regex = '\\${(.*)}:?(.*)';
+  const found = key.match(regex);
+  if (found) {
+    const envVariableKey = found[1];
+    const envVariableDefaultValue = found[2];
+
+    updatedNodeValue = envConfig[envVariableKey] ?? envVariableDefaultValue;
+  }
+
+  return updatedNodeValue;
+}
