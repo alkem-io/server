e9f0529308d1ddf4d5b37e95a43f778ec664702b
Angel <atstojanov@gmail.com>
2021-05-17 13:59:18 +0300

    Fix enums

--- src/services/membership/membership.resolver.queries.ts
+++ src/services/membership/membership.resolver.queries.ts
@@ -3,14 +3,15 @@ import { Args, Resolver, Query } from '@nestjs/graphql';
 import { MembershipService } from './membership.service';
 import { Profiling } from '@src/common/decorators';
 import { AuthorizationGlobalRoles } from '@common/decorators';
-import { AuthorizationRolesGlobal, GraphqlGuard } from '@core/authorization';
+import { GraphqlGuard } from '@core/authorization';
 import { Membership, MembershipInput } from './index';
+import { AuthorizationRoleGlobal } from '@common/enums';
 
 @Resolver()
 export class MembershipResolverQueries {
   constructor(private membershipService: MembershipService) {}
 
-  @AuthorizationGlobalRoles(AuthorizationRolesGlobal.Registered)
+  @AuthorizationGlobalRoles(AuthorizationRoleGlobal.Registered)
   @UseGuards(GraphqlGuard)
   @Query(() => Membership, {
     nullable: false,
