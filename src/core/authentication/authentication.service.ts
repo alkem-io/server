9b6ff2d02d9dcf1a5425b7a12028dd88d4a2d29d
Neil Smyth <neil@thesmyths.eu>
2021-06-02 13:30:12 +0200

    reworked logic / logging in creating auth info

--- src/core/authentication/authentication.service.ts
+++ src/core/authentication/authentication.service.ts
@@ -1,16 +1,33 @@
+import { LogContext } from '@common/enums';
 import { UserService } from '@domain/community/user/user.service';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { UserInfo } from './user-info';
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService
+  ) {}
 
   async createUserInfo(email: string): Promise<UserInfo> {
-    let knownUser;
-    try {
-      knownUser = await this.userService.getUserWithAgent(email);
-    } catch (_error) {}
-    return { email, user: knownUser };
+    const userInfo = new UserInfo();
+    userInfo.email = email;
+    const userExists = await this.userService.isRegisteredUser(email);
+    if (userExists) {
+      const user = await this.userService.getUserWithAgent(email);
+      userInfo.user = user;
+      this.logger.verbose?.(
+        `Authentication Info: User registered: ${email}, with id: ${user.id}`,
+        LogContext.AUTH
+      );
+    } else {
+      this.logger.verbose?.(
+        `Authentication Info: User not registered: ${email}`,
+        LogContext.AUTH
+      );
+    }
+    return userInfo;
   }
 }
