12e2fa885cc8d83414550eedb36aa7c4e850ab56
Neil Smyth <neil@thesmyths.eu>
2021-06-14 07:29:30 +0200

    added query to retrive the set of privileges assigned to a particular user given an authorization definition

--- src/core/authorization/authorization.module.ts
+++ src/core/authorization/authorization.module.ts
@@ -1,7 +1,9 @@
 import { AgentModule } from '@domain/agent/agent/agent.module';
 import { CredentialModule } from '@domain/agent/credential/credential.module';
+import { AuthorizationDefinition } from '@domain/common/authorization-definition';
 import { UserModule } from '@domain/community/user/user.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorizationEngineModule } from '@src/services/platform/authorization-engine/authorization-engine.module';
 import { AuthorizationResolverMutations } from './authorization.resolver.mutations';
 import { AuthorizationResolverQueries } from './authorization.resolver.queries';
@@ -13,6 +15,7 @@ import { AuthorizationService } from './authorization.service';
     AgentModule,
     UserModule,
     CredentialModule,
+    TypeOrmModule.forFeature([AuthorizationDefinition]),
   ],
   providers: [
     AuthorizationService,
--- src/domain/common/authorization-definition/authorization.definition.interface.ts
+++ src/domain/common/authorization-definition/authorization.definition.interface.ts
@@ -1,10 +1,8 @@
+import { IBaseCherrytwist } from '@domain/common/base-entity';
 import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType('Authorization')
-export abstract class IAuthorizationDefinition {
-  // declare here so that the field does not get shown on api
-  id!: string;
-
+export abstract class IAuthorizationDefinition extends IBaseCherrytwist {
   @Field(() => Boolean)
   anonymousReadAccess!: boolean;
 
