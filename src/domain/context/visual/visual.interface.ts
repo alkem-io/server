7afe8472964bf6a8498cabb74f7474a9f08e5690
Neil Smyth <neil@thesmyths.eu>
2021-06-16 15:50:39 +0200

    added descriptions

--- src/domain/context/visual/visual.interface.ts
+++ src/domain/context/visual/visual.interface.ts
@@ -3,12 +3,22 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType('Visual')
 export abstract class IVisual extends IBaseCherrytwist {
-  @Field(() => String)
+  @Field(() => String, {
+    nullable: false,
+    description: 'The avatar (logo) to be used.',
+  })
   avatar!: string;
 
-  @Field(() => String)
+  @Field(() => String, {
+    nullable: false,
+    description:
+      'The background image to be used, for example when displaying previews.',
+  })
   background!: string;
 
-  @Field(() => String)
+  @Field(() => String, {
+    nullable: false,
+    description: 'The banner to be shown at the top of the page.',
+  })
   banner!: string;
 }
