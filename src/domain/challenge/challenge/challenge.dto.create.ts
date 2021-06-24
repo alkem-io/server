93ef01a18d7367cc31b80a8ae2e2c8a85eacf6c5
Neil Smyth <neil@thesmyths.eu>
2021-06-24 07:38:54 +0200

    set / update challenge leads directly via challenge mutations

--- src/domain/challenge/challenge/challenge.dto.create.ts
+++ src/domain/challenge/challenge/challenge.dto.create.ts
@@ -1,9 +1,17 @@
 import { UUID_NAMEID } from '@domain/common/scalars';
 import { Field, InputType } from '@nestjs/graphql';
 import { CreateBaseChallengeInput } from '@domain/challenge/base-challenge/base.challenge.dto.create';
+import { IsOptional } from 'class-validator';
 
 @InputType()
 export class CreateChallengeInput extends CreateBaseChallengeInput {
   @Field(() => UUID_NAMEID, { nullable: false })
   parentID!: string;
+
+  @Field(() => [UUID_NAMEID], {
+    nullable: true,
+    description: 'Set lead Organisations for the Challenge.',
+  })
+  @IsOptional()
+  leadOrganisations?: string[];
 }
