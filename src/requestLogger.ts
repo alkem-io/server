a3646eb41b8deadfb90577a378da46c3314d0a9e
Angel <atstojanov@gmail.com>
2021-07-01 16:25:39 +0300

    Added request logging middleware

---
+++ src/requestLogger.ts
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  NestMiddleware,
+  LoggerService,
+  Inject,
+} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
+import { ConfigurationTypes } from '@common/enums';
+
+@Injectable()
+export class RequestLoggerMiddleware implements NestMiddleware {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService
+  ) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const reqLogging: boolean = this.configService.get(
+      ConfigurationTypes.Monitoring
+    )?.logging?.requestLoggingEnables;
+
+    reqLogging &&
+      this.logger.verbose &&
+      this.logger.verbose(JSON.stringify(req, undefined, ' '));
+
+    const reqHeadersLogging: boolean = this.configService.get(
+      ConfigurationTypes.Monitoring
+    )?.logging?.requestHeadersLoggingEnables;
+
+    !reqLogging &&
+      reqHeadersLogging &&
+      this.logger.verbose &&
+      this.logger.verbose(JSON.stringify(req.headers, undefined, ' '));
+
+    next();
+  }
+}
