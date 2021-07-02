5389a750c7623e614ea8650db1b4c4df8fe36699
Neil Smyth <neil@thesmyths.eu>
2021-07-02 15:58:02 +0200

    added extra logic check to searchable return type

--- src/domain/common/interfaces/searchable.interface.ts
+++ src/domain/common/interfaces/searchable.interface.ts
@@ -4,6 +4,8 @@ import { IUser } from '@domain/community/user/user.interface';
 import { IUserGroup } from '@domain/community/user-group/user-group.interface';
 import { UUID } from '@domain/common/scalars';
 import { IChallenge } from '@domain/challenge/challenge/challenge.interface';
+import { RelationshipNotFoundException } from '@common/exceptions';
+import { LogContext } from '@common/enums';
 
 @InterfaceType('Searchable', {
   resolveType(searchable) {
@@ -16,7 +18,13 @@ import { IChallenge } from '@domain/challenge/challenge/challenge.interface';
     if (searchable.email) {
       return IUser;
     }
-    return IUserGroup;
+    if (searchable.name) {
+      return IUserGroup;
+    }
+    throw new RelationshipNotFoundException(
+      `Unable to determine search result type for ${searchable.id}`,
+      LogContext.SEARCH
+    );
   },
 })
 export abstract class ISearchable {
