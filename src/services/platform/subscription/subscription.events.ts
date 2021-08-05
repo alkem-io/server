08283db40c65fe8f1c6fadbcc9f51fb3c518be4f
nevelichkoff <nevelichkoff@gmail.com>
2021-08-05 11:16:06 +0300

    Standardize Pub/Sub usage in server

--- src/services/platform/subscription/subscription.events.ts
+++ src/services/platform/subscription/subscription.events.ts
@@ -1,2 +1,3 @@
 export const COMMUNICATION_MESSAGE_RECEIVED = 'communicationMessageReceived';
 export const ROOM_INVITATION_RECEIVED = 'roomInvitationReceived';
+export const USER_AVATAR_UPLOADED = 'avatarUploaded';
