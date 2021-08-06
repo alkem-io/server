9d6788c3406b47f9ee1a829a3f5a5e5c73d1f5c3
Neil Smyth <neil@thesmyths.eu>
2021-08-06 15:31:40 +0200

    updated retrieving cid from returned ipfs client result

--- src/services/platform/ipfs/ipfs.service.ts
+++ src/services/platform/ipfs/ipfs.service.ts
@@ -1,17 +1,23 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { ConfigurationTypes } from '@common/enums';
+import { ConfigurationTypes, LogContext } from '@common/enums';
 import { streamToBuffer } from '@common/utils';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as fs from 'fs';
 import { ReadStream } from 'fs';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 const { create } = require('ipfs-http-client');
 
 @Injectable()
 export class IpfsService {
   private ipfsEndpoint = '';
   private ipfsClientEndpoint = '';
-  constructor(private configService: ConfigService) {
+
+  constructor(
+    private configService: ConfigService,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: LoggerService
+  ) {
     this.ipfsEndpoint = this.configService.get(
       ConfigurationTypes.Storage
     )?.ipfs?.endpoint;
@@ -20,11 +26,13 @@ export class IpfsService {
     )?.ipfs?.clientEndpoint;
   }
 
-  public async uploadFile(filePath: string) {
-    const ipfsClient = create(new URL(this.ipfsEndpoint));
-    const image = fs.readFileSync(filePath);
-    const res = await ipfsClient.add(image, { pin: true });
-    return `${this.ipfsClientEndpoint}/${res.cid.string}`;
+  public async uploadFile(filePath: string): Promise<string> {
+    this.logger.verbose?.(
+      `Uploading file from path: ${filePath}`,
+      LogContext.IPFS
+    );
+    const imageBuffer = fs.readFileSync(filePath);
+    return this.uploadFileFromBuffer(imageBuffer);
   }
 
   public async uploadFileFromStream(stream: ReadStream): Promise<string> {
@@ -36,6 +44,10 @@ export class IpfsService {
     const ipfsClient = create(new URL(this.ipfsEndpoint));
 
     const res = await ipfsClient.add(buffer, { pin: true });
-    return `${this.ipfsClientEndpoint}/${res.cid.string}`;
+    this.logger.verbose?.(
+      `Uploaded filewith CID: ${res.path}`,
+      LogContext.IPFS
+    );
+    return `${this.ipfsClientEndpoint}/${res.path}`;
   }
 }
