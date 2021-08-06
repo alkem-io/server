abb1b2a8b688f1101cc403fef32fb3f1e5ee1a1c
Neil Smyth <neil@thesmyths.eu>
2021-08-06 11:58:24 +0200

    moved Question/IQuestion our of application dto

--- src/domain/community/application/application.entity.ts
+++ src/domain/community/application/application.entity.ts
@@ -7,13 +7,13 @@ import {
   ManyToOne,
   OneToOne,
 } from 'typeorm';
-import { IQuestion } from '@domain/community/application/application.dto.create';
 import { Community } from '@domain/community/community/community.entity';
 import { Lifecycle } from '@domain/common/lifecycle/lifecycle.entity';
 import { IApplication } from './application.interface';
 import { NVP } from '@domain/common/nvp/nvp.entity';
 import { User } from '@domain/community/user/user.entity';
 import { AuthorizableEntity } from '@domain/common/entity/authorizable-entity';
+import { IQuestion } from '@domain/common/question/question.interface';
 @Entity()
 export class Application extends AuthorizableEntity implements IApplication {
   @Column()
@@ -27,25 +27,21 @@ export class Application extends AuthorizableEntity implements IApplication {
   @JoinColumn()
   lifecycle!: Lifecycle;
 
-  @ManyToMany(
-    () => NVP,
-    nvp => nvp.id,
-    { eager: true, cascade: true }
-  )
+  @ManyToMany(() => NVP, nvp => nvp.id, { eager: true, cascade: true })
   @JoinTable({ name: 'application_questions' })
   questions?: IQuestion[];
 
-  @ManyToOne(
-    () => User,
-    user => user.applications,
-    { eager: false, cascade: false, onDelete: 'CASCADE' }
-  )
+  @ManyToOne(() => User, user => user.applications, {
+    eager: false,
+    cascade: false,
+    onDelete: 'CASCADE',
+  })
   user!: User;
 
-  @ManyToOne(
-    () => Community,
-    community => community.applications,
-    { eager: false, cascade: false, onDelete: 'CASCADE' }
-  )
+  @ManyToOne(() => Community, community => community.applications, {
+    eager: false,
+    cascade: false,
+    onDelete: 'CASCADE',
+  })
   community?: Community;
 }
