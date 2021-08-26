9fb728ecfdff35fd1f81265b02be9048014d8850
Neil Smyth <neil@thesmyths.eu>
2021-08-26 07:57:48 +0200

    added opportunities count to meta data

--- src/services/domain/metadata/metadata.module.ts
+++ src/services/domain/metadata/metadata.module.ts
@@ -1,5 +1,6 @@
 import { ChallengeModule } from '@domain/challenge/challenge/challenge.module';
 import { EcoverseModule } from '@domain/challenge/ecoverse/ecoverse.module';
+import { OpportunityModule } from '@domain/collaboration/opportunity/opportunity.module';
 import { OrganisationModule } from '@domain/community/organisation/organisation.module';
 import { UserModule } from '@domain/community/user/user.module';
 import { Module } from '@nestjs/common';
@@ -7,7 +8,13 @@ import { MetadataResolverQueries } from './metadata.resolver.queries';
 import { MetadataService } from './metadata.service';
 
 @Module({
-  imports: [UserModule, ChallengeModule, EcoverseModule, OrganisationModule],
+  imports: [
+    UserModule,
+    ChallengeModule,
+    EcoverseModule,
+    OpportunityModule,
+    OrganisationModule,
+  ],
   providers: [MetadataResolverQueries, MetadataService],
   exports: [MetadataService],
 })
