12810fd85be80eecbe931bb2b97722f95ddfb7c2
Neil Smyth <neil@thesmyths.eu>
2021-09-15 13:21:44 +0200

    add support for OpportunityAdmin credential

--- src/domain/collaboration/opportunity/index.ts
+++ src/domain/collaboration/opportunity/index.ts
@@ -1,6 +1,3 @@
 export * from './opportunity.entity';
 export * from './opportunity.interface';
-export * from './opportunity.dto.create';
-export * from './opportunity.dto.update';
-export * from './opportunity.dto.delete';
-export * from './opportunity.dto.event';
+export * from './dto';
