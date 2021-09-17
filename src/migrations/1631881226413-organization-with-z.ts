af14e855c626aaac90e5c205dc3a040ac5ee84da
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-09-17 18:57:36 +0300

    OrganiZation + Authorization_POLICY migration

--- src/migrations/1631881226413-organization-with-z.ts
+++ src/migrations/1631881226413-organization-with-z.ts
@@ -2,6 +2,19 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class organizationWithZ1631881226413 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE organisation RENAME TO organization');
+    await queryRunner.query(
+      'ALTER TABLE authorization_definition RENAME TO authorization_policy'
+    );
+    await queryRunner.query(
+      'ALTER TABLE `user_group` DROP FOREIGN KEY `FK_2b8381df8c3a1680f50e4bc2351`'
+    );
+    await queryRunner.query(
+      'ALTER TABLE `user_group` CHANGE COLUMN `organisationId` `organizationId` VARCHAR(36) NULL DEFAULT NULL'
+    );
+    await queryRunner.query(
+      'ALTER TABLE `user_group` ADD CONSTRAINT `FK_2b8381df8c3a1680f50e4bc2351` FOREIGN KEY (`organizationId`) REFERENCES `alkemio`.`organization` (`id`) ON DELETE CASCADE'
+    );
     await queryRunner.query(
       "UPDATE `credential` SET `type` = 'organization-owner' WHERE `type` = 'organisation-owner'"
     );
@@ -14,6 +27,19 @@ export class organizationWithZ1631881226413 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE organization RENAME TO organisation');
+    await queryRunner.query(
+      'ALTER TABLE authorization_policy RENAME TO authorization_definition'
+    );
+    await queryRunner.query(
+      'ALTER TABLE `user_group` DROP FOREIGN KEY `FK_2b8381df8c3a1680f50e4bc2351`'
+    );
+    await queryRunner.query(
+      'ALTER TABLE `user_group` CHANGE COLUMN `organizationId` `organisationId` VARCHAR(36) NULL DEFAULT NULL'
+    );
+    await queryRunner.query(
+      'ALTER TABLE `user_group` ADD CONSTRAINT `FK_2b8381df8c3a1680f50e4bc2351` FOREIGN KEY (`organisationId`) REFERENCES `alkemio`.`organisation` (`id`) ON DELETE CASCADE'
+    );
     await queryRunner.query(
       "UPDATE `credential` SET `type` = 'organisation-owner' WHERE `type` = 'organization-owner'"
     );
