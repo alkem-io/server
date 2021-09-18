eff4ebca7201f1a2dfb5977daf941b897bffbe64
Neil Smyth <neil@thesmyths.eu>
2021-09-18 18:38:59 +0200

    redid migration to run after organiZation

--- src/migrations/1631982968128-org-verification.ts
+++ src/migrations/1631982968128-org-verification.ts
@@ -8,25 +8,25 @@ export class orgVerification1631859505743 implements MigrationInterface {
       "CREATE TABLE `OrganizationVerification` (`id` char(36) NOT NULL, `createdDate` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedDate` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), `version` int NOT NULL, `organizationID` varchar(255) NOT NULL, `status` varchar(255) NOT NULL DEFAULT 'not-verified', `authorizationId` char(36) NULL, `lifecycleId` char(36) NULL, UNIQUE INDEX `REL_3795f9dd15ef3ef2dd1d27e309` (`authorizationId`), UNIQUE INDEX `REL_22be0d440df7972d9b3a94aa6d` (`lifecycleId`), PRIMARY KEY (`id`)) ENGINE=InnoDB"
     );
     await queryRunner.query(
-      'ALTER TABLE `organisation` DROP COLUMN `verificationType`'
+      'ALTER TABLE `organization` DROP COLUMN `verificationType`'
     );
     await queryRunner.query(
-      'ALTER TABLE `organisation` ADD `verificationId` char(36) NULL'
+      'ALTER TABLE `organization` ADD `verificationId` char(36) NULL'
     );
     await queryRunner.query(
-      'ALTER TABLE `organisation` ADD UNIQUE INDEX `IDX_95bbac07221e98072beafa6173` (`verificationId`)'
+      'ALTER TABLE `organization` ADD UNIQUE INDEX `IDX_95bbac07221e98072beafa6173` (`verificationId`)'
     );
     await queryRunner.query(
-      'CREATE UNIQUE INDEX `REL_95bbac07221e98072beafa6173` ON `organisation` (`verificationId`)'
+      'CREATE UNIQUE INDEX `REL_95bbac07221e98072beafa6173` ON `organization` (`verificationId`)'
     );
     await queryRunner.query(
-      'ALTER TABLE `OrganizationVerification` ADD CONSTRAINT `FK_3795f9dd15ef3ef2dd1d27e309c` FOREIGN KEY (`authorizationId`) REFERENCES `authorization_definition`(`id`) ON DELETE SET NULL ON UPDATE NO ACTION'
+      'ALTER TABLE `OrganizationVerification` ADD CONSTRAINT `FK_3795f9dd15ef3ef2dd1d27e309c` FOREIGN KEY (`authorizationId`) REFERENCES `authorization_policy`(`id`) ON DELETE SET NULL ON UPDATE NO ACTION'
     );
     await queryRunner.query(
       'ALTER TABLE `OrganizationVerification` ADD CONSTRAINT `FK_22be0d440df7972d9b3a94aa6d5` FOREIGN KEY (`lifecycleId`) REFERENCES `lifecycle`(`id`) ON DELETE SET NULL ON UPDATE NO ACTION'
     );
     await queryRunner.query(
-      'ALTER TABLE `organisation` ADD CONSTRAINT `FK_95bbac07221e98072beafa61732` FOREIGN KEY (`verificationId`) REFERENCES `OrganizationVerification`(`id`) ON DELETE SET NULL ON UPDATE NO ACTION'
+      'ALTER TABLE `organization` ADD CONSTRAINT `FK_95bbac07221e98072beafa61732` FOREIGN KEY (`verificationId`) REFERENCES `OrganizationVerification`(`id`) ON DELETE SET NULL ON UPDATE NO ACTION'
     );
   }
 
@@ -38,10 +38,10 @@ export class orgVerification1631859505743 implements MigrationInterface {
       'ALTER TABLE `OrganizationVerification` DROP FOREIGN KEY `FK_3795f9dd15ef3ef2dd1d27e309c`'
     );
     await queryRunner.query(
-      'ALTER TABLE `organisation` DROP COLUMN `verificationId`'
+      'ALTER TABLE `organization` DROP COLUMN `verificationId`'
     );
     await queryRunner.query(
-      "ALTER TABLE `organisation` ADD `verificationType` varchar(255) NOT NULL DEFAULT 'not-verified'"
+      "ALTER TABLE `organization` ADD `verificationType` varchar(255) NOT NULL DEFAULT 'not-verified'"
     );
     await queryRunner.query(
       'DROP INDEX `REL_22be0d440df7972d9b3a94aa6d` ON `OrganizationVerification`'
