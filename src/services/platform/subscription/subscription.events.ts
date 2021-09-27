c094858169d1505a148c2aa18c065f917e148f02
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-09-27 11:01:33 +0300

    Removed USER_AVATAR_UPLOADED

--- src/services/platform/subscription/subscription.events.ts
+++ src/services/platform/subscription/subscription.events.ts
@@ -1,3 +1,2 @@
 export const COMMUNICATION_MESSAGE_RECEIVED = 'communicationMessageReceived';
 export const MATRIX_ROOM_JOINED = 'matrixRoomJoined';
-export const USER_AVATAR_UPLOADED = 'avatarUploaded';
