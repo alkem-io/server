5b8144f959dd479585f568f538e77d81a8755cc8
Neil Smyth <neil@thesmyths.eu>
2021-10-13 08:53:39 +0200

    removed data mgmt module from app module; removed usage of auth enabled in auth service

--- src/core/authorization/authorization.service.ts
+++ src/core/authorization/authorization.service.ts
@@ -4,11 +4,11 @@ import { ICredential } from '@domain/agent/credential/credential.interface';
 import { AuthorizationPrivilege } from '@common/enums/authorization.privilege';
 import { ConfigService } from '@nestjs/config';
 import { ForbiddenException } from '@common/exceptions';
-import { ConfigurationTypes, LogContext } from '@common/enums';
 import { AgentInfo } from '@core/authentication';
 import { IAuthorizationPolicy } from '@domain/common/authorization-policy/authorization.policy.interface';
 import { AuthorizationPolicyRuleCredential } from './authorization.policy.rule.credential';
 import { AuthorizationPolicyRuleVerifiedCredential } from './authorization.policy.rule.verified.credential';
+import { LogContext } from '@common/enums';
 
 @Injectable()
 export class AuthorizationService {
@@ -24,8 +24,6 @@ export class AuthorizationService {
     privilegeRequired: AuthorizationPrivilege,
     msg: string
   ) {
-    if (this.isAuthenticationDisabled()) return true;
-
     const auth = this.validateAuthorization(authorization);
     if (this.isAccessGranted(agentInfo, auth, privilegeRequired)) return true;
 
