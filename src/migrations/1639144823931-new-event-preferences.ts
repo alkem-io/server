25b87d373e40965656f1608302fd9ed12f0f4d22
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-12-13 16:30:13 +0200

    Fixed migration of new preferences

---
+++ src/migrations/1639144823931-new-event-preferences.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { RandomGenerator } from 'typeorm/util/RandomGenerator';
+
+export class newEventPreferences1639144823931 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `INSERT INTO user_preference_definition (id, version, groupName, displayName, description, valueType, type)
+        VALUES (UUID(), 1, 'Notification', '[Admin] Community Discussion Created', 'Receive notification when a new discussion is created for a community for which I am an administrator', 'boolean', 'NotificationCommunityDiscussionCreatedAdmin')`
+    );
+    await queryRunner.query(
+      `INSERT INTO user_preference_definition (id, version, groupName, displayName, description, valueType, type)
+        VALUES (UUID(), 1, 'Notification', '[Admin] Community Updates', 'Receive notification when a new update is shared with a community for which I am an administrator', 'boolean', 'NotificationCommunityUpdateSentAdmin')`
+    );
+
+    const users: any[] = await queryRunner.query(
+      `SELECT u.id, u.displayName FROM user as u;`
+    );
+
+    const definitions: any[] = await queryRunner.query(
+      `SELECT * FROM user_preference_definition as upd WHERE upd.type='NotificationCommunityUpdateSentAdmin' || upd.type='NotificationCommunityDiscussionCreatedAdmin';`
+    );
+
+    users.forEach(user =>
+      definitions.forEach(async def => {
+        const uuid = RandomGenerator.uuid4();
+        await queryRunner.query(
+          `INSERT INTO authorization_policy VALUES ('${uuid}', NOW(), NOW(), 1, '', '', 0)`
+        );
+        await queryRunner.query(
+          `INSERT INTO user_preference VALUES (UUID(), NOW(), NOW(), 1, 'false', '${uuid}', '${def.id}', '${user.id}')`
+        );
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
