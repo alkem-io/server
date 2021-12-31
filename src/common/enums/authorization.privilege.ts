42bbc1a131b6919c5a8226b6fb5bde1d3fc69495
Neil Smyth <neil@thesmyths.eu>
2021-12-31 11:49:15 +0100

    fixed string value for CREATE_HUB privilege

--- src/common/enums/authorization.privilege.ts
+++ src/common/enums/authorization.privilege.ts
@@ -7,7 +7,7 @@ export enum AuthorizationPrivilege {
   DELETE = 'delete',
   GRANT = 'grant', // allow the issuing / revoking of credentials of the same type within a given scope
   CREATE_CANVAS = 'create-canvas',
-  CREATE_HUB = 'ceate-hub',
+  CREATE_HUB = 'create-hub',
   CREATE_ORGANIZATION = 'create-organization',
   READ_USERS = 'read-users',
 }
