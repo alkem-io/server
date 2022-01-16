23f74198e8c29f2d159397df1f3563b4bf5a4802
Neil Smyth <neil@thesmyths.eu>
2022-01-16 10:40:45 +0100

    removed avatar uploaded subscription type from enum as was not used

--- src/common/enums/subscription.type.ts
+++ src/common/enums/subscription.type.ts
@@ -1,6 +1,5 @@
 export enum SubscriptionType {
   COMMUNICATION_UPDATE_MESSAGE_RECEIVED = 'communicationUpdateMessageReceived',
   COMMUNICATION_DISCUSSION_MESSAGE_RECEIVED = 'communicationDiscussionMessageReceived',
-  USER_AVATAR_UPLOADED = 'userAvatarUploaded',
   CANVAS_CONTENT_UPDATED = 'canvasContentUpdated',
 }
