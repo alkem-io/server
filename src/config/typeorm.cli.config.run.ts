85eae1cd9258992d4224f850417607ffa54b4fcb
Neil Smyth <neil@thesmyths.eu>
2022-03-09 15:14:58 +0100

    update running of migrations to not use the entities

---
+++ src/config/typeorm.cli.config.run.ts
@@ -0,0 +1,24 @@
+import { ConnectionOptions } from 'typeorm';
+import { join } from 'path';
+
+export const typeormCliConfig: ConnectionOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST ?? 'localhost',
+  port: process.env.MYSQL_DB_PORT ? Number(process.env.MYSQL_DB_PORT) : 3306,
+  cache: true,
+  username: 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD ?? 'toor',
+  database: process.env.MYSQL_DATABASE ?? 'alkemio',
+  insecureAuth: true,
+  synchronize: false,
+  logger: 'advanced-console',
+  logging: process.env.ENABLE_ORM_LOGGING === 'true',
+  migrations: [join('src', 'migrations', '*.{ts,js}')],
+  migrationsTableName: 'migrations_typeorm',
+  migrationsRun: true,
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+module.exports = typeormCliConfig;
