10b2fec9945bceb9b7852aa53d6b2c6993c3506d
Neil Smyth <neil@thesmyths.eu>
2022-04-13 16:42:03 +0200

    updated authorization on aspects so that creator cannot delete all comments; added option for message sender to be able to delete their own comment

--- src/domain/communication/comments/comments.service.authorization.ts
+++ src/domain/communication/comments/comments.service.authorization.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { AuthorizationPrivilege } from '@common/enums';
+import { AuthorizationCredential, AuthorizationPrivilege } from '@common/enums';
 import { IAuthorizationPolicy } from '@domain/common/authorization-policy';
 import { AuthorizationPolicyService } from '@domain/common/authorization-policy/authorization.policy.service';
 import { CommentsService } from './comments.service';
 import { IComments } from './comments.interface';
 import { AuthorizationPolicyRulePrivilege } from '@core/authorization/authorization.policy.rule.privilege';
+import { AuthorizationPolicyRuleCredential } from '@core/authorization/authorization.policy.rule.credential';
+import { RoomService } from '../room/room.service';
 
 @Injectable()
 export class CommentsAuthorizationService {
   constructor(
     private authorizationPolicyService: AuthorizationPolicyService,
-    private commentsService: CommentsService
+    private commentsService: CommentsService,
+    private roomService: RoomService
   ) {}
 
   async applyAuthorizationPolicy(
@@ -43,5 +46,34 @@ export class CommentsAuthorizationService {
       authorization,
       privilegeRules
     );
+  }
+
+  async extendAuthorizationPolicyForMessageSender(
+    comments: IComments,
+    messageID: string
+  ): Promise<IAuthorizationPolicy> {
+    const newRules: AuthorizationPolicyRuleCredential[] = [];
+
+    const senderUserID = await this.roomService.getUserIdForMessage(
+      comments,
+      messageID
+    );
+
+    if (senderUserID !== '') {
+      const messageSender = new AuthorizationPolicyRuleCredential(
+        [AuthorizationPrivilege.UPDATE, AuthorizationPrivilege.DELETE],
+        AuthorizationCredential.USER_SELF_MANAGEMENT,
+        senderUserID
+      );
+      newRules.push(messageSender);
+    }
+
+    const updatedAuthorization =
+      this.authorizationPolicyService.appendCredentialAuthorizationRules(
+        comments.authorization,
+        newRules
+      );
+
+    return updatedAuthorization;
   }
 }
