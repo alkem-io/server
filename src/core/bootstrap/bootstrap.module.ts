b62a807eee72270e3f43ac371de1b213443631a1
Neil Smyth <neil@thesmyths.eu>
2022-04-22 15:39:30 +0200

    removed preference definitions from bootstrap

--- src/core/bootstrap/bootstrap.module.ts
+++ src/core/bootstrap/bootstrap.module.ts
@@ -2,24 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Hub } from '@domain/challenge/hub/hub.entity';
 import { HubModule } from '@domain/challenge/hub/hub.module';
-import { ProfileModule } from '@domain/community/profile/profile.module';
-import { TagsetModule } from '@domain/common/tagset/tagset.module';
 import { UserModule } from '@domain/community/user/user.module';
 import { OrganizationModule } from '@domain/community/organization/organization.module';
 import { AgentModule } from '@domain/agent/agent/agent.module';
 import { AdminAuthorizationModule } from '@services/admin/authorization/admin.authorization.module';
 import { BootstrapService } from './bootstrap.service';
 import { CommunicationModule } from '@domain/communication/communication/communication.module';
-import { PreferenceModule } from '@domain/common/preference/preference.module';
 
 @Module({
   imports: [
     AgentModule,
     HubModule,
-    ProfileModule,
-    TagsetModule,
     UserModule,
-    PreferenceModule,
     AdminAuthorizationModule,
     CommunicationModule,
     OrganizationModule,
