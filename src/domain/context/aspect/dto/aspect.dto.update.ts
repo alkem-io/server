f09b286440f50e32d79c9ba7d627a5319328b8b1
Neil Smyth <neil@thesmyths.eu>
2022-04-29 08:18:10 +0200

    increase data size limit on Apect.description field

--- src/domain/context/aspect/dto/aspect.dto.update.ts
+++ src/domain/context/aspect/dto/aspect.dto.update.ts
@@ -1,13 +1,13 @@
 import { UpdateNameableInput } from '@domain/common/entity/nameable-entity/nameable.dto.update';
 import { UpdateReferenceInput } from '@domain/common/reference/reference.dto.update';
 import { InputType, Field } from '@nestjs/graphql';
-import { LONG_TEXT_LENGTH } from '@src/common/constants';
+import { VERY_LONG_TEXT_LENGTH } from '@src/common/constants';
 import { IsOptional, MaxLength } from 'class-validator';
 
 @InputType()
 export class UpdateAspectInput extends UpdateNameableInput {
   @Field({ nullable: true })
-  @MaxLength(LONG_TEXT_LENGTH)
+  @MaxLength(VERY_LONG_TEXT_LENGTH)
   description?: string;
 
   @Field(() => [String], {
