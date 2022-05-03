0776a92a95c7b01d1b54ffd8e1f0859db2e13b96
Svetoslav Petkov <svetoslav@alkem.io>
2022-05-03 17:43:33 +0300

    pagination args validation added

--- src/common/exceptions/pagination/pagination.parameter.not.found.exception.ts
+++ src/common/exceptions/pagination/pagination.parameter.not.found.exception.ts
@@ -1,8 +1,8 @@
 import { LogContext, AlkemioErrorStatus } from '@common/enums';
 import { BaseException } from '../base.exception';
 
-export class PaginationInputOutOfBoundException extends BaseException {
+export class PaginationParameterNotFoundException extends BaseException {
   constructor(error: string, context = LogContext.PAGINATION) {
-    super(error, context, AlkemioErrorStatus.PAGINATION_INPUT_OUT_OF_BOUND);
+    super(error, context, AlkemioErrorStatus.PAGINATION_PARAM_NOT_FOUND);
   }
 }
