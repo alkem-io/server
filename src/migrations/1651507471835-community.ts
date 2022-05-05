05e119b6ed8b684d770d46103b3be0fc0d4d7e59
Neil Smyth <neil@thesmyths.eu>
2022-05-05 17:00:50 +0200

    reworked migration to drop usage of community membership credentials

--- src/migrations/1651507471835-community.ts
+++ src/migrations/1651507471835-community.ts
@@ -1,44 +1,75 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { randomUUID } from 'crypto';
+import { AuthorizationCredential } from '@common/enums/authorization.credential';
+import { CommunityPolicy } from '@domain/community/community/community.policy';
 
 export class community1651507471835 implements MigrationInterface {
   name = 'community1651507471835';
 
-  hubCommunityPolicy = {
-    minOrg: 1,
-    maxOrg: 1,
-    minUser: 0,
-    maxUser: 2,
+  private challengeCommunityPolicy: CommunityPolicy = {
+    member: {
+      credentialType: AuthorizationCredential.CHALLENGE_MEMBER,
+      credentialResourceID: '',
+      minOrg: 0,
+      maxOrg: -1,
+      minUser: 0,
+      maxUser: -1,
+    },
+    leader: {
+      credentialType: AuthorizationCredential.CHALLENGE_LEAD,
+      credentialResourceID: '',
+      minOrg: 0,
+      maxOrg: 9,
+      minUser: 0,
+      maxUser: 2,
+    },
   };
 
-  defaultCommunityPolicy = {
-    minOrg: 0,
-    maxOrg: 9,
-    minUser: 0,
-    maxUser: 2,
+  private opportunityCommunityPolicy: CommunityPolicy = {
+    member: {
+      credentialType: AuthorizationCredential.OPPORTUNITY_MEMBER,
+      credentialResourceID: '',
+      minOrg: 0,
+      maxOrg: -1,
+      minUser: 0,
+      maxUser: -1,
+    },
+    leader: {
+      credentialType: AuthorizationCredential.OPPORTUNITY_LEAD,
+      credentialResourceID: '',
+      minOrg: 0,
+      maxOrg: 9,
+      minUser: 0,
+      maxUser: 2,
+    },
+  };
+
+  private hubCommunityPolicy: CommunityPolicy = {
+    member: {
+      credentialType: AuthorizationCredential.HUB_MEMBER,
+      credentialResourceID: '',
+      minOrg: 0,
+      maxOrg: -1,
+      minUser: 0,
+      maxUser: -1,
+    },
+    leader: {
+      credentialType: AuthorizationCredential.HUB_HOST,
+      credentialResourceID: '',
+      minOrg: 0,
+      maxOrg: 9,
+      minUser: 0,
+      maxUser: 2,
+    },
   };
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`community\` ADD \`leadershipCredentialId\` char(36) NULL`
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`community\` ADD UNIQUE INDEX \`IDX_99999ca8ac212b8357637794d6\` (\`leadershipCredentialId\`)`
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`community\` ADD CONSTRAINT \`FK_99999ca8ac212b8357637794d6f\` FOREIGN KEY (\`leadershipCredentialId\`) REFERENCES \`credential\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
-    );
-
-    await queryRunner.query(
       `ALTER TABLE \`community\` ADD \`policy\` text NULL`
     );
 
-    await queryRunner.query(
-      `ALTER TABLE \`community\` RENAME COLUMN \`credentialId\` TO \`membershipCredentialId\``
-    );
-
     const communities: any[] = await queryRunner.query(
-      `SELECT id, membershipCredentialId from community`
+      `SELECT id, credentialId from community`
     );
     for (const community of communities) {
       console.log(`Retrieved community with id: ${community.id}`);
@@ -50,43 +81,38 @@ export class community1651507471835 implements MigrationInterface {
       );
       if (credentials.length === 1) {
         const credential = credentials[0];
-        let leadCredentialType = '';
+        let policy: CommunityPolicy;
         if (credential.type === 'hub-member') {
-          leadCredentialType = 'hub-host';
-          await queryRunner.query(
-            `update community set policy = '${JSON.stringify(
-              this.hubCommunityPolicy
-            )}' WHERE (id = '${community.id}')`
-          );
+          policy = this.hubCommunityPolicy;
+        } else if (credential.type === 'challenge-member') {
+          policy = this.challengeCommunityPolicy;
+        } else if (credential.type === 'opportunity-member') {
+          policy = this.opportunityCommunityPolicy;
+        } else {
+          throw new Error(`Credential type not defined`);
         }
-        if (credential.type === 'challenge-member') {
-          leadCredentialType = 'challenge-lead';
-          await queryRunner.query(
-            `update community set policy = '${JSON.stringify(
-              this.defaultCommunityPolicy
-            )}' WHERE (id = '${community.id}')`
-          );
-        }
-        if (credential.type === 'opportunity-member') {
-          leadCredentialType = 'opportunity-lead';
-          await queryRunner.query(
-            `update community set policy = '${JSON.stringify(
-              this.defaultCommunityPolicy
-            )}' WHERE (id = '${community.id}')`
-          );
-        }
-        if (leadCredentialType.length > 0) {
-          await queryRunner.query(
-            `INSERT INTO credential (id, version, type, resourceID)
-            values ('${leadCredentialId}', 1,  '${leadCredentialType}', '${credential.resourceID}')`
-          );
-          await queryRunner.query(
-            `update community set leadershipCredentialId = '${leadCredentialId}' WHERE (id = '${community.id}')`
-          );
-        }
+        policy.member.credentialResourceID = credential.resourceID;
+        policy.leader.credentialResourceID = credential.resourceID;
+        await queryRunner.query(
+          `update community set policy = '${JSON.stringify(
+            policy
+          )}' WHERE (id = '${community.id}')`
+        );
+        // delete the credential
+        await queryRunner.query(
+          `DELETE FROM credential WHERE (id = '${credential.id}')`
+        );
       }
     }
+    await queryRunner.query(
+      'ALTER TABLE `community` DROP FOREIGN KEY `FK_973fe78e64b8a79056d58ead433`'
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`context\` DROP COLUMN \`credentialId\``
+    );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // todo: down??
+  }
 }
