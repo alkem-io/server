28ea5a3c0d26441c8a0c115fdf050e2cea4f7365
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-06-17 12:19:54 +0300

    Fixed platform tests

--- src/services/platform/configuration/config/config.resolver.spec.ts
+++ src/services/platform/configuration/config/config.resolver.spec.ts
@@ -1,14 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from '@src/app.module';
 import { ConfigResolver } from './config.resolver';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { MockCacheManager } from '@test/mocks/cache-manager.mock';
+import { MockWinstonProvider } from '@test/mocks/winston.provider.mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('ConfigResolver', () => {
   let resolver: ConfigResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      providers: [ConfigResolver, MockCacheManager, MockWinstonProvider],
+    })
+      .useMocker(token => {
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     resolver = module.get<ConfigResolver>(ConfigResolver);
   });
--- src/services/platform/ipfs/ipfs.service.spec.ts
+++ src/services/platform/ipfs/ipfs.service.spec.ts
@@ -1,14 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from '@src/app.module';
 import { IpfsService } from './ipfs.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { MockCacheManager } from '@test/mocks/cache-manager.mock';
+import { MockWinstonProvider } from '@test/mocks/winston.provider.mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('IpfsService', () => {
   let service: IpfsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      providers: [IpfsService, MockCacheManager, MockWinstonProvider],
+    })
+      .useMocker(token => {
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     service = module.get<IpfsService>(IpfsService);
   });
