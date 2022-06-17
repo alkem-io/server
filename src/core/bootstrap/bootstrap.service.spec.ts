f0b8970c1d7770b71990f08672bd3acf748ff329
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-06-17 18:52:10 +0300

    Mocked all src/core tests

--- src/core/bootstrap/bootstrap.service.spec.ts
+++ src/core/bootstrap/bootstrap.service.spec.ts
@@ -1,5 +1,10 @@
+import { Hub } from '@domain/challenge/hub/hub.entity';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from '@src/app.module';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockCacheManager } from '@test/mocks/cache-manager.mock';
+import { MockWinstonProvider } from '@test/mocks/winston.provider.mock';
+import { defaultMockerFactory } from '@test/utils/default.mocker.factory';
+import { repositoryMockFactory } from '@test/utils/repository.mock.factory';
 import { BootstrapService } from './bootstrap.service';
 
 describe('BootstrapService', () => {
@@ -7,10 +12,20 @@ describe('BootstrapService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      providers: [
+        BootstrapService,
+        MockCacheManager,
+        MockWinstonProvider,
+        {
+          provide: getRepositoryToken(Hub),
+          useFactory: repositoryMockFactory,
+        },
+      ],
+    })
+      .useMocker(defaultMockerFactory)
+      .compile();
 
-    service = module.get<BootstrapService>(BootstrapService);
+    service = module.get(BootstrapService);
   });
 
   it('should be defined', () => {
