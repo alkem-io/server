eef04cc3e4eda4d367186189d0a865621c793596
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-06-23 16:04:49 +0300

    Update src/core/authentication/ory.strategy.ts
    Logging with the WinstonLogger format

--- src/core/authentication/ory.strategy.ts
+++ src/core/authentication/ory.strategy.ts
@@ -53,7 +53,12 @@ export class OryStrategy extends PassportStrategy(Strategy, 'oathkeeper-jwt') {
       oryIdentity = session.identity as OryDefaultIdentitySchema;
 
       if (this.authService.shouldExtendSession(session)) {
-        this.authService.extendSession(session);
+        this.authService.extendSession(session).catch(error => {
+          this.logger.error(
+            `Ory Kratos session failed to be extended: ${error}`,
+            LogContext.AUTH
+          );
+        });
       }
     } else {
       this.logger.verbose?.('No Ory Kratos session', LogContext.AUTH);
