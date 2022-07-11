dd172e3e6dfa6c1999f19d66e2bff78658390b8a
Andrew Pazniak <andrew@yess.llc>
2022-07-11 13:34:44 +0300

    Removed NOT NULL constraint from bannerCardID

--- src/migrations/1657204514370-canvas-visual.ts
+++ src/migrations/1657204514370-canvas-visual.ts
@@ -5,9 +5,7 @@ const column = '`bannerCardID`';
 
 export class canvasVisual1657204514370 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE ${table} ADD ${column} char(36) NOT NULL`
-    );
+    await queryRunner.query(`ALTER TABLE ${table} ADD ${column} char(36)`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
