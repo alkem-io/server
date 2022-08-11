aa6b462ff7aefc5fdf82f31528644efc70d02394
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-08-11 18:19:52 +0300

    Removed unused variables from alkemio.yml (#2078)
    Co-authored-by: Evgeni Dimitrov <comoque@gmail.com>

--- src/config/winston.config.ts
+++ src/config/winston.config.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import * as winston from 'winston';
 import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import { ConfigService } from '@nestjs/config';
-import * as WinstonElasticsearch from 'winston-elasticsearch';
 import { ConfigurationTypes } from '@common/enums';
 import { FileTransportOptions } from 'winston/lib/winston/transports';
 import * as logform from 'logform';
@@ -43,39 +42,6 @@ export class WinstonConfigService {
         silent: !consoleEnabled,
       }),
     ];
-
-    if (
-      this.configService.get(ConfigurationTypes.MONITORING)?.elastic?.enabled
-    ) {
-      transports.push(
-        new WinstonElasticsearch.ElasticsearchTransport({
-          level: this.configService.get(ConfigurationTypes.MONITORING)?.elastic
-            ?.logging_level,
-          transformer: logData => {
-            return {
-              '@timestamp': new Date().getTime(),
-              severity: logData.level,
-              message: `[${logData.level}] LOG Message: ${logData.message}`,
-              environment: this.configService.get(ConfigurationTypes.HOSTING)
-                ?.environment as string,
-              fields: { ...logData.meta },
-            };
-          },
-          clientOpts: {
-            cloud: {
-              id: this.configService.get(ConfigurationTypes.MONITORING)?.elastic
-                ?.cloud?.id,
-            },
-            auth: {
-              username: this.configService.get(ConfigurationTypes.MONITORING)
-                ?.elastic?.cloud?.username,
-              password: this.configService.get(ConfigurationTypes.MONITORING)
-                ?.elastic?.cloud?.password,
-            },
-          },
-        })
-      );
-    }
 
     const contextToFileConfig = this.configService.get(
       ConfigurationTypes.MONITORING
