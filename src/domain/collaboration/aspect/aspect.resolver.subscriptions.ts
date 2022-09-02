f15a7ffa1187bb40eab2ccdb3757179f32bd685f
svetoslav <svetoslav@alkem.io>
2022-09-02 15:04:42 +0300

    aspect message received

--- src/domain/collaboration/aspect/aspect.resolver.subscriptions.ts
+++ src/domain/collaboration/aspect/aspect.resolver.subscriptions.ts
@@ -3,7 +3,7 @@ import { SubscriptionType } from '@common/enums/subscription.type';
 import { AgentInfo } from '@core/authentication/agent-info';
 import { GraphqlGuard } from '@core/authorization';
 import { Inject, LoggerService, UseGuards } from '@nestjs/common';
-import { Args, Resolver, Subscription } from '@nestjs/graphql';
+import { Args, Resolver } from '@nestjs/graphql';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { PubSubEngine } from 'graphql-subscriptions';
 import { LogContext } from '@common/enums/logging.context';
@@ -13,6 +13,8 @@ import { AuthorizationPrivilege } from '@common/enums/authorization.privilege';
 import { SUBSCRIPTION_ASPECT_COMMENT } from '@constants/providers';
 import { AspectCommentsMessageReceived } from './dto/aspect.dto.event.message.received';
 import { AspectService } from '@domain/collaboration/aspect/aspect.service';
+import { TypedSubscription } from '@src/common/decorators';
+import { AspectMessageReceivedArgs } from './dto/aspect.message.received.args';
 
 @Resolver()
 export class AspectResolverSubscriptions {
@@ -28,43 +30,41 @@ export class AspectResolverSubscriptions {
   // Note: the resolving method should not be doing any heavy lifting.
   // Relies on users being cached for performance.
   @UseGuards(GraphqlGuard)
-  @Subscription(() => AspectCommentsMessageReceived, {
-    description: 'Receive new comment on Aspect',
-    async resolve(
-      this: AspectResolverSubscriptions,
-      value: AspectCommentsMessageReceived,
-      _: any,
-      context: any
-    ): Promise<AspectCommentsMessageReceived> {
-      const agentInfo = context.req?.user;
-      const logMsgPrefix = `[User (${agentInfo.email}) Aspect comments] - `;
-      this.logger.verbose?.(
-        `${logMsgPrefix} sending out new message event for Aspect: ${value.aspectID} `,
-        LogContext.SUBSCRIPTIONS
-      );
-      return value;
-    },
-    async filter(
-      this: AspectResolverSubscriptions,
-      payload: AspectCommentsMessageReceived,
-      variables: { aspectID: string },
-      context: { req: { user: AgentInfo } }
-    ) {
-      const agentInfo = context.req.user;
-      const logMsgPrefix = `[User (${agentInfo.email}) Aspect comments]`;
-      this.logger.verbose?.(
-        `${logMsgPrefix} Filtering event '${payload.eventID}'`,
-        LogContext.SUBSCRIPTIONS
-      );
+  @TypedSubscription<AspectCommentsMessageReceived, AspectMessageReceivedArgs>(
+    () => AspectCommentsMessageReceived,
+    {
+      description: 'Receive new comment on Aspect',
+      resolve(this: AspectResolverSubscriptions, payload, args, context) {
+        const agentInfo = context.req?.user;
+        const logMsgPrefix = `[User (${agentInfo.email}) Aspect comments] - `;
+        this.logger.verbose?.(
+          `${logMsgPrefix} sending out new message event for Aspect: ${payload.aspectID} `,
+          LogContext.SUBSCRIPTIONS
+        );
+        return payload;
+      },
+      async filter(
+        this: AspectResolverSubscriptions,
+        payload,
+        variables,
+        context
+      ) {
+        const agentInfo = context.req.user;
+        const logMsgPrefix = `[User (${agentInfo.email}) Aspect comments]`;
+        this.logger.verbose?.(
+          `${logMsgPrefix} Filtering event '${payload.eventID}'`,
+          LogContext.SUBSCRIPTIONS
+        );
 
-      const isSameCommentsInstance = payload.aspectID === variables.aspectID;
-      this.logger.verbose?.(
-        `${logMsgPrefix} Filter result is ${isSameCommentsInstance}`,
-        LogContext.SUBSCRIPTIONS
-      );
-      return isSameCommentsInstance;
-    },
-  })
+        const isSameCommentsInstance = payload.aspectID === variables.aspectID;
+        this.logger.verbose?.(
+          `${logMsgPrefix} Filter result is ${isSameCommentsInstance}`,
+          LogContext.SUBSCRIPTIONS
+        );
+        return isSameCommentsInstance;
+      },
+    }
+  )
   async aspectCommentsMessageReceived(
     @CurrentUser() agentInfo: AgentInfo,
     @Args({
