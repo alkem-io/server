a7fbf2a6882663db9d3155a42db3eefa8dd12dc6
Neil Smyth <neil@thesmyths.eu>
2022-09-12 15:27:47 +0200

    updated migration to use varchar instead of char

--- src/migrations/1662128944732-activity.ts
+++ src/migrations/1662128944732-activity.ts
@@ -5,7 +5,7 @@ export class activity1662128944732 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`activity\` (\`id\` char(36) NOT NULL, \`createdDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`version\` int NOT NULL, \`triggeredBy\` varchar(36) NULL, \`collaborationID\` varchar(36) NULL, \`resourceID\` varchar(36) NULL, \`description\` varchar(255) NOT NULL, \`type\` char(128) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+      `CREATE TABLE \`activity\` (\`id\` varchar(36) NOT NULL, \`createdDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`version\` int NOT NULL, \`triggeredBy\` varchar(36) NULL, \`collaborationID\` varchar(36) NULL, \`resourceID\` varchar(36) NULL, \`description\` varchar(255) NOT NULL, \`type\` varchar(128) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
     );
   }
 
