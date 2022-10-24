e558f0247069ba95ed3a3794824dc3d2150dc645
Neil Smyth <neil@thesmyths.eu>
2022-10-24 21:22:45 +0200

    renamed organization members to associates

--- src/domain/community/organization/dto/organization.dto.assign.associate.ts
+++ src/domain/community/organization/dto/organization.dto.assign.associate.ts
@@ -2,7 +2,7 @@ import { UUID_NAMEID, UUID_NAMEID_EMAIL } from '@domain/common/scalars';
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class RemoveOrganizationMemberInput {
+export class AssignOrganizationAssociateInput {
   @Field(() => UUID_NAMEID, { nullable: false })
   organizationID!: string;
 
--- src/domain/community/organization/dto/organization.dto.remove.associate.ts
+++ src/domain/community/organization/dto/organization.dto.remove.associate.ts
@@ -2,7 +2,7 @@ import { UUID_NAMEID, UUID_NAMEID_EMAIL } from '@domain/common/scalars';
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class AssignOrganizationMemberInput {
+export class RemoveOrganizationAssociateInput {
   @Field(() => UUID_NAMEID, { nullable: false })
   organizationID!: string;
 
