e4fab540cffd3b95a33c7dea98a5afb075def671
Andrew P <594548+me-andre@users.noreply.github.com>
2022-10-26 07:50:27 +0200

    stronger typings / long switch statement refactored (#2246)
    * stronger typings
    
    * Addressed comments
    
    * better typing

---
+++ src/services/api/activity-log/activity.log.builder.interface.ts
@@ -0,0 +1,25 @@
+import { ActivityEventType } from '@common/enums/activity.event.type';
+import { IActivityLogEntryMemberJoined } from '@services/api/activity-log/dto/activity.log.dto.entry.member.joined.interface';
+import { IActivityLogEntryCalloutPublished } from '@services/api/activity-log/dto/activity.log.dto.entry.callout.published';
+import { IActivityLogEntryCalloutCardCreated } from '@services/api/activity-log/dto/activity.log.dto.entry.callout.card.created';
+import { IActivityLogEntryCalloutCanvasCreated } from '@services/api/activity-log/dto/activity.log.dto.entry.callout.canvas.created';
+import { IActivityLogEntryChallengeCreated } from '@services/api/activity-log/dto/activity.log.dto.entry.challenge.created';
+import { IActivityLogEntryOpportunityCreated } from '@services/api/activity-log/dto/activity.log.dto.entry.opportunity.created';
+import { IActivityLogEntryCalloutCardComment } from '@services/api/activity-log/dto/activity.log.dto.entry.callout.card.comment';
+import { IActivityLogEntryCalloutDiscussionComment } from '@services/api/activity-log/dto/activity.log.dto.entry.callout.discussion.comment';
+import { IActivity } from '@platform/activity';
+
+interface ActivityLogBuilderFunction<TypedActivityLogEntry> {
+  (rawActivity: IActivity): Promise<TypedActivityLogEntry>;
+}
+
+export interface IActivityLogBuilder {
+  [ActivityEventType.MEMBER_JOINED]: ActivityLogBuilderFunction<IActivityLogEntryMemberJoined>;
+  [ActivityEventType.CALLOUT_PUBLISHED]: ActivityLogBuilderFunction<IActivityLogEntryCalloutPublished>;
+  [ActivityEventType.CARD_CREATED]: ActivityLogBuilderFunction<IActivityLogEntryCalloutCardCreated>;
+  [ActivityEventType.CANVAS_CREATED]: ActivityLogBuilderFunction<IActivityLogEntryCalloutCanvasCreated>;
+  [ActivityEventType.CHALLENGE_CREATED]: ActivityLogBuilderFunction<IActivityLogEntryChallengeCreated>;
+  [ActivityEventType.OPPORTUNITY_CREATED]: ActivityLogBuilderFunction<IActivityLogEntryOpportunityCreated>;
+  [ActivityEventType.CARD_COMMENT]: ActivityLogBuilderFunction<IActivityLogEntryCalloutCardComment>;
+  [ActivityEventType.DISCUSSION_COMMENT]: ActivityLogBuilderFunction<IActivityLogEntryCalloutDiscussionComment>;
+}
--- src/services/api/activity-log/activity.log.service.ts
+++ src/services/api/activity-log/activity.log.service.ts
@@ -8,18 +8,16 @@ import { IActivityLogEntry } from './dto/activity.log.entry.interface';
 import { UserService } from '@domain/community/user/user.service';
 import { ActivityEventType } from '@common/enums/activity.event.type';
 import { CommunityService } from '@domain/community/community/community.service';
-import { IActivityLogEntryMemberJoined } from './dto/activity.log.dto.entry.member.joined.interface';
 import { CalloutService } from '@domain/collaboration/callout/callout.service';
-import { IActivityLogEntryCalloutPublished } from './dto/activity.log.dto.entry.callout.published';
 import { AspectService } from '@domain/collaboration/aspect/aspect.service';
-import { IActivityLogEntryCalloutCardCreated } from './dto/activity.log.dto.entry.callout.card.created';
 import { CanvasService } from '@domain/common/canvas/canvas.service';
-import { IActivityLogEntryCalloutCanvasCreated } from './dto/activity.log.dto.entry.callout.canvas.created';
-import { IActivityLogEntryCalloutDiscussionComment } from './dto/activity.log.dto.entry.callout.discussion.comment';
 import { ChallengeService } from '@domain/challenge/challenge/challenge.service';
 import { OpportunityService } from '@domain/collaboration/opportunity/opportunity.service';
-import { IActivityLogEntryChallengeCreated } from './dto/activity.log.dto.entry.challenge.created';
-import { IActivityLogEntryOpportunityCreated } from './dto/activity.log.dto.entry.opportunity.created';
+import { IActivity } from '@platform/activity';
+import ActivityLogBuilderService, {
+  IActivityLogBuilder,
+} from '@services/api/activity-log/activity.log.builder.service';
+import { IActivityLogEntryBase } from '@services/api/activity-log/activity.log.entry.base.interface';
 
 export class ActivityLogService {
   constructor(
@@ -45,7 +43,7 @@ export class ActivityLogService {
       LogContext.ACTIVITY
     );
 
-    let rawActivities = [];
+    let rawActivities: IActivity[] = [];
     if (queryData && queryData.collaborationID) {
       rawActivities = await this.activityService.getAllActivityForCollaboration(
         queryData.collaborationID
@@ -53,122 +51,35 @@ export class ActivityLogService {
     } else {
       rawActivities = await this.activityService.getAllActivity();
     }
-    const results: IActivityLogEntry[] = [];
-    for (const rawActivity of rawActivities) {
-      const userTriggeringActivity = await this.userService.getUserOrFail(
-        rawActivity.triggeredBy
-      );
-      const activityLogEntryBase = {
-        id: rawActivity.id,
-        triggeredBy: userTriggeringActivity,
-        createdDate: rawActivity.createdDate,
-        type: rawActivity.type,
-        description: rawActivity.description,
-        collaborationID: rawActivity.collaborationID,
-      };
-      switch (rawActivity.type) {
-        case ActivityEventType.MEMBER_JOINED:
-          const community = await this.communityService.getCommunityOrFail(
-            rawActivity.parentID
+    const results: IActivityLogEntry[] = await Promise.all(
+      rawActivities.map(async rawActivity => {
+        const userTriggeringActivity = await this.userService.getUserOrFail(
+          rawActivity.triggeredBy
+        );
+        const activityLogEntryBase: IActivityLogEntryBase = {
+          id: rawActivity.id,
+          triggeredBy: userTriggeringActivity,
+          createdDate: rawActivity.createdDate,
+          type: rawActivity.type,
+          description: rawActivity.description,
+          collaborationID: rawActivity.collaborationID,
+        };
+        const activityBuilder: IActivityLogBuilder =
+          new ActivityLogBuilderService(
+            activityLogEntryBase,
+            this.activityService,
+            this.userService,
+            this.calloutService,
+            this.aspectService,
+            this.canvasService,
+            this.challengeService,
+            this.opportunityService,
+            this.communityService
           );
-          const userJoining = await this.userService.getUserOrFail(
-            rawActivity.resourceID
-          );
-          const activityMemberJoined: IActivityLogEntryMemberJoined = {
-            ...activityLogEntryBase,
-            community: community,
-            user: userJoining,
-            communityType: `${community.type}`,
-          };
-          results.push(activityMemberJoined);
-          break;
-        case ActivityEventType.CALLOUT_PUBLISHED:
-          const callout = await this.calloutService.getCalloutOrFail(
-            rawActivity.resourceID
-          );
-          const activityCalloutPublished: IActivityLogEntryCalloutPublished = {
-            ...activityLogEntryBase,
-            callout: callout,
-          };
-          results.push(activityCalloutPublished);
-          break;
-        case ActivityEventType.CARD_CREATED:
-          const calloutCardCreated = await this.calloutService.getCalloutOrFail(
-            rawActivity.parentID
-          );
-          const cardCreated = await this.aspectService.getAspectOrFail(
-            rawActivity.resourceID
-          );
-          const activityCalloutCardCreated: IActivityLogEntryCalloutCardCreated =
-            {
-              ...activityLogEntryBase,
-              callout: calloutCardCreated,
-              card: cardCreated,
-            };
-          results.push(activityCalloutCardCreated);
-          break;
-        case ActivityEventType.CANVAS_CREATED:
-          const calloutCanvasCreated =
-            await this.calloutService.getCalloutOrFail(rawActivity.parentID);
-          const canvasCreated = await this.canvasService.getCanvasOrFail(
-            rawActivity.resourceID
-          );
-          const activityCalloutCanvasCreated: IActivityLogEntryCalloutCanvasCreated =
-            {
-              ...activityLogEntryBase,
-              callout: calloutCanvasCreated,
-              canvas: canvasCreated,
-            };
-          results.push(activityCalloutCanvasCreated);
-          break;
-        case ActivityEventType.CHALLENGE_CREATED:
-          const challenge = await this.challengeService.getChallengeOrFail(
-            rawActivity.resourceID
-          );
-          const activityChallengeCreated: IActivityLogEntryChallengeCreated = {
-            ...activityLogEntryBase,
-            challenge: challenge,
-          };
-          results.push(activityChallengeCreated);
-          break;
-        case ActivityEventType.OPPORTUNITY_CREATED:
-          const opportunity =
-            await this.opportunityService.getOpportunityOrFail(
-              rawActivity.resourceID
-            );
-          const activityOpportunityCreated: IActivityLogEntryOpportunityCreated =
-            {
-              ...activityLogEntryBase,
-              opportunity: opportunity,
-            };
-          results.push(activityOpportunityCreated);
-          break;
-        case ActivityEventType.CARD_COMMENT:
-          const calloutCardComment = await this.calloutService.getCalloutOrFail(
-            rawActivity.parentID
-          );
-          const cardCommentedOn = await this.aspectService.getAspectOrFail(
-            rawActivity.resourceID
-          );
-          const activityCalloutCardCommentedOn: IActivityLogEntryCalloutCardCreated =
-            {
-              ...activityLogEntryBase,
-              callout: calloutCardComment,
-              card: cardCommentedOn,
-            };
-          results.push(activityCalloutCardCommentedOn);
-          break;
-        case ActivityEventType.DISCUSSION_COMMENT:
-          const calloutDiscussionComment =
-            await this.calloutService.getCalloutOrFail(rawActivity.resourceID);
-          const activityCalloutDiscussionComment: IActivityLogEntryCalloutDiscussionComment =
-            {
-              ...activityLogEntryBase,
-              callout: calloutDiscussionComment,
-            };
-          results.push(activityCalloutDiscussionComment);
-      }
-    }
+        const activityType = rawActivity.type as ActivityEventType;
+        return activityBuilder[activityType](rawActivity);
+      })
+    );
     return results;
   }
 }
