fc7b34b5936a7331674bd948583603124659e10b
Neil Smyth <30729240+techsmyth@users.noreply.github.com>
2022-10-26 07:55:20 +0200

    acitivityLogOnCollaboration to be strongly typed (#2244)
    * initial prototype of having activity strongly typed
    
    * first set of activity types added
    
    * new activity log service initial draft
    
    * removed old service + renamed new one
    
    * added community type for member joined
    
    * for messages only put the actual message into description; for new members use the actual triggering user
    
    * stronger typings / long switch statement refactored (#2246)
    
    * stronger typings
    
    * Addressed comments
    
    * better typing
    
    Co-authored-by: Andrew P <594548+me-andre@users.noreply.github.com>

--- src/services/api/activity-log/activity.log.module.ts
+++ src/services/api/activity-log/activity.log.module.ts
@@ -5,6 +5,13 @@ import { ActivityModule } from '@src/platform/activity/activity.module';
 import { ActivityLogResolverQueries } from './activity.log.resolver.queries';
 import { ActivityLogService } from './activity.log.service';
 import { CollaborationModule } from '@domain/collaboration/collaboration/collaboration.module';
+import { UserModule } from '@domain/community/user/user.module';
+import { CommunityModule } from '@domain/community/community/community.module';
+import { CalloutModule } from '@domain/collaboration/callout/callout.module';
+import { AspectModule } from '@domain/collaboration/aspect/aspect.module';
+import { CanvasModule } from '@domain/common/canvas/canvas.module';
+import { ChallengeModule } from '@domain/challenge/challenge/challenge.module';
+import { OpportunityModule } from '@domain/collaboration/opportunity/opportunity.module';
 
 @Module({
   imports: [
@@ -12,6 +19,13 @@ import { CollaborationModule } from '@domain/collaboration/collaboration/collabo
     AuthorizationPolicyModule,
     ActivityModule,
     CollaborationModule,
+    UserModule,
+    CommunityModule,
+    CalloutModule,
+    AspectModule,
+    CanvasModule,
+    ChallengeModule,
+    OpportunityModule,
   ],
   providers: [ActivityLogService, ActivityLogResolverQueries],
   exports: [],
