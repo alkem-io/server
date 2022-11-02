0c70c692a2f4ad7ffbf29f7b02006d3bd470249d
Neil Smyth <neil@thesmyths.eu>
2022-11-02 21:26:53 +0100

    blank migration

---
+++ src/migrations/1667420764375-library.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class library1667420764375 implements MigrationInterface {
+  name = 'library1667420764375';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
