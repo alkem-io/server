020baf67c77b28fe92b5b1387ce4fd8f400517b1
Neil Smyth <neil@thesmyths.eu>
2022-11-03 07:40:50 +0100

    added create innovatino pack, sample mutation

--- src/library/library/dto/library.dto.create.innovation.pack.ts
+++ src/library/library/dto/library.dto.create.innovation.pack.ts
@@ -1,9 +1,5 @@
-import { UUID } from '@domain/common/scalars';
 import { CreateInnovationPackInput } from '@library/innovation-pack/dto/innovation.pack.dto.create';
-import { InputType, Field } from '@nestjs/graphql';
+import { InputType } from '@nestjs/graphql';
 
 @InputType()
-export class CreateInnovationPackOnLibraryInput extends CreateInnovationPackInput {
-  @Field(() => UUID, { nullable: false })
-  libraryID!: string;
-}
+export class CreateInnovationPackOnLibraryInput extends CreateInnovationPackInput {}
--- src/library/library/library.resolver.mutations.ts
+++ src/library/library/library.resolver.mutations.ts
@@ -1,5 +1,5 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Mutation } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { CurrentUser, Profiling } from '@src/common/decorators';
 import { GraphqlGuard } from '@core/authorization';
 import { AgentInfo } from '@core/authentication';
@@ -8,13 +8,17 @@ import { AuthorizationPrivilege } from '@common/enums/authorization.privilege';
 import { LibraryService } from './library.service';
 import { ILibrary } from './library.interface';
 import { LibraryAuthorizationService } from './library.service.authorization';
+import { IInnovationPack } from '@library/innovation-pack/innovation.pack.interface';
+import { CreateInnovationPackOnLibraryInput } from './dto/library.dto.create.innovation.pack';
+import { InnovationPackAuthorizationService } from '@library/innovation-pack/innovation.pack.service.authorization';
 
 @Resolver()
 export class LibraryResolverMutations {
   constructor(
     private authorizationService: AuthorizationService,
     private libraryService: LibraryService,
-    private libraryAuthorizationService: LibraryAuthorizationService
+    private libraryAuthorizationService: LibraryAuthorizationService,
+    private innovationPackAuthorizationService: InnovationPackAuthorizationService
   ) {}
 
   @UseGuards(GraphqlGuard)
@@ -35,5 +39,34 @@ export class LibraryResolverMutations {
     return await this.libraryAuthorizationService.applyAuthorizationPolicy(
       library
     );
+  }
+
+  @UseGuards(GraphqlGuard)
+  @Mutation(() => IInnovationPack, {
+    description: 'Create a new InnovatonPack on the Library.',
+  })
+  @Profiling.api
+  async createInnovationPackOnLibrary(
+    @CurrentUser() agentInfo: AgentInfo,
+    @Args('packData') packData: CreateInnovationPackOnLibraryInput
+  ): Promise<IInnovationPack> {
+    const library = await this.libraryService.getLibraryOrFail();
+
+    this.authorizationService.grantAccessOrFail(
+      agentInfo,
+      library.authorization,
+      AuthorizationPrivilege.CREATE,
+      `create innovationPack on library: ${library.id}`
+    );
+
+    const innovationPack = await this.libraryService.createInnovationPack(
+      packData
+    );
+    const innovationPackAuthorized =
+      await this.innovationPackAuthorizationService.applyAuthorizationPolicy(
+        innovationPack,
+        library.authorization
+      );
+    return innovationPackAuthorized;
   }
 }
