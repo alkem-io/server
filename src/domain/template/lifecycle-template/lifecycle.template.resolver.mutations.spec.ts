4a5bec2dec44febd4259ee653ec08ea0e12f43aa
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2022-11-04 12:13:14 +0200

    Reverted hack

--- src/domain/template/lifecycle-template/lifecycle.template.resolver.mutations.spec.ts
+++ src/domain/template/lifecycle-template/lifecycle.template.resolver.mutations.spec.ts
@@ -1,20 +1,16 @@
-import { ContextResolverMutations } from '@domain/context/context/context.resolver.mutations';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MockCacheManager } from '@test/mocks/cache-manager.mock';
 import { MockWinstonProvider } from '@test/mocks/winston.provider.mock';
 import { defaultMockerFactory } from '@test/utils/default.mocker.factory';
+import { LifecycleTemplateResolverMutations } from './lifecycle.template.resolver.mutations';
 
 describe('LifecycleTemplateResolverMutations', () => {
-  //toDo fix this hack placeholder asap
-  // let resolver: LifecycleTemplateResolverMutations;
-  let resolver: ContextResolverMutations;
+  let resolver: LifecycleTemplateResolverMutations;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        //toDo fix this hack placeholder asap
-        // LifecycleTemplateResolverMutations,
-        ContextResolverMutations,
+        LifecycleTemplateResolverMutations,
         MockCacheManager,
         MockWinstonProvider,
       ],
@@ -22,7 +18,7 @@ describe('LifecycleTemplateResolverMutations', () => {
       .useMocker(defaultMockerFactory)
       .compile();
 
-    resolver = module.get(ContextResolverMutations);
+    resolver = module.get(LifecycleTemplateResolverMutations);
   });
 
   it('should be defined', () => {
