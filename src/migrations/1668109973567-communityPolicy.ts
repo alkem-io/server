6faca683b006e37cdad51175a0684c1a0b5e000a
Neil Smyth <neil@thesmyths.eu>
2022-11-17 21:03:53 +0100

    revert previous fix

--- src/migrations/1668109973567-communityPolicy.ts
+++ src/migrations/1668109973567-communityPolicy.ts
@@ -13,7 +13,7 @@ export class communityPolicy1668109973567 implements MigrationInterface {
     );
 
     await queryRunner.query(
-      `ALTER TABLE \`community\` ADD \`policyId\` varchar(36) NULL`
+      `ALTER TABLE \`community\` ADD \`policyId\` varchar(36) NOT NULL`
     );
 
     const communities: { id: string; policy: string }[] =
@@ -89,7 +89,7 @@ export class communityPolicy1668109973567 implements MigrationInterface {
       'ALTER TABLE `community` ADD UNIQUE INDEX `IDX_c9ff67519d26140f98265a542e` (`policyId`)'
     );
     await queryRunner.query(
-      `ALTER TABLE \`community\` ADD CONSTRAINT \`FK_35533901817dd09d5906537e088\` FOREIGN KEY (\`policyId\`) REFERENCES \`community_policy\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+      `ALTER TABLE \`community\` ADD CONSTRAINT \`FK_35533901817dd09d5906537e088\` FOREIGN KEY (\`policyId\`) REFERENCES \`community_policy\`(\`id\`) ON UPDATE NO ACTION`
     );
   }
 
