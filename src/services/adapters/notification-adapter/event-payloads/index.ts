b4cb5a091fc39be8a186dede77fa247e179c47bc
Neil Smyth <neil@thesmyths.eu>
2022-11-26 09:40:41 +0100

    using notificaitons-lib package

--- src/services/adapters/notification-adapter/event-payloads/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './aspect.comment.created.event.payload';
-export * from './aspect.created.event.payload';
-export * from './community.collaboration.interest.event.payload';
-export * from './callout.published.event.payload';
