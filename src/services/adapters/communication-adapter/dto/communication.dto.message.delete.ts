ff31a7095902786acf8fb37404d4fad967e57eac
Neil Smyth <neil@thesmyths.eu>
2022-12-17 12:59:23 +0100

    removed graphql decorations from dtos used for communication adapter

--- src/services/adapters/communication-adapter/dto/communication.dto.message.delete.ts
+++ src/services/adapters/communication-adapter/dto/communication.dto.message.delete.ts
@@ -1,12 +1,7 @@
-import { Field, InputType } from '@nestjs/graphql';
-@InputType()
 export class CommunicationDeleteMessageInput {
-  @Field(() => String, { nullable: false })
   senderCommunicationsID!: string;
 
-  @Field(() => String, { nullable: false })
   messageId!: string;
 
-  @Field(() => String, { nullable: false })
   roomID!: string;
 }
