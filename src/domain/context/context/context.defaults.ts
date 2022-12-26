f0c9534f5ae355d082656a3397cd1d7e6bae0ca3
Neil Smyth <neil@thesmyths.eu>
2022-12-26 10:24:22 +0100

    added better defaults for recommendations

---
+++ src/domain/context/context/context.defaults.ts
@@ -0,0 +1,21 @@
+export const contextDefaults: any = {
+  recommendations: [
+    {
+      name: 'Share your ideas and insights',
+      description:
+        'Here you can engage with your community. Sharing ideas and insights, making impact together.',
+      uri: './contribute',
+    },
+    {
+      name: 'About this ommunity',
+      description:
+        'Click here to find out more about the background of this community and where it is going.',
+      uri: './about',
+    },
+    {
+      name: 'Highlighted callout',
+      description: '',
+      uri: '',
+    },
+  ],
+};
