06563c20b2188827876c3c1c85d2797ca929b433
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2022-12-28 14:16:37 +0100

    Change url to better differentiate external links (#2418)

--- src/domain/context/context/context.defaults.ts
+++ src/domain/context/context/context.defaults.ts
@@ -1,16 +1,16 @@
-export const contextDefaults: any = {
+export const contextDefaults = {
   recommendations: [
     {
       name: 'Share your ideas and insights',
       description:
         'Here you can engage with your community. Sharing ideas and insights, making impact together.',
-      uri: './contribute',
+      uri: 'contribute',
     },
     {
       name: 'About this community',
       description:
         'Click here to find out more about the background of this community and where it is going.',
-      uri: './about',
+      uri: 'about',
     },
     {
       name: 'Highlighted callout',
