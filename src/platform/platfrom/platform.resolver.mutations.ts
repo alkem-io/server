785bf7b7026492cff88a33061a90e0aae477c798
Neil Smyth <neil@thesmyths.eu>
2023-01-04 13:31:39 +0100

    removed unused service

--- src/platform/platfrom/platform.resolver.mutations.ts
+++ src/platform/platfrom/platform.resolver.mutations.ts
@@ -5,7 +5,6 @@ import { GraphqlGuard } from '@core/authorization';
 import { AgentInfo } from '@core/authentication';
 import { AuthorizationService } from '@core/authorization/authorization.service';
 import { AuthorizationPrivilege } from '@common/enums/authorization.privilege';
-import { PlatformService } from './platform.service';
 import { IPlatform } from './platform.interface';
 import { PlatformAuthorizationService } from './platform.service.authorization';
 import { PlatformAuthorizationPolicyService } from '@platform/authorization/platform.authorization.policy.service';
@@ -14,7 +13,6 @@ import { PlatformAuthorizationPolicyService } from '@platform/authorization/plat
 export class PlatformResolverMutations {
   constructor(
     private authorizationService: AuthorizationService,
-    private platformService: PlatformService,
     private platformAuthorizationService: PlatformAuthorizationService,
     private platformAuthorizationPolicyService: PlatformAuthorizationPolicyService
   ) {}
