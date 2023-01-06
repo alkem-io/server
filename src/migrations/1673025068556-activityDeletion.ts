bfef5ddbaf85605140ea0a08d650c1cbda0555a5
Neil Smyth <neil@thesmyths.eu>
2023-01-06 18:16:57 +0100

    expand activity table to allow for hidden + storing message IDs

---
+++ src/migrations/1673025068556-activityDeletion.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class activityDeletion1673025068556 implements MigrationInterface {
+  name = 'activityDeletion1673025068556';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`activity\` ADD \`messageID\` char(44) NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`activity\` ADD \`hidden\` boolean DEFAULT FALSE `
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`activity\` DROP COLUMN \`messageID\``
+    );
+    await queryRunner.query(`ALTER TABLE \`activity\` DROP COLUMN \`hidden\``);
+  }
+}
