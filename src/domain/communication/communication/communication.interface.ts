cd5ed02fc0f9a6c30079c0e835fc5ae29cf7a787
Neil Smyth <30729240+techsmyth@users.noreply.github.com>
2023-01-17 11:15:06 +0100

    Added allowed discussion categories to communication (#2471)
    * added allowed discussion categories to communication; composed discussion category like with preferences; set as part of communication creation
    
    * fixed creation of default canvas callout on collaboration
    
    * added logic to check for valid category; added exception type for discussion category not valid; fixed bug with authorization on discussions
    
    Co-authored-by: Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>

--- src/domain/communication/communication/communication.interface.ts
+++ src/domain/communication/communication/communication.interface.ts
@@ -1,7 +1,8 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { IAuthorizable } from '@domain/common/entity/authorizable-entity';
 import { IDiscussion } from '../discussion/discussion.interface';
 import { IUpdates } from '../updates/updates.interface';
+import { DiscussionCategory } from '@common/enums/communication.discussion.category';
 
 @ObjectType('Communication')
 export abstract class ICommunication extends IAuthorizable {
@@ -11,6 +12,9 @@ export abstract class ICommunication extends IAuthorizable {
   hubID!: string;
 
   displayName!: string;
+
+  @Field(() => [DiscussionCategory])
+  discussionCategories!: string[];
 
   // Communications related information
   communicationGroupID!: string;
