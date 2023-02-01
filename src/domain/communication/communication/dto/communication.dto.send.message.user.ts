398498d283d6a1cbcf93768a2ab061da23c1bb3b
Aleksandar Stojanovic <aleksandarstojanovic.coa@gmail.com>
2023-02-01 13:57:04 +0100

    Change message length validation rule

--- src/domain/communication/communication/dto/communication.dto.send.message.user.ts
+++ src/domain/communication/communication/dto/communication.dto.send.message.user.ts
@@ -1,4 +1,4 @@
-import { MID_TEXT_LENGTH } from '@common/constants';
+import { LONG_TEXT_LENGTH } from '@common/constants';
 import { UUID } from '@domain/common/scalars';
 import { Field, InputType } from '@nestjs/graphql';
 
@@ -16,6 +16,6 @@ export class CommunicationSendMessageToUserInput {
     nullable: false,
     description: 'The message being sent',
   })
-  @MaxLength(MID_TEXT_LENGTH)
+  @MaxLength(LONG_TEXT_LENGTH)
   message!: string;
 }
