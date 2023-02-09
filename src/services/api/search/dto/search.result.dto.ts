1815573d564ebb7fe7feed66fcdf6e297c0f7a6b
Neil Smyth <neil@thesmyths.eu>
2023-02-09 21:23:29 +0100

    split results into multiple sets, and added a limit on each set re the amount of results returned; added information re total number of matches

---
+++ src/services/api/search/dto/search.result.dto.ts
@@ -0,0 +1,51 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { ISearchResult } from './search.result.entry.interface';
+
+@ObjectType()
+export abstract class ISearchResults {
+  @Field(() => [ISearchResult], {
+    nullable: false,
+    description: 'The search results for contributors (Users, Organizations).',
+  })
+  contributorResults!: ISearchResult[];
+
+  @Field(() => Number, {
+    nullable: false,
+    description:
+      'The total number of search results for contributors (Users, Organizations).',
+  })
+  contributorResultsCount!: number;
+
+  @Field(() => [ISearchResult], {
+    nullable: false,
+    description:
+      'The search results for contributions (Cards, Whiteboards etc).',
+  })
+  contributionResults!: ISearchResult[];
+
+  @Field(() => Number, {
+    nullable: false,
+    description:
+      'The total number of search results for contributions (Cards, Whiteboards etc).',
+  })
+  contributionResultsCount!: number;
+
+  @Field(() => [ISearchResult], {
+    nullable: false,
+    description: 'The search results for Hubs / Challenges / Opportunities.',
+  })
+  journeyResults!: ISearchResult[];
+
+  @Field(() => Number, {
+    nullable: false,
+    description:
+      'The total number of results for Hubs / Challenges / Opportunities.',
+  })
+  journeyResultsCount!: number;
+
+  @Field(() => [ISearchResult], {
+    nullable: false,
+    description: 'The search results for Groups.',
+  })
+  groupResults!: ISearchResult[];
+}
