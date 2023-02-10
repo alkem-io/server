2b5d18c5717a4485ef0cc15e2110068e17913cef
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2023-02-10 20:14:39 +0100

    Use regex for mention matching

--- src/domain/communication/messaging/messaging.service.ts
+++ src/domain/communication/messaging/messaging.service.ts
@@ -1,4 +1,4 @@
-import { ConfigurationTypes, UserPreferenceType } from '@common/enums';
+import { UserPreferenceType } from '@common/enums';
 import { IPreferenceSet } from '@domain/common/preference-set/preference.set.interface';
 import { PreferenceSetService } from '@domain/common/preference-set/preference.set.service';
 import { Injectable } from '@nestjs/common';
@@ -22,62 +22,24 @@ export class MessagingService {
   }
 
   public async getMentionsFromText(text: string): Promise<Mention[]> {
-    const endpoint = this.configService.get(
-      ConfigurationTypes.HOSTING
-    )?.endpoint_cluster;
-
-    const userSearchStr = `(${endpoint}/user/`;
-    const orgSearchStr = `(${endpoint}/organization/`;
-
+    const mentionRegex =
+      /\[@[^\]]*\]\(http:\/\/[^\)]*\/(user|organization)([^\)]+)\)/gm;
     const result: Mention[] = [];
-
-    if (text.length === 0) return result;
-
-    let userSearchIndex = 0;
-    let orgSearchIndex = 0;
-    let userMatchIndex;
-    let orgMatchIndex;
-    let urlEndIndex;
-    let nameId;
-
-    userMatchIndex = text.indexOf(userSearchStr, userSearchIndex);
-    orgMatchIndex = text.indexOf(orgSearchStr, orgSearchIndex);
-    while (userSearchIndex < text.length || orgSearchIndex < text.length) {
-      if (userMatchIndex === -1 && orgMatchIndex === -1) break;
-
-      if (userMatchIndex > -1) {
-        urlEndIndex = text.indexOf(')', userMatchIndex);
-        if (urlEndIndex === -1) {
-          userSearchIndex = userMatchIndex + 1;
-          userMatchIndex = text.indexOf(userSearchStr, userSearchIndex);
-          continue;
-        }
-        nameId = text.substring(
-          userMatchIndex + userSearchStr.length,
-          urlEndIndex
-        );
-        result.push({ userType: MentionedEntityType.USER, nameId });
-        userSearchIndex = urlEndIndex;
-        userMatchIndex = text.indexOf(userSearchStr, userSearchIndex);
+    const mentions = [...text.matchAll(mentionRegex)];
+    for (const mention of mentions) {
+      if (mention[1] === MentionedEntityType.USER) {
+        result.push({
+          nameId: mention[2].substring(1),
+          userType: MentionedEntityType.USER,
+        });
       }
-
-      if (orgMatchIndex > -1) {
-        urlEndIndex = text.indexOf(')', orgMatchIndex);
-        if (urlEndIndex === -1) {
-          orgSearchIndex = orgMatchIndex + 1;
-          orgMatchIndex = text.indexOf(orgSearchStr, orgSearchIndex);
-          continue;
-        }
-        nameId = text.substring(
-          orgMatchIndex + orgSearchStr.length,
-          urlEndIndex
-        );
-        result.push({ userType: MentionedEntityType.ORGANIZATION, nameId });
-        orgSearchIndex = urlEndIndex;
-        orgMatchIndex = text.indexOf(orgSearchStr, orgSearchIndex);
+      if (mention[1] === MentionedEntityType.ORGANIZATION) {
+        result.push({
+          nameId: mention[2].substring(1),
+          userType: MentionedEntityType.ORGANIZATION,
+        });
       }
     }
-
     return result;
   }
 }
