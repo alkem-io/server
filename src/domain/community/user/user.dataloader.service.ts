432287a424cf14e799fdd41e8543239b08ab6411
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-03-14 17:51:23 +0200

    Added an import

--- src/domain/community/user/user.dataloader.service.ts
+++ src/domain/community/user/user.dataloader.service.ts
@@ -2,7 +2,7 @@ import { LogContext } from '@common/enums';
 import { EntityNotFoundException } from '@common/exceptions';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { IProfile } from '../profile';
 import { User } from './user.entity';
 import { IAgent } from '@src/domain';
