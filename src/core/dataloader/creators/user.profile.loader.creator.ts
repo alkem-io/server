313428ff4a5cb48273cafa2401aae81d8caafe9e
svetoslav <svetoslav@alkem.io>
2023-03-15 15:37:14 +0200

    some changes to how is the query constructed and the options provided

--- src/core/dataloader/creators/user.profile.loader.creator.ts
+++ src/core/dataloader/creators/user.profile.loader.creator.ts
@@ -1,10 +1,10 @@
 import DataLoader from 'dataloader';
-import { Repository } from 'typeorm';
+import { FindOptionsSelect, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { IProfile, User } from '@src/domain';
 import { DataLoaderCreator, DataLoaderCreatorOptions } from './base';
-import { findByBatchIds } from '../utils/findByBatchIds';
+import { findByBatchIdsNew } from '../utils/findByBatchIds';
 
 @Injectable()
 export class UserProfileLoaderCreator implements DataLoaderCreator<IProfile> {
@@ -14,13 +14,32 @@ export class UserProfileLoaderCreator implements DataLoaderCreator<IProfile> {
   ) {}
 
   create(options?: DataLoaderCreatorOptions<IProfile>) {
+    // todo: use this approach in https://app.zenhub.com/workspaces/alkemio-development-5ecb98b262ebd9f4aec4194c/issues/gh/alkem-io/server/2598
+    const { fields = [], ...restOptions } = options ?? {};
+
+    const a = fields.reduce<FindOptionsSelect<IProfile>>(
+      (acc, val) => ({
+        ...acc,
+        [val]: true,
+      }),
+      {}
+    );
+
     return new DataLoader<string, IProfile>(
       keys =>
-        findByBatchIds<User, IProfile>(
+        findByBatchIdsNew<User, IProfile>(
           this.userRepository,
           keys as string[],
-          'profile',
-          options
+          {
+            profile: true,
+          },
+          {
+            ...restOptions,
+            fields: {
+              id: true,
+              profile: a,
+            },
+          }
         ),
       {
         cache: options?.cache,
--- src/core/dataloader/utils/findByBatchIds.ts
+++ src/core/dataloader/utils/findByBatchIds.ts
@@ -1,4 +1,10 @@
-import { Repository } from 'typeorm';
+import {
+  FindOptionsRelations,
+  FindOptionsSelect,
+  FindOptionsWhere,
+  In,
+  Repository,
+} from 'typeorm';
 import { EntityNotFoundException } from '@common/exceptions';
 import { LogContext } from '@common/enums';
 import { EntityRelations } from '@src/types';
@@ -12,7 +18,7 @@ export const findByBatchIds = async <
 >(
   repo: Repository<TParent>,
   ids: string[],
-  relation: EntityRelations<TParent>,
+  relations: EntityRelations<TParent> | string[],
   options?: {
     // todo make it use DataLoaderCreatorOptions
     fields?: (keyof TResult)[];
@@ -24,9 +30,17 @@ export const findByBatchIds = async <
     return [];
   }
 
+  const relation = Array.isArray(relations) ? relations[0] : relations;
+
   const { fields, limit } = options ?? {};
   // todo make alias based on TResult name
   const qb = repo.createQueryBuilder(parentAlias).whereInIds(ids);
+
+  if (Array.isArray(relations)) {
+    relations.forEach(relation =>
+      qb.leftJoin(`${parentAlias}.${relation}`, resultAlias)
+    );
+  }
 
   if (fields && fields.length) {
     qb.leftJoin(`${parentAlias}.${relation}`, resultAlias)
@@ -50,8 +64,62 @@ export const findByBatchIds = async <
     id =>
       resultsById.get(id) ??
       new EntityNotFoundException(
-        `Could not load user ${id}`,
-        LogContext.COMMUNITY
+        `Could not load relation '${relations}' for '${id}'`,
+        LogContext.DATA_LOADER
+      )
+  );
+};
+
+export const findByBatchIdsNew = async <
+  TParent extends { id: string } & { [key: string]: any }, // todo better type
+  TResult
+>(
+  repo: Repository<TParent>,
+  ids: string[],
+  relations: FindOptionsRelations<TParent>,
+  options?: {
+    // todo make it use DataLoaderCreatorOptions
+    fields?: FindOptionsSelect<TParent>;
+    limit?: number;
+    shuffle?: boolean;
+  }
+): Promise<(TResult | Error)[] | never> => {
+  if (!ids.length) {
+    return [];
+  }
+
+  const relationKeys = Object.keys(relations);
+
+  if (relationKeys.length > 1) {
+    throw new Error(
+      `'relations' support only on top level relation, '${relationKeys}' found instead`
+    );
+  }
+
+  const { fields, limit } = options ?? {};
+
+  const results = await repo.find({
+    take: limit,
+    where: {
+      id: In(ids),
+    } as FindOptionsWhere<TParent>,
+    relations: relations,
+    select: fields,
+    // relationLoadStrategy
+  });
+
+  const [topLevelRelation] = relationKeys;
+
+  const resultsById = new Map<string, TResult>(
+    results.map<[string, TResult]>(x => [x.id, x[topLevelRelation]])
+  );
+  // ensure the result length matches the input length
+  return ids.map(
+    id =>
+      resultsById.get(id) ??
+      new EntityNotFoundException(
+        `Could not load relation '${topLevelRelation}' for '${id}'`,
+        LogContext.DATA_LOADER
       )
   );
 };
