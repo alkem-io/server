3bef0bdddb609fb86fe26facd317d65db20275b7
svetoslav <svetoslav@alkem.io>
2023-03-27 09:48:04 +0300

    poc getting a header

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -2,3 +2,4 @@ export * from './current-user.decorator';
 export * from './profiling.decorator';
 export * from './authorization.agent.privilege';
 export * from './typed.subscription';
+export * from './headers.decorator';
