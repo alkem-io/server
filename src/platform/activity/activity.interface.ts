60679929d1fb349167d834090f62e1bb80546fe5
Svetoslav Petkov <svetoslav@alkem.io>
2023-04-21 14:11:29 +0300

    Activity query and subscription filterable by type (#2742)
    * activity query to be filterable by type
    
    * subscription to be filterable by type

--- src/platform/activity/activity.interface.ts
+++ src/platform/activity/activity.interface.ts
@@ -48,7 +48,7 @@ export abstract class IActivity extends IBaseAlkemio {
     nullable: false,
     description: 'The event type for this Activity.',
   })
-  type!: string;
+  type!: ActivityEventType;
 
   messageID?: string;
   visibility!: boolean;
