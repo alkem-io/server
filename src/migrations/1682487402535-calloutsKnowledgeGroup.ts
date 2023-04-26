7ff4840aa698f11d7e4620e725532416876e9bb1
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2023-04-26 14:08:25 +0200

    Update migration.

--- src/migrations/1682487402535-calloutsKnowledgeGroup.ts
+++ src/migrations/1682487402535-calloutsKnowledgeGroup.ts
@@ -2,25 +2,14 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class calloutsKnowledgeGroup1682487402535 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const callouts: { id: string }[] = await queryRunner.query(
-      `SELECT id FROM callout WHERE (callout.group IS NULL OR callout.group = '')`
+    await queryRunner.query(
+      `UPDATE callout SET callout.group = 'KNOWLEDGE' WHERE (callout.group = '' OR callout.group IS NULL)`
     );
-    for (const callout of callouts) {
-      await queryRunner.query(
-        `UPDATE callout SET callout.group = 'KNOWLEDGE' WHERE id = '${callout.id}'`
-      );
-    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    const callouts: { id: string }[] = await queryRunner.query(
-      `SELECT id FROM callout WHERE callout.group = 'KNOWLEDGE'`
+    await queryRunner.query(
+      `UPDATE callout SET callout.group = NULL WHERE callout.group = 'KNOWLEDGE'`
     );
-
-    for (const callout of callouts) {
-      await queryRunner.query(
-        `UPDATE callout SET callout.group = NULL WHERE id = '${callout.id}'`
-      );
-    }
   }
 }
