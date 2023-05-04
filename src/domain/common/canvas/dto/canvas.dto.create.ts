dd797b2ee927bce7d904b249444570aa2441e587
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2023-05-04 11:33:10 +0200

    Update create canvas dto.

--- src/domain/common/canvas/dto/canvas.dto.create.ts
+++ src/domain/common/canvas/dto/canvas.dto.create.ts
@@ -1,9 +1,14 @@
+import { CANVAS_VALUE_LENGTH } from '@common/constants';
 import { CreateNameableInput } from '@domain/common/entity/nameable-entity/dto/nameable.dto.create';
 import { NameID } from '@domain/common/scalars/scalar.nameid';
 import { Field, InputType } from '@nestjs/graphql';
+import { IsOptional, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreateCanvasInput extends CreateNameableInput {
+  @Field(() => String, { nullable: true })
+  @IsOptional()
+  @MaxLength(CANVAS_VALUE_LENGTH)
   value?: string;
 
   // Override
