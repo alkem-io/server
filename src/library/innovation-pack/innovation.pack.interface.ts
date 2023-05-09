f47760b73298f9b09355aee9c177e1ca06a480e3
Andrew Pazniak <andrew@yess.llc>
2023-05-09 20:18:38 +0200

    InnovationPack ObjectType typo fixed

--- src/library/innovation-pack/innovation.pack.interface.ts
+++ src/library/innovation-pack/innovation.pack.interface.ts
@@ -2,7 +2,7 @@ import { ObjectType } from '@nestjs/graphql';
 import { ITemplatesSet } from '@domain/template/templates-set';
 import { INameable } from '@domain/common/entity/nameable-entity/nameable.interface';
 
-@ObjectType('InnovatonPack')
+@ObjectType('InnovationPack')
 export abstract class IInnovationPack extends INameable {
   templatesSet?: ITemplatesSet;
 }
