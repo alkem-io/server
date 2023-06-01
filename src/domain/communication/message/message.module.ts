3416ec4ca2d4bdf392463bc2bf296a7cb68de8f2
Neil Smyth <neil@thesmyths.eu>
2023-06-01 22:09:24 +0200

    moved new mutations to room module

--- src/domain/communication/message/message.module.ts
+++ src/domain/communication/message/message.module.ts
@@ -2,7 +2,6 @@ import { UserModule } from '@domain/community/user/user.module';
 import { Module } from '@nestjs/common';
 import { MessageResolverFields } from './message.resolver.fields';
 import { MessageReactionModule } from '../message.reaction/message.reaction.module';
-import { MessageResolverMutations } from './message.resolver.mutations';
 import { EntityResolverModule } from '@services/infrastructure/entity-resolver/entity.resolver.module';
 import { AuthorizationModule } from '@core/authorization/authorization.module';
 import { CommunicationAdapterModule } from '@services/adapters/communication-adapter/communication-adapter.module';
@@ -16,7 +15,7 @@ import { MessageService } from './message.service';
     AuthorizationModule,
     CommunicationAdapterModule,
   ],
-  providers: [MessageService, MessageResolverFields, MessageResolverMutations],
-  exports: [MessageService, MessageResolverFields, MessageResolverMutations],
+  providers: [MessageService, MessageResolverFields],
+  exports: [MessageService, MessageResolverFields],
 })
 export class MessageModule {}
