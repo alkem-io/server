93f16edf9283b5da2f5f21ba93d32617b77c0d52
Neil Smyth <neil@thesmyths.eu>
2023-06-01 21:44:55 +0200

    extended up, added down migration

--- src/migrations/1685643382237-room.ts
+++ src/migrations/1685643382237-room.ts
@@ -44,15 +44,86 @@ export class room1685643382237 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE \`room\` ADD CONSTRAINT \`FK_77775901817dd09d5906537e088\` FOREIGN KEY (\`authorizationId\`) REFERENCES \`authorization_policy\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
     );
+
+    // Set the type properly
+    const rooms: { id: string }[] = await queryRunner.query(
+      `SELECT id FROM room`
+    );
+    for (const room of rooms) {
+      const callouts: { id: string; commentsId: string }[] =
+        await queryRunner.query(
+          `SELECT id, commentsId FROM callout WHERE (commentsId = '${room.id}')`
+        );
+      if (callouts.length > 0) {
+        await queryRunner.query(
+          `UPDATE \`room\` SET type='callout' WHERE id='${room.id}'`
+        );
+        continue;
+      }
+
+      const aspects: { id: string; commentsId: string }[] =
+        await queryRunner.query(
+          `SELECT id, commentsId FROM aspect WHERE (commentsId = '${room.id}')`
+        );
+      if (aspects.length > 0) {
+        await queryRunner.query(
+          `UPDATE \`room\` SET type='post' WHERE id='${room.id}'`
+        );
+        continue;
+      }
+
+      const events: { id: string; commentsId: string }[] =
+        await queryRunner.query(
+          `SELECT id, commentsId FROM calendar_event WHERE (commentsId = '${room.id}')`
+        );
+      if (aspects.length > 0) {
+        await queryRunner.query(
+          `UPDATE \`room\` SET type='calendar_event' WHERE id='${room.id}'`
+        );
+        continue;
+      }
+      console.log('Unable to idenfity type for Room');
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    // Aspect ==> comments
     await queryRunner.query(
-      `ALTER TABLE \`room\` DROP FOREIGN KEY \`FK_d1d94dd8e0c417b4188a05ccbca\``
+      `ALTER TABLE \`aspect\` DROP FOREIGN KEY \`FK_c4fb636888fc391cf1d7406e891\``
+    );
+    // CalendarEvent ==> comments
+    await queryRunner.query(
+      `ALTER TABLE \`calendar_event\` DROP FOREIGN KEY \`FK_157de0ce487e25bb69437e80b13\``
+    );
+    // Callout ==> comments
+    await queryRunner.query(
+      `ALTER TABLE \`callout\` DROP FOREIGN KEY \`FK_62ed316cda7b75735b20307b47e\``
+    );
+    // Comments ==> Authorization
+    await queryRunner.query(
+      `ALTER TABLE \`room\` DROP FOREIGN KEY \`FK_77775901817dd09d5906537e088\` `
+    );
+
+    await queryRunner.query('ALTER TABLE room RENAME TO comments');
+    await queryRunner.query('ALTER TABLE `comments` DROP COLUMN `type`');
+    await queryRunner.query(
+      `ALTER TABLE \`comments\` RENAME COLUMN \`externalRoomID\` TO \`communicationRoomID\``
     );
     await queryRunner.query(
-      `DROP INDEX \`REL_d1d94dd8e0c417b4188a05ccbc\` ON \`room\``
+      `ALTER TABLE \`comments\` RENAME COLUMN \`messagesCount\` TO \`commentsCount\``
     );
-    await queryRunner.query(`DROP TABLE \`room\``);
+
+    await queryRunner.query(
+      `ALTER TABLE \`aspect\` ADD CONSTRAINT \`FK_c4fb636888fc391cf1d7406e891\` FOREIGN KEY (\`commentsId\`) REFERENCES \`comments\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`calendar_event\` ADD CONSTRAINT \`FK_157de0ce487e25bb69437e80b13\` FOREIGN KEY (\`commentsId\`) REFERENCES \`comments\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`callout\` ADD CONSTRAINT \`FK_62ed316cda7b75735b20307b47e\` FOREIGN KEY (\`commentsId\`) REFERENCES \`comments\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`comments\` ADD CONSTRAINT \`FK_77775901817dd09d5906537e088\` FOREIGN KEY (\`authorizationId\`) REFERENCES \`authorization_policy\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
   }
 }
