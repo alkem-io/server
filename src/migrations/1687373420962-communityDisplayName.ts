daba5fbc56c1a64429642f0121252499f2777012
Neil Smyth <neil@thesmyths.eu>
2023-06-23 06:39:17 +0200

    down migration to put back the displayName on community

--- src/migrations/1687373420962-communityDisplayName.ts
+++ src/migrations/1687373420962-communityDisplayName.ts
@@ -13,6 +13,29 @@ export class communityDisplayName1687373420962 implements MigrationInterface {
     await queryRunner.query(
       'ALTER TABLE `community` ADD `displayName` varchar(255) NULL'
     );
-    // Todo: add back in the actual value via lookup for profile to use for a community?
+    // add back in the actual value via lookup for profile to use for a community
+    // Update existing community entries to set the type properly
+    const communities: { id: string; type: string }[] = await queryRunner.query(
+      `SELECT id, type FROM community`
+    );
+    for (const community of communities) {
+      const journeys: { id: string; profileId: string }[] =
+        await queryRunner.query(
+          `SELECT id, profileId FROM ${community.type}  WHERE communityId='${community.id}'`
+        );
+      if (journeys.length !== 1) continue;
+      const journey = journeys[0];
+      const profiles: { id: string; displayName: string }[] =
+        await queryRunner.query(
+          `SELECT id, displayName FROM profile  WHERE id='${journey.profileId}'`
+        );
+
+      if (profiles.length !== 1) continue;
+      const displayName = profiles[0].displayName;
+
+      await queryRunner.query(
+        `UPDATE community SET displayName='${displayName}' WHERE id='${community.id}'`
+      );
+    }
   }
 }
