73eeb360f1235dbc6f7aec0b00cfa51ea684a52c
Neil Smyth <neil@thesmyths.eu>
2023-06-25 09:49:20 +0200

    modified community mutations to take a role as part of the input

--- src/domain/community/community/dto/community.dto.remove.organization.role.ts
+++ src/domain/community/community/dto/community.dto.remove.organization.role.ts
@@ -1,11 +1,15 @@
+import { CommunityRole } from '@common/enums/community.role';
 import { UUID, UUID_NAMEID } from '@domain/common/scalars';
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class RemoveCommunityMemberOrganizationInput {
+export class RemoveCommunityOrganizationRoleInput {
   @Field(() => UUID, { nullable: false })
   communityID!: string;
 
   @Field(() => UUID_NAMEID, { nullable: false })
   organizationID!: string;
+
+  @Field(() => CommunityRole, { nullable: false })
+  role!: CommunityRole;
 }
--- src/domain/community/community/index.ts
+++ src/domain/community/community/index.ts
@@ -1,4 +1,4 @@
 export * from './community.entity';
 export * from './community.interface';
-export * from './dto/community.dto.assign.member.user';
-export * from './dto/community.dto.remove.member.user';
+export * from './dto/community.dto.assign.user.role';
+export * from './dto/community.dto.remove.user.role';
