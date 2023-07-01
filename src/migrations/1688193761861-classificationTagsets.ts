f1b07bba74673bc04b63987ec1c80da9f13020ac
Neil Smyth <neil@thesmyths.eu>
2023-07-01 11:49:05 +0200

    empty migration

---
+++ src/migrations/1688193761861-classificationTagsets.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class classificationTagsets1688193761861 implements MigrationInterface {
+  name = 'classificationTagsets1688193761861';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
