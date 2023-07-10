c92981113d3dae81bc6d3360b282a3126df7b738
Neil Smyth <neil@thesmyths.eu>
2023-07-10 07:02:56 +0200

    migration to rename states tagset to flow-state tagset

--- src/common/enums/tagset.reserved.name.ts
+++ src/common/enums/tagset.reserved.name.ts
@@ -4,7 +4,7 @@ export enum TagsetReservedName {
   DEFAULT = 'default',
   DISPLAY_LOCATION_SPACE = 'display-location-space',
   DISPLAY_LOCATION_CHALLENGE = 'display-location-challenge',
-  FLOW_STATE = 'states',
+  FLOW_STATE = 'flow-state',
   SKILLS = 'skills',
   CAPABILITIES = 'capabilities',
   KEYWORDS = 'keywords',
