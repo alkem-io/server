9c8ab1d7dd063a1a76308be20f47e3e32805ed98
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-07-12 10:40:30 +0300

    Updated migration to include tagset_template allowedValues

--- src/migrations/1689087486575-tagset-names.ts
+++ src/migrations/1689087486575-tagset-names.ts
@@ -2,27 +2,40 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class tagsetNames1689087486575 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // Get all records from 'tagset'
     const tagsets = await queryRunner.query(`SELECT * FROM tagset`);
 
-    // Iterate through each record
     for (const tagset of tagsets) {
-      // Check if 'tags' field is a JSON array
       let tagsString;
       if (this.isJsonArray(tagset.tags)) {
-        // If it is a JSON array, parse it and convert to comma-separated string
         const tagsArray = JSON.parse(tagset.tags);
         tagsString = tagsArray.join(',');
       } else {
-        // If it is not a JSON array, use it as it is
         tagsString = tagset.tags;
       }
 
-      // Update the 'tags' field with new value
       await queryRunner.query(`UPDATE tagset SET tags = ? WHERE id = ?`, [
         tagsString,
         tagset.id,
       ]);
+    }
+
+    const tagsetTemplates = await queryRunner.query(
+      `SELECT * FROM tagset_template`
+    );
+
+    for (const tagsetTemplate of tagsetTemplates) {
+      let tagsetTemplateString;
+      if (this.isJsonArray(tagsetTemplate.allowedValues)) {
+        const tagsetTemplateArray = JSON.parse(tagsetTemplate.allowedValues);
+        tagsetTemplateString = tagsetTemplateArray.join(',');
+      } else {
+        tagsetTemplateString = tagsetTemplate.allowedValues;
+      }
+
+      await queryRunner.query(
+        `UPDATE tagset_template SET allowedValues = ? WHERE id = ?`,
+        [tagsetTemplateString, tagsetTemplate.id]
+      );
     }
   }
 
