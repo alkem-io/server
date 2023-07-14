f85f5a969e7970c3b6d3df12edc91ab7be8107bb
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-07-14 18:36:24 +0300

    Tagset Entity - Cleanup + Additional data

--- src/domain/common/tagset/tagset.entity.ts
+++ src/domain/common/tagset/tagset.entity.ts
@@ -2,26 +2,10 @@ import { Column, Entity, ManyToOne } from 'typeorm';
 import { ITagset } from '@domain/common/tagset/tagset.interface';
 import { Profile } from '@domain/common/profile/profile.entity';
 import { AuthorizableEntity } from '@domain/common/entity/authorizable-entity';
-import { TagsetType } from '@common/enums/tagset.type';
 import { TagsetTemplate } from '../tagset-template';
-import { TagsetReservedName } from '@common/enums/tagset.reserved.name';
 
 @Entity()
 export class Tagset extends AuthorizableEntity implements ITagset {
-  @Column('varchar', {
-    default: TagsetReservedName.DEFAULT,
-    length: 255,
-    nullable: false,
-  })
-  name!: string;
-
-  @Column('varchar', {
-    default: TagsetType.FREEFORM,
-    length: 255,
-    nullable: false,
-  })
-  type!: TagsetType;
-
   @Column('simple-array')
   tags!: string[];
 
