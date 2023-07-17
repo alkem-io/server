2148cb5c320efb632948be1408ecf8c0f9286263
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-07-17 16:58:24 +0300

    Syncing flow-state classification tagsets with innovation flows (#3025)
    * Sync flow states
    
    * Revert "Sync flow states"
    
    This reverts commit 9563c99054b7278215e2d5eca1541dc1c021a802.
    
    * Added updating of tagsets when tagsetTemplate is changed
    
    * Opportunity Display Location
    
    * Tagset Entity - Cleanup + Additional data
    
    * Reverted tagset schema changes
    
    * Added missed changes from the revert
    
    * Added state syncing on state transition
    
    * Fixed DEFAULT tag lookup
    
    * Removed redundant check
    
    * reviewer's suggestions

--- src/common/enums/tagset.reserved.name.ts
+++ src/common/enums/tagset.reserved.name.ts
@@ -4,6 +4,7 @@ export enum TagsetReservedName {
   DEFAULT = 'default',
   DISPLAY_LOCATION_SPACE = 'display-location-space',
   DISPLAY_LOCATION_CHALLENGE = 'display-location-challenge',
+  DISPLAY_LOCATION_OPPORTUNITY = 'display-location-opportunity',
   FLOW_STATE = 'flow-state',
   SKILLS = 'skills',
   CAPABILITIES = 'capabilities',
