5c606c7b13a4edb4552a77222acf8af269956807
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-07-25 11:10:18 +0300

    WIP

--- src/services/adapters/chat-guidance-adapter/chat.guidance.adapter.module.ts
+++ src/services/adapters/chat-guidance-adapter/chat.guidance.adapter.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { ChatGuidanceAdapter } from './chat,guidance.adapter';
+import { ChatGuidanceAdapter } from './chat.guidance.adapter';
 
 @Module({
   imports: [HttpModule],
