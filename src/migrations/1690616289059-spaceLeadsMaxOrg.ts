e04b8a50dbb8a685d2ae73b8f998de06490fe2be
Neil Smyth <neil@thesmyths.eu>
2023-07-29 09:54:17 +0200

    only update space community policies

--- src/migrations/1690616289059-spaceLeadsMaxOrg.ts
+++ src/migrations/1690616289059-spaceLeadsMaxOrg.ts
@@ -5,11 +5,35 @@ export class spaceLeadsMaxOrg1690616289059 implements MigrationInterface {
   name = 'spaceLeadsMaxOrg1690616289059';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const communityPolicies: {
+    const spaces: {
       id: string;
-      lead: string;
-    }[] = await queryRunner.query(`SELECT id, lead FROM community_policy`);
-    for (const communityPolicy of communityPolicies) {
+      communityId: string;
+    }[] = await queryRunner.query(`SELECT id, communityId FROM space`);
+    for (const space of spaces) {
+      const communities: {
+        id: string;
+        policyId: string;
+      }[] = await queryRunner.query(
+        `SELECT id, policyId FROM community WHERE (id = '${space.communityId}')`
+      );
+      if (communities.length !== 1) {
+        console.warn(
+          "Migration 'spaceLeadsMaxOrg1690616289059' found Space whose community could not be found."
+        );
+      }
+      const community = communities[0];
+      const communityPolicies: {
+        id: string;
+        lead: string;
+      }[] = await queryRunner.query(
+        `SELECT id, lead FROM community_policy WHERE (id = '${community.policyId}')`
+      );
+      if (communityPolicies.length !== 1) {
+        console.warn(
+          "Migration 'spaceLeadsMaxOrg1690616289059' found Community whose community policy could not be found."
+        );
+      }
+      const communityPolicy = communityPolicies[0];
       // Update for leads
       const leadRolePolicy: communityRolePolicy = JSON.parse(
         communityPolicy.lead
