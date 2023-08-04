78b1eb79a267045f240c599f810f2d43931f6572
Neil Smyth <30729240+techsmyth@users.noreply.github.com>
2023-08-04 15:15:48 +0200

    extended users query to take an optional paramter IDs, removed usersByIDs query (#3107)
    * extended users query to take an optional paramter IDs, removed usersByIDs query
    
    * Added filtering by IDs as an additional, not OR filter
    
    Improved typing
    
    ---------
    
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

---
+++ src/domain/community/user/dto/users.query.args.ts
@@ -0,0 +1,13 @@
+import { UUID } from '@domain/common/scalars/scalar.uuid';
+import { ContributorQueryArgs } from '@domain/community/contributor/dto/contributor.query.args';
+import { ArgsType, Field } from '@nestjs/graphql';
+
+@ArgsType()
+export class UsersQueryArgs extends ContributorQueryArgs {
+  @Field(() => [UUID], {
+    name: 'IDs',
+    description: 'Retrieve the specified users by ID.',
+    nullable: true,
+  })
+  ids?: string[];
+}
