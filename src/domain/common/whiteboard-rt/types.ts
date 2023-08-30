fd454d70f215ced3a1341768ebc8d0a3b5c2330a
svetoslav <svetoslav@alkem.io>
2023-08-30 17:18:49 +0300

    removed deleted export

--- src/domain/common/whiteboard-rt/types.ts
+++ src/domain/common/whiteboard-rt/types.ts
@@ -1,7 +1,6 @@
 export * from './dto/whiteboard.rt.dto.create';
 export * from './dto/whiteboard.rt.dto.update';
 export * from './dto/whiteboard.rt.dto.update.direct';
-export * from './dto/whiteboard.rt.dto.delete';
 
 export * from './whiteboard.rt.entity';
 export * from './whiteboard.rt.interface';
