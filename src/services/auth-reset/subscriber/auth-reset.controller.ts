ad94c3ae25d36b908577eb2790304058e3712555
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-08-31 16:14:09 +0300

    Addressing PR Comments

--- src/services/auth-reset/subscriber/auth-reset.controller.ts
+++ src/services/auth-reset/subscriber/auth-reset.controller.ts
@@ -10,13 +10,15 @@ import { Channel, Message } from 'amqplib';
 import { SpaceAuthorizationService } from '@domain/challenge/space/space.service.authorization';
 import { SpaceService } from '@domain/challenge/space/space.service';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import { LogContext } from '@common/enums';
+import { LogContext, MessagingQueue } from '@common/enums';
 import { PlatformAuthorizationService } from '@platform/platfrom/platform.service.authorization';
 import { OrganizationService } from '@domain/community/organization/organization.service';
 import { OrganizationAuthorizationService } from '@domain/community/organization/organization.service.authorization';
 import { UserService } from '@domain/community/user/user.service';
 import { UserAuthorizationService } from '@domain/community/user/user.service.authorization';
 
+const MAX_RETRIES = 5;
+const RETRY_HEADER = 'x-retry-count';
 @Controller()
 export class AuthResetController {
   constructor(
@@ -30,6 +32,46 @@ export class AuthResetController {
     private organizationService: OrganizationService,
     private userService: UserService
   ) {}
+  @EventPattern('test-reset', Transport.RMQ)
+  public testReset(
+    @Payload() payload: { id: string },
+    @Ctx() context: RmqContext
+  ) {
+    const channel: Channel = context.getChannelRef();
+    const originalMsg = context.getMessage() as Message;
+
+    const retryCount = originalMsg.properties.headers[RETRY_HEADER] ?? 0;
+
+    try {
+      if (Math.random() < 0.7) {
+        throw new Error('Processing failed');
+      }
+
+      console.log(
+        'Processing succeeded',
+        retryCount
+          ? `after ${retryCount} retr${retryCount > 1 ? 'ies' : 'y'}`
+          : ''
+      );
+
+      channel.ack(originalMsg); // Acknowledge the message
+    } catch (e) {
+      if (retryCount >= MAX_RETRIES) {
+        console.error('Max retries reached. Rejecting message');
+        channel.reject(originalMsg, false); // Reject and don't requeue
+      } else {
+        console.warn(
+          `Processing failed. Retrying (${retryCount + 1}/${MAX_RETRIES})`
+        );
+        channel.publish('', 'auth-reset', originalMsg.content, {
+          headers: { [RETRY_HEADER]: retryCount + 1 },
+          persistent: true, // Make the message durable
+        });
+        channel.ack(originalMsg); // Acknowledge the original message
+      }
+    }
+  }
+
   @EventPattern('space-reset', Transport.RMQ)
   public async authResetSpace(
     @Payload() payload: { id: string },
@@ -42,6 +84,8 @@ export class AuthResetController {
     const channel: Channel = context.getChannelRef();
     const originalMsg = context.getMessage() as Message;
 
+    const retryCount = originalMsg.properties.headers[RETRY_HEADER] ?? 0;
+
     try {
       const space = await this.spaceService.getSpaceOrFail(payload.id);
       await this.spaceAuthorizationService.applyAuthorizationPolicy(space);
@@ -51,11 +95,25 @@ export class AuthResetController {
       );
       channel.ack(originalMsg);
     } catch (error) {
-      this.logger.verbose?.(
-        `Resetting authorization for space with id ${payload.id} failed!`,
-        LogContext.AUTH_POLICY
-      );
-      channel.nack(originalMsg);
+      if (retryCount >= MAX_RETRIES) {
+        this.logger.error(
+          `Resetting authorization for space with id ${payload.id} failed! Max retries reached. Rejecting message.`,
+          LogContext.AUTH
+        );
+        channel.reject(originalMsg, false); // Reject and don't requeue
+      } else {
+        this.logger.warn(
+          `Processing  authorization reset for space with id ${
+            payload.id
+          } failed. Retrying (${retryCount + 1}/${MAX_RETRIES})`,
+          LogContext.AUTH
+        );
+        channel.publish('', MessagingQueue.AUTH_RESET, originalMsg.content, {
+          headers: { [RETRY_HEADER]: retryCount + 1 },
+          persistent: true, // Make the message durable
+        });
+        channel.ack(originalMsg); // Acknowledge the original message
+      }
     }
   }
 
@@ -68,6 +126,8 @@ export class AuthResetController {
     const channel: Channel = context.getChannelRef();
     const originalMsg = context.getMessage() as Message;
 
+    const retryCount = originalMsg.properties.headers[RETRY_HEADER] ?? 0;
+
     try {
       await this.platformAuthorizationService.applyAuthorizationPolicy();
       this.logger.verbose?.(
@@ -76,11 +136,25 @@ export class AuthResetController {
       );
       channel.ack(originalMsg);
     } catch (error) {
-      this.logger.verbose?.(
-        'Resetting authorization for platform failed!',
-        LogContext.AUTH_POLICY
-      );
-      channel.nack(originalMsg);
+      if (retryCount >= MAX_RETRIES) {
+        this.logger.error(
+          'Resetting authorization for platform failed! Max retries reached. Rejecting message.',
+          LogContext.AUTH
+        );
+        channel.reject(originalMsg, false); // Reject and don't requeue
+      } else {
+        this.logger.warn(
+          `Processing  authorization reset for platform failed. Retrying (${
+            retryCount + 1
+          }/${MAX_RETRIES})`,
+          LogContext.AUTH
+        );
+        channel.publish('', MessagingQueue.AUTH_RESET, originalMsg.content, {
+          headers: { [RETRY_HEADER]: retryCount + 1 },
+          persistent: true, // Make the message durable
+        });
+        channel.ack(originalMsg); // Acknowledge the original message
+      }
     }
   }
 
@@ -96,6 +170,8 @@ export class AuthResetController {
     const channel: Channel = context.getChannelRef();
     const originalMsg = context.getMessage() as Message;
 
+    const retryCount = originalMsg.properties.headers[RETRY_HEADER] ?? 0;
+
     try {
       const user = await this.userService.getUserOrFail(payload.id);
       await this.userAuthorizationService.applyAuthorizationPolicy(user);
@@ -105,11 +181,25 @@ export class AuthResetController {
       );
       channel.ack(originalMsg);
     } catch (error) {
-      this.logger.verbose?.(
-        `Resetting authorization for user with id ${payload.id} failed!`,
-        LogContext.AUTH_POLICY
-      );
-      channel.nack(originalMsg);
+      if (retryCount >= MAX_RETRIES) {
+        this.logger.error(
+          `Resetting authorization for user with id ${payload.id} failed! Max retries reached. Rejecting message.`,
+          LogContext.AUTH
+        );
+        channel.reject(originalMsg, false); // Reject and don't requeue
+      } else {
+        this.logger.warn(
+          `Processing  authorization reset for user with id ${
+            payload.id
+          } failed. Retrying (${retryCount + 1}/${MAX_RETRIES})`,
+          LogContext.AUTH
+        );
+        channel.publish('', MessagingQueue.AUTH_RESET, originalMsg.content, {
+          headers: { [RETRY_HEADER]: retryCount + 1 },
+          persistent: true, // Make the message durable
+        });
+        channel.ack(originalMsg); // Acknowledge the original message
+      }
     }
   }
 
@@ -125,6 +215,8 @@ export class AuthResetController {
     const channel: Channel = context.getChannelRef();
     const originalMsg = context.getMessage() as Message;
 
+    const retryCount = originalMsg.properties.headers[RETRY_HEADER] ?? 0;
+
     try {
       const organization = await this.organizationService.getOrganizationOrFail(
         payload.id
@@ -138,11 +230,25 @@ export class AuthResetController {
       );
       channel.ack(originalMsg);
     } catch (error) {
-      this.logger.verbose?.(
-        `Resetting authorization for organization with id ${payload.id} failed!`,
-        LogContext.AUTH_POLICY
-      );
-      channel.nack(originalMsg);
+      if (retryCount >= MAX_RETRIES) {
+        this.logger.error(
+          `Resetting authorization for organization with id ${payload.id} failed! Max retries reached. Rejecting message.`,
+          LogContext.AUTH
+        );
+        channel.reject(originalMsg, false); // Reject and don't requeue
+      } else {
+        this.logger.warn(
+          `Processing  authorization reset for organization with id ${
+            payload.id
+          } failed. Retrying (${retryCount + 1}/${MAX_RETRIES})`,
+          LogContext.AUTH
+        );
+        channel.publish('', MessagingQueue.AUTH_RESET, originalMsg.content, {
+          headers: { [RETRY_HEADER]: retryCount + 1 },
+          persistent: true, // Make the message durable
+        });
+        channel.ack(originalMsg); // Acknowledge the original message
+      }
     }
   }
 }
