6539639f6da5fdebac055da87891ee0d14735e4c
Neil Smyth <neil@thesmyths.eu>
2023-09-07 07:03:28 +0200

    added simple validation to whiteboard content

--- src/domain/common/scalars/scalar.whiteboard.content.ts
+++ src/domain/common/scalars/scalar.whiteboard.content.ts
@@ -1,7 +1,9 @@
 import { LogContext } from '@common/enums';
 import { ValidationException } from '@common/exceptions';
+import { validateExcalidrawContent } from '@core/validation/excalidraw/validateExcalidrawContent';
 import { Scalar, CustomScalar } from '@nestjs/graphql';
 import { Kind, ValueNode } from 'graphql';
+import { ErrorObject } from 'ajv';
 
 const WHITEBOARD_CONTENT_LENGTH = 8388608;
 
@@ -39,8 +41,42 @@ export class WhiteboardContent implements CustomScalar<string, string> {
       );
     }
 
-    // todo: add more validation here...
+    // todo: json validation can be expanded
+    const errors = validateExcalidrawContent(value);
+
+    if (errors) {
+      let message: string;
+      if (Array.isArray(errors)) {
+        const errorTuple = Object.entries(formatErrors(errors));
+        message = errorTuple
+          .map(([path, messages]) => `${path}: ${messages.join(', ')}`)
+          .join('; ');
+      } else {
+        message = errors.message;
+      }
+      throw new ValidationException(
+        `Value is not valid excalidraw content definition: ${message}`,
+        LogContext.API
+      );
+    }
 
     return value;
   };
 }
+
+const formatErrors = (errors: ErrorObject[]) => {
+  const map = errors.map(formatError).reduce((acc, { path, message }) => {
+    const messages = acc.get(path) ?? [];
+    messages.push(message);
+    acc.set(path, messages);
+    return acc;
+  }, new Map<string, Array<string>>());
+  return Object.fromEntries(map);
+};
+
+const formatError = (error: ErrorObject) => {
+  return {
+    path: error.instancePath.substring(1).replace(/\//g, '.'),
+    message: error.message ?? '',
+  };
+};
