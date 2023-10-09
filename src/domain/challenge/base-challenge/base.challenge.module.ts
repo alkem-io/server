48cc29ff0b7879a00044f17a43e39a83bdaa4c6a
Neil Smyth <30729240+techsmyth@users.noreply.github.com>
2023-10-09 17:52:25 +0200

    Journey authorization streamlined (#3272)
    * removed shared base challenge authorization service, made profile + context authorization globally readable, streamlined loading + storing of entities on journeys when doing authorization reset
    
    * Fixes
    
    ---------
    
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/domain/challenge/base-challenge/base.challenge.module.ts
+++ src/domain/challenge/base-challenge/base.challenge.module.ts
@@ -5,7 +5,6 @@ import { Module } from '@nestjs/common';
 import { AuthorizationModule } from '@core/authorization/authorization.module';
 import { NamingModule } from '@services/infrastructure/naming/naming.module';
 import { BaseChallengeService } from './base.challenge.service';
-import { BaseChallengeAuthorizationService } from './base.challenge.service.authorization';
 import { AuthorizationPolicyModule } from '@domain/common/authorization-policy/authorization.policy.module';
 import { CollaborationModule } from '@domain/collaboration/collaboration/collaboration.module';
 import { CommunityPolicyModule } from '@domain/community/community-policy/community.policy.module';
@@ -23,7 +22,7 @@ import { ProfileModule } from '@domain/common/profile/profile.module';
     NamingModule,
     CollaborationModule,
   ],
-  providers: [BaseChallengeService, BaseChallengeAuthorizationService],
-  exports: [BaseChallengeService, BaseChallengeAuthorizationService],
+  providers: [BaseChallengeService],
+  exports: [BaseChallengeService],
 })
 export class BaseChallengeModule {}
