068ed46afe3c490d1cbf45e64acd685fbd59753e
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-10-12 14:43:08 +0300

    Fixed typo

--- src/common/constants/authorization/policy.rule.constants.ts
+++ src/common/constants/authorization/policy.rule.constants.ts
@@ -11,7 +11,7 @@ export const POLICY_RULE_COMMUNICATION_CONTRIBUTE =
 export const POLICY_RULE_COMMUNICATION_CREATE =
   'policyRule-communicationCreate';
 export const POLICY_RULE_PLATFORM_CREATE = 'policyRule-platformCreate';
-export const POLICY_RULE_PLATFORM_DELETE = 'policyRule-platformDreate';
+export const POLICY_RULE_PLATFORM_DELETE = 'policyRule-platformDelete';
 export const POLICY_RULE_CALLOUT_CREATE = 'policyRule-calloutCreate';
 export const POLICY_RULE_CALLOUT_CONTRIBUTE = 'policyRule-calloutContribute';
 export const POLICY_RULE_COLLABORATION_CREATE =
