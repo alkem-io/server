ac7efa8f4fa51594b2e929b55f5ed4f77c1759cb
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-10-27 16:37:28 +0300

    Cleaned up prototype table + service

--- src/services/api/chat-guidance/chat.guidance.module.ts
+++ src/services/api/chat-guidance/chat.guidance.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthorizationModule } from '@core/authorization/authorization.module';
 import { PlatformAuthorizationPolicyModule } from '@platform/authorization/platform.authorization.policy.module';
 import { GuidanceEngineAdapterModule } from '@services/adapters/chat-guidance-adapter/guidance.engine.adapter.module';
-import { ChatGuidanceLogModule } from '@services/api/chat-guidance/chat.guidance.log.module';
 import { ChatGuidanceService } from './chat.guidance.service';
 import { ChatGuidanceResolverQueries } from './chat.guidance.resolver.queries';
 import { ChatGuidanceResolverMutations } from './chat.guidance.resolver.mutations';
+import { GuidanceReporterModule } from '@services/external/elasticsearch/guidance-reporter';
 
 @Module({
   imports: [
     AuthorizationModule,
     PlatformAuthorizationPolicyModule,
     GuidanceEngineAdapterModule,
-    ChatGuidanceLogModule,
+    GuidanceReporterModule,
   ],
   providers: [
     ChatGuidanceService,
