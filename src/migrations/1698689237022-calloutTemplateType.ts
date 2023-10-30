c3b109751e890a57329d335b75d3a5375e733813
Neil Smyth <neil@thesmyths.eu>
2023-10-30 19:17:16 +0100

    added type to callout template

---
+++ src/migrations/1698689237022-calloutTemplateType.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class calloutTemplateType1698689237022 implements MigrationInterface {
+  name = 'calloutTemplateType1698689237022';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`callout_template\` ADD \`type\` varchar(255) NULL`
+    );
+    await queryRunner.query(`UPDATE callout_template SET type = 'post'`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`callout_template\` DROP COLUMN \`type\``
+    );
+  }
+}
