6a1533ca176efccb50dbfa69b8714fba1deb3835
Svetoslav Petkov <svetoslav@alkem.io>
2023-11-03 12:41:56 +0200

    Socket io redis adapter + refactor#3212 (#3406)
    * introduced visitor pattern to excalidraw received events over queue
    
    * improved socket sync
    
    * more docs
    
    * socket.io redis adapter
    
    * socket.io redis adapter
    
    * switch old privilege after a merge
    
    * code styling
    
    * removed some dependencies for amqp impl

---
+++ src/services/excalidraw-pubsub/subscriber/excalidraw.event.factory.ts
@@ -0,0 +1,70 @@
+import {
+  DISCONNECT,
+  DISCONNECTING,
+  ROOM_USER_CHANGE,
+  SERVER_BROADCAST,
+  SERVER_VOLATILE_BROADCAST,
+} from '@services/external/excalidraw-backend/event.names';
+import {
+  BaseEvent,
+  DisconnectedEvent,
+  DisconnectingEvent,
+  RoomUserChangeEvent,
+  ServerBroadcastEvent,
+  ServerVolatileBroadcastEvent,
+} from '../events';
+import {
+  BasePayload,
+  RoomUserChangePayload,
+  ServerBroadcastPayload,
+  ServerVolatileBroadcastPayload,
+} from '../payloads';
+
+export const excalidrawEventFactory = (
+  payload: BasePayload
+): BaseEvent | undefined => {
+  switch (payload.name) {
+    case DISCONNECT:
+      return createDisconnectedEvent(payload);
+    case DISCONNECTING:
+      return createDisconnectingEvent(payload);
+    case ROOM_USER_CHANGE:
+      return createRoomUserChangeEvent(payload);
+    case SERVER_BROADCAST:
+      return createServerBroadcastEvent(payload);
+    case SERVER_VOLATILE_BROADCAST:
+      return createServerVolatileBroadcastEvent(payload);
+    default:
+      return undefined;
+  }
+};
+
+const createDisconnectedEvent = (payload: BasePayload): DisconnectedEvent => {
+  return new DisconnectedEvent(payload.publisherId);
+};
+
+const createDisconnectingEvent = (payload: BasePayload): DisconnectingEvent => {
+  return new DisconnectingEvent(payload.publisherId);
+};
+
+const createRoomUserChangeEvent = (
+  payload: BasePayload
+): RoomUserChangeEvent => {
+  const { roomID, publisherId, socketIDs } = payload as RoomUserChangePayload;
+  return new RoomUserChangeEvent(roomID, socketIDs, publisherId);
+};
+
+const createServerBroadcastEvent = (
+  payload: BasePayload
+): ServerBroadcastEvent => {
+  const { roomID, publisherId, data } = payload as ServerBroadcastPayload;
+  return new ServerBroadcastEvent(roomID, data, publisherId);
+};
+
+const createServerVolatileBroadcastEvent = (
+  payload: BasePayload
+): ServerVolatileBroadcastEvent => {
+  const { roomID, publisherId, data } =
+    payload as ServerVolatileBroadcastPayload;
+  return new ServerVolatileBroadcastEvent(roomID, data, publisherId);
+};
--- src/services/excalidraw-pubsub/subscriber/excalidraw.event.subscriber.service.ts
+++ src/services/excalidraw-pubsub/subscriber/excalidraw.event.subscriber.service.ts
@@ -1,6 +1,9 @@
 import { AMQPPubSub } from 'graphql-amqp-subscriptions';
-import { Inject, Injectable } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { APP_ID, EXCALIDRAW_PUBSUB_PROVIDER } from '@common/constants';
+import { AlkemioErrorStatus, LogContext } from '@common/enums';
+import { BaseException } from '@common/exceptions/base.exception';
 import {
   DISCONNECT,
   DISCONNECTING,
@@ -10,6 +13,8 @@ import {
   SERVER_VOLATILE_BROADCAST,
 } from '@services/external/excalidraw-backend/event.names';
 import { BasePayload } from '../payloads';
+import { BaseEvent } from '../events';
+import { excalidrawEventFactory } from './excalidraw.event.factory';
 
 const subscribableEvents = [
   NEW_USER,
@@ -19,10 +24,17 @@ const subscribableEvents = [
   DISCONNECTING,
   DISCONNECT,
 ];
+// the library used executes Buffer.toJSON
+// on the received payload before passing it down to be consumed
+type BaseReceivedPayload = BasePayload & {
+  data?: ReturnType<Buffer['toJSON']>;
+};
 
 @Injectable()
 export class ExcalidrawEventSubscriberService {
   constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: LoggerService,
     @Inject(EXCALIDRAW_PUBSUB_PROVIDER) private excalidrawPubSub: AMQPPubSub,
     @Inject(APP_ID) private appId: string
   ) {}
@@ -33,25 +45,47 @@ export class ExcalidrawEventSubscriberService {
    * @param next
    * @returns Promise<Array<number>>
    */
-  public async subscribeToAll(next: (payload: BasePayload) => void) {
-    // todo; simplify; breakdown
-    try {
-      const asyncIterator =
-        this.excalidrawPubSub.asyncIterator<any /*BasePayload*/>( // todo
-          subscribableEvents
-        );
-      while (true) {
-        const { done, value } = await asyncIterator.next();
-        if (done) {
-          return;
+  public subscribeToAll(next: (event: BaseEvent) => void) {
+    // wrap in iife to avoid make the main function async
+    (async () => {
+      try {
+        const asyncIterator =
+          this.excalidrawPubSub.asyncIterator<BaseReceivedPayload>(
+            subscribableEvents
+          );
+        while (true) {
+          const { done, value } = await asyncIterator.next();
+          if (done) {
+            return;
+          }
+          if (value.publisherId === this.appId) {
+            // skip the event if it's coming from the same publisher/service instance
+            // events are handled by the ws server in the current instance
+            continue;
+          }
+          const instance = excalidrawEventFactory({
+            ...value,
+            // parse the parsed value back to Buffer - see BaseReceivedPayload
+            data: value.data && Buffer.from(value.data),
+          });
+
+          if (!instance) {
+            this.logger.warn(
+              `Could not instantiate event from payload: ${value}`,
+              LogContext.EXCALIDRAW_SERVER
+            );
+            continue;
+          }
+
+          next(instance);
         }
-        value.data = value?.data && new Uint8Array(value.data.data).buffer;
-        next(value);
+      } catch (e) {
+        throw new BaseException(
+          `Exception while waiting for result: ${(e as Error).message}`,
+          LogContext.EXCALIDRAW_SERVER,
+          AlkemioErrorStatus.EXCALIDRAW_AMQP_RESULT_ERROR
+        );
       }
-    } catch (e) {
-      throw new Error(
-        `Exception while waiting for result: ${(e as Error).message}`
-      );
-    }
+    })();
   }
 }
