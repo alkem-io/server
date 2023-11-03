5e2421c5da3c4ff34537b2d6aaeed916d546b9d4
Neil Smyth <neil@thesmyths.eu>
2023-11-03 20:35:40 +0100

    removed metrics from platform

--- src/platform/metadata/metadata.module.ts
+++ src/platform/metadata/metadata.module.ts
@@ -1,12 +1,8 @@
-import { ChallengeModule } from '@domain/challenge/challenge/challenge.module';
-import { SpaceModule } from '@domain/challenge/space/space.module';
-import { OrganizationModule } from '@domain/community/organization/organization.module';
-import { UserModule } from '@domain/community/user/user.module';
 import { Module } from '@nestjs/common';
 import { MetadataService } from './metadata.service';
 
 @Module({
-  imports: [UserModule, ChallengeModule, SpaceModule, OrganizationModule],
+  imports: [],
   providers: [MetadataService],
   exports: [MetadataService],
 })
