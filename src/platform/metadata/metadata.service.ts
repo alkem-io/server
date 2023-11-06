98a4bc1b70a8e694d3169847aaeb3e39f8f46e06
Neil Smyth <30729240+techsmyth@users.noreply.github.com>
2023-11-06 13:20:42 +0100

    Removed metrics from metadata as no longer used (#3415)
    * removed metrics from platform
    
    * fixed test
    
    ---------
    
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/platform/metadata/metadata.service.ts
+++ src/platform/metadata/metadata.service.ts
@@ -1,8 +1,3 @@
-import { ChallengeService } from '@domain/challenge/challenge/challenge.service';
-import { SpaceService } from '@domain/challenge/space/space.service';
-import { INVP, NVP } from '@domain/common/nvp';
-import { OrganizationService } from '@domain/community/organization/organization.service';
-import { UserService } from '@domain/community/user/user.service';
 import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { IMetadata } from './metadata.interface';
@@ -11,17 +6,12 @@ import { IServiceMetadata } from './service/service.metadata.interface';
 @Injectable()
 export class MetadataService {
   constructor(
-    private userService: UserService,
-    private spaceService: SpaceService,
-    private challengeService: ChallengeService,
-    private organizationService: OrganizationService,
     @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService
   ) {}
 
   async getMetadata(): Promise<IMetadata> {
     return {
       services: await this.getServicesMetadata(),
-      metrics: await this.getMetrics(),
     };
   }
 
@@ -40,38 +30,5 @@ export class MetadataService {
 
   async getVersion(): Promise<string> {
     return process.env.npm_package_version ?? '';
-  }
-
-  async getMetrics(): Promise<INVP[]> {
-    const metrics: INVP[] = [];
-
-    // Challenges
-    const spacesCount = await this.spaceService.getSpaceCount();
-    const spacesTopic = new NVP('spaces', spacesCount.toString());
-    spacesTopic.id = 'spaces';
-    metrics.push(spacesTopic);
-
-    const challengesCount = await this.challengeService.getChallengesCount();
-    const challengesTopic = new NVP('challenges', challengesCount.toString());
-    challengesTopic.id = 'challenges';
-    metrics.push(challengesTopic);
-
-    // Users
-    const usersCount = await this.userService.getUserCount();
-    const usersTopic = new NVP('users', usersCount.toString());
-    usersTopic.id = 'users';
-    metrics.push(usersTopic);
-
-    // Organizations
-    const organizationsCount =
-      await this.organizationService.getOrganizationCount();
-    const organizationsTopic = new NVP(
-      'organizations',
-      organizationsCount.toString()
-    );
-    organizationsTopic.id = 'organizations';
-    metrics.push(organizationsTopic);
-
-    return metrics;
   }
 }
