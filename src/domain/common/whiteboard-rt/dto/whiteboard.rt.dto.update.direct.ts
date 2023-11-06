8b822521005ec54a043ab3b102229f2697307d8d
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-11-06 12:11:40 +0200

    Update / UpdateContent separated for WhiteboardRT

--- src/domain/common/whiteboard-rt/dto/whiteboard.rt.dto.update.direct.ts
+++
@@ -1,9 +0,0 @@
-import { UUID } from '@domain/common/scalars/scalar.uuid';
-import { InputType, Field } from '@nestjs/graphql';
-import { UpdateWhiteboardRtInput } from './whiteboard.rt.dto.update';
-
-@InputType()
-export class UpdateWhiteboardRtDirectInput extends UpdateWhiteboardRtInput {
-  @Field(() => UUID, { nullable: false })
-  ID!: string;
-}
