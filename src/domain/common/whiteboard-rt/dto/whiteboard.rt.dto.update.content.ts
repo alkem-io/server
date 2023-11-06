d4cc771fea402493c9c6fe2a16639c15c96516a6
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-11-06 14:52:11 +0200

    Fix old whiteboardsRT not being accessible by non-owners (#3413)
    * Fixed owner contributors content policy
    
    * Refactored license auth propagation
    
    * Update / UpdateContent separated for WhiteboardRT
    
    ---------
    
    Co-authored-by: Carlos Cano <carlos@alkem.io>

---
+++ src/domain/common/whiteboard-rt/dto/whiteboard.rt.dto.update.content.ts
@@ -0,0 +1,11 @@
+import { UpdateNameableInput } from '@domain/common/entity/nameable-entity/dto/nameable.dto.update';
+import { WhiteboardContent } from '@domain/common/scalars/scalar.whiteboard.content';
+import { InputType, Field } from '@nestjs/graphql';
+import { IsOptional } from 'class-validator';
+
+@InputType()
+export class UpdateWhiteboardContentRtInput extends UpdateNameableInput {
+  @Field(() => WhiteboardContent, { nullable: true })
+  @IsOptional()
+  content?: string;
+}
