72a1d4891fcf16864b1b112ae44f6585295adbd2
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-11-16 15:02:19 +0200

    Upload on reference fix (#3460)
    * Fixed upload on reference
    
    * PR feedback fix
    
    * Visual upload fix

--- src/domain/storage/storage-bucket/storage.bucket.service.ts
+++ src/domain/storage/storage-bucket/storage.bucket.service.ts
@@ -247,8 +247,11 @@ export class StorageBucketService {
         mimetype,
         userID
       );
-      // Delete the old document, if any
-      if (documentForReference) {
+      // Delete the old document, if any. Do not delete the same doc.
+      if (
+        documentForReference &&
+        newDocument.externalID != documentForReference?.externalID
+      ) {
         await this.documentService.deleteDocument({
           ID: documentForReference.id,
         });
@@ -297,7 +300,10 @@ export class StorageBucketService {
         true
       );
       // Delete the old document
-      if (documentForVisual) {
+      if (
+        documentForVisual &&
+        newDocument.externalID != documentForVisual?.externalID
+      ) {
         await this.documentService.deleteDocument({
           ID: documentForVisual.id,
         });
