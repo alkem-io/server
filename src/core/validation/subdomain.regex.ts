1371da5a41102375a574e4b5c9fd39c90f162387
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2023-11-24 15:23:32 +0200

    Fixed wrong domain regex for InnovationHubs

--- src/core/validation/subdomain.regex.ts
+++ src/core/validation/subdomain.regex.ts
@@ -1,3 +1,4 @@
 // https://www.rfc-editor.org/rfc/rfc1034#section-3.5
 export const SUBDOMAIN_PATTERN = '[a-z][a-z0-9\\-]{0,61}[a-z0-9]';
+export const DOMAIN_PATTERN = '[a-zA-Z0-9\\-_]+';
 export const SUBDOMAIN_REGEX = new RegExp(`^${SUBDOMAIN_PATTERN}$`);
