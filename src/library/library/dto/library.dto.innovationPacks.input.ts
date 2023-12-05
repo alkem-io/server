0043fed0ee896d18aeef1ca989e48908649b85ed
Carlos Cano <carlos@alkem.io>
2023-12-05 17:52:05 +0100

    Add limit and order to innovationPacks query

---
+++ src/library/library/dto/library.dto.innovationPacks.input.ts
@@ -0,0 +1,19 @@
+import { Field, Float, InputType } from '@nestjs/graphql';
+import { InnovationPacksOrderBy } from '@common/enums/innovation.packs.orderBy';
+
+@InputType()
+export class InnovationPacksInput {
+  @Field(() => Float, {
+    nullable: true,
+    description:
+      'The number of Discussion entries to return; if omitted return all InnovationPacks.',
+  })
+  limit?: number;
+
+  @Field(() => InnovationPacksOrderBy, {
+    description:
+      'The sort order of the InnovationPacks to return. Defaults to number of templates ASC',
+    nullable: true,
+  })
+  orderBy?: InnovationPacksOrderBy;
+}
