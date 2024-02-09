9aa89d1669562d690e3c9defbc4834ae2641f5ee
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2024-02-09 09:49:01 +0200

    Mariadb --> MySQL fixes

--- .scripts/migrations/restore_snapshot.sh
+++ .scripts/migrations/restore_snapshot.sh
@@ -10,4 +10,4 @@ BASE_DIR="$(dirname "$(realpath "$0")")"
 . "$BASE_DIR/.env"
 
 # Restore snapshot using the mariadb docker container
-docker exec -i alkemio_dev_mariadb /usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < "$BASE_DIR/$ENV"
+docker exec -i alkemio_dev_mysql /usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < "$BASE_DIR/$ENV"
