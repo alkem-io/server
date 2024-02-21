a237befa8b109539113912100c407757809455bd
svetoslav <svetoslav@alkem.io>
2024-02-21 15:50:50 +0200

    contribution policy of all wb set to 'admins'

--- src/migrations/1708354354175-whiteboardToRt.ts
+++ src/migrations/1708354354175-whiteboardToRt.ts
@@ -95,6 +95,10 @@ export class whiteboardToRt1708354354175 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE \`callout_framing\` ADD CONSTRAINT \`FK_8bc0e1f40be5816d3a593cbf7fa\` FOREIGN KEY (\`whiteboardId\`) REFERENCES \`whiteboard\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
     );
+    // enforce the contribution policy to OWNER for all whiteboards that are responses to a callout
+    await queryRunner.query(
+      `UPDATE whiteboard SET contentUpdatePolicy = 'admins'`
+    );
     // update all the callouts with whiteboardRT type
     await queryRunner.query(
       `UPDATE \`callout\` SET type = 'whiteboard' WHERE type = 'whiteboard_rt'`
