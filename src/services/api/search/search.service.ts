cffa822d4ec558e8aea9c016b2117a17874d12e4
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2024-03-05 16:50:05 +0100

    Update search callout mock.

--- src/services/api/search/search.service.ts
+++ src/services/api/search/search.service.ts
@@ -1194,6 +1194,47 @@ const calloutResultsMock = [
     type: SearchResultType.CALLOUT,
     result: {
       id: 'calloutid-1234567890',
+      nameID: 'calloutName-1',
+      type: CalloutType.POST_COLLECTION,
+      framing: {
+        id: 'framingid-1221321',
+        profile: {
+          id: 'profileId-w21213213',
+          displayName: 'Test callout',
+          desciption: 'post collection callout',
+        },
+      },
+      contributions: [
+        {
+          post: {
+            id: 'postId-2132132423',
+          },
+        },
+      ],
+    },
+    callout: {
+      id: 'calloutid-1234567890',
+      nameID: 'calloutName-1',
+      type: CalloutType.POST_COLLECTION,
+      framing: {
+        id: 'framingid-1221321',
+        profile: {
+          id: 'profileId-w21213213',
+          displayName: 'Test callout',
+          desciption: 'post collection callout',
+        },
+      },
+      contributions: [
+        {
+          post: {
+            id: 'postId-2132132423',
+          },
+        },
+      ],
+      authorization: {
+        id: 'authId-21432142134',
+        credentialRules: '',
+      },
     },
   },
 ];
