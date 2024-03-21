27fc6ee592dbb0cc7746585a7a9b6ee7a3e4834b
Carlos Cano <carlos@alkem.io>
2024-03-21 17:30:15 +0100

    Fix unable to set default innovationFlow

--- src/domain/challenge/space/space.resolver.mutations.ts
+++ src/domain/challenge/space/space.resolver.mutations.ts
@@ -39,11 +39,13 @@ import { ISpaceDefaults } from '../space.defaults/space.defaults.interface';
 import { UpdateSpaceDefaultsInput } from './dto/space.dto.update.defaults';
 import { SpaceDefaultsService } from '../space.defaults/space.defaults.service';
 import { InnovationFlowTemplateService } from '@domain/template/innovation-flow-template/innovation.flow.template.service';
+import { AccountService } from '../account/account.service';
 
 @Resolver()
 export class SpaceResolverMutations {
   constructor(
     private contributionReporter: ContributionReporterService,
+    private accountService: AccountService,
     private activityAdapter: ActivityAdapter,
     private authorizationService: AuthorizationService,
     private spaceService: SpaceService,
@@ -189,10 +191,24 @@ export class SpaceResolverMutations {
       AuthorizationPrivilege.UPDATE,
       `update spaceDefaults: ${space.id}`
     );
+    const account = await this.accountService.getAccountOrFail(
+      space.account.id,
+      {
+        relations: {
+          defaults: true,
+        },
+      }
+    );
+    if (!account.defaults) {
+      throw new EntityNotInitializedException(
+        `Account doesn't have Defaults: ${account.id}`,
+        LogContext.COLLABORATION
+      );
+    }
 
     const spaceDefaults =
       await this.spaceDefaultsService.getSpaceDefaultsOrFail(
-        spaceDefaultsData.spaceID
+        account.defaults.id
       );
 
     if (spaceDefaultsData.flowTemplateID) {
