5256ef2c7b4c19995b32f2d6cbe9253b50828abb
Neil Smyth <neil@thesmyths.eu>
2024-04-07 10:18:22 +0200

    expanded enum for mentions

--- src/domain/communication/messaging/mention.interface.ts
+++ src/domain/communication/messaging/mention.interface.ts
@@ -1,6 +1,7 @@
 export enum MentionedEntityType {
   USER = 'user',
   ORGANIZATION = 'organization',
+  VIRTUAL_CONTRIBUTOR = 'virtual-contributor',
 }
 export abstract class Mention {
   nameId!: string;
