f6f781396237dbde7a01ed1f8759afcb88ea8780
svetoslav <svetoslav@alkem.io>
2024-04-10 11:23:20 +0300

    fixed a default playground query

--- src/config/graphql/spaces.ts
+++ src/config/graphql/spaces.ts
@@ -9,7 +9,6 @@ export const spacesQuery = gql`
       }
       community {
         id
-        displayName
       }
       challenges {
         nameID
@@ -18,7 +17,6 @@ export const spacesQuery = gql`
         }
         community {
           id
-          displayName
         }
         opportunities {
           nameID
@@ -27,7 +25,6 @@ export const spacesQuery = gql`
           }
           community {
             id
-            displayName
           }
         }
       }
