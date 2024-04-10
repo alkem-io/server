2120da2db8c7072fa551b56f8f9aa43e5376e01c
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2024-04-10 12:00:49 +0300

    VP Profile

--- src/domain/community/virtual-persona/virtual.persona.resolver.fields.ts
+++ src/domain/community/virtual-persona/virtual.persona.resolver.fields.ts
@@ -10,6 +10,10 @@ import { AuthorizationService } from '@core/authorization/authorization.service'
 import { IAuthorizationPolicy } from '@domain/common/authorization-policy';
 import { IVirtualPersona } from './virtual.persona.interface';
 import { AgentInfo } from '@core/authentication/agent-info';
+import { ProfileLoaderCreator } from '@core/dataloader/creators';
+import { Loader } from '@core/dataloader/decorators';
+import { ILoader } from '@core/dataloader/loader.interface';
+import { IProfile } from '@domain/common/profile';
 
 @Resolver(() => IVirtualPersona)
 export class VirtualPersonaResolverFields {
@@ -40,5 +44,27 @@ export class VirtualPersonaResolverFields {
     );
 
     return virtualPersona.authorization;
+  }
+
+  @ResolveField('profile', () => IProfile, {
+    nullable: false,
+    description: 'The Profile for this VirtualPersona.',
+  })
+  @UseGuards(GraphqlGuard)
+  async profile(
+    @Parent() parent: VirtualPersona,
+    @CurrentUser() agentInfo: AgentInfo,
+    @Loader(ProfileLoaderCreator, { parentClassRef: VirtualPersona })
+    loader: ILoader<IProfile>
+  ): Promise<IProfile> {
+    const profile = await loader.load(parent.id);
+    //toDo needs proper auth
+    // await this.authorizationService.grantAccessOrFail(
+    //   agentInfo,
+    //   profile.authorization,
+    //   AuthorizationPrivilege.READ,
+    //   `read profile on User: ${profile.displayName}`
+    // );
+    return profile;
   }
 }
