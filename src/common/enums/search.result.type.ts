534f169d2a2f1a286813ed2a7b6ea33422ae8373
svetoslav <svetoslav@alkem.io>
2024-04-16 17:59:55 +0300

    api wip

--- src/common/enums/search.result.type.ts
+++ src/common/enums/search.result.type.ts
@@ -2,8 +2,6 @@ import { registerEnumType } from '@nestjs/graphql';
 
 export enum SearchResultType {
   SPACE = 'space',
-  CHALLENGE = 'challenge',
-  OPPORTUNITY = 'opportunity',
   USER = 'user',
   ORGANIZATION = 'organization',
   USERGROUP = 'usergroup',
