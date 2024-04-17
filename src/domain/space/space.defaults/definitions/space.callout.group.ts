076d86df0ebf2a9344e68f84eff0a0f5c05a7859
Carlos Cano <carlos@alkem.io>
2024-04-17 16:31:18 +0200

    Callouts only on the right side (#3834)
    * Callouts only on the right side
    
    * Migration
    
    * Cleanup and fix orders
    
    ---------
    
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>
    Co-authored-by: Svetoslav Petkov <svetoslav@alkem.io>

--- src/domain/space/space.defaults/definitions/space.callout.group.ts
+++ src/domain/space/space.defaults/definitions/space.callout.group.ts
@@ -3,28 +3,16 @@ import { ICalloutGroup } from '@domain/collaboration/callout-groups/callout.grou
 
 export const spaceCalloutGroups: ICalloutGroup[] = [
   {
-    displayName: CalloutGroupName.HOME_1,
-    description: 'The left column on the Home page.',
+    displayName: CalloutGroupName.HOME,
+    description: 'The Home page.',
   },
   {
-    displayName: CalloutGroupName.HOME_2,
-    description: 'The right column on the Home page.',
+    displayName: CalloutGroupName.COMMUNITY,
+    description: 'The Community page.',
   },
   {
-    displayName: CalloutGroupName.COMMUNITY_1,
-    description: 'The left column on the Community page.',
-  },
-  {
-    displayName: CalloutGroupName.COMMUNITY_2,
-    description: 'The right column on the Community page.',
-  },
-  {
-    displayName: CalloutGroupName.SUBSPACES_1,
-    description: 'The left column on the Subspaces page.',
-  },
-  {
-    displayName: CalloutGroupName.SUBSPACES_2,
-    description: 'The right column on the Subspaces page.',
+    displayName: CalloutGroupName.SUBSPACES,
+    description: 'The Subspaces page.',
   },
   {
     displayName: CalloutGroupName.KNOWLEDGE,
