128516bdba0cc48bd254ab47e5640e101dc7b513
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2024-04-17 11:26:06 +0300

    ingest update

--- src/services/api/search2/search.ingest/search.ingest.service.ts
+++ src/services/api/search2/search.ingest/search.ingest.service.ts
@@ -194,18 +194,14 @@ export class SearchIngestService {
       {
         index: `${this.indexPattern}posts`,
         fetchFn: this.fetchPosts.bind(this),
-        batchSize: 15,
+        batchSize: 10,
       },
     ];
 
     return asyncReduceSequential(
       params,
       async (acc, { index, fetchFn, batchSize }) => {
-        // introduced some delay between the ingestion of different entities
-        // to not overwhelm the elasticsearch cluster
-        await setTimeout(500, null);
-
-        const batches = await this.fetchAndIngest(index, fetchFn, batchSize);
+        const batches = await this._ingest(index, fetchFn, batchSize);
         const total = batches.reduce((acc, val) => acc + (val.total ?? 0), 0);
         acc[index] = {
           total: total + (acc[index]?.total ?? 0),
@@ -242,6 +238,8 @@ export class SearchIngestService {
       }
 
       start += batchSize;
+      // delay between batches
+      await setTimeout(1000, null);
     }
 
     return results;
