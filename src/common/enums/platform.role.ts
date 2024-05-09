80cc320c0c7cfc94e2efe881f6cd547b44c88f5f
Neil Smyth <neil@thesmyths.eu>
2024-05-09 12:02:40 +0200

    add comment

--- src/common/enums/platform.role.ts
+++ src/common/enums/platform.role.ts
@@ -2,7 +2,7 @@ import { registerEnumType } from '@nestjs/graphql';
 
 export enum PlatformRole {
   GLOBAL_ADMIN = 'global-admin',
-  SUPPORT = 'support',
+  SUPPORT = 'support', // Platform management; can be allowed to act as a SpaceAdmin depending on Space settings
   LICENSE_MANAGER = 'license-manager',
   COMMUNITY_READER = 'community-reader',
   SPACES_READER = 'spaces-reader',
