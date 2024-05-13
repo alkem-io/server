115e2f66e79745785ef264116b81326edc203d4a
Svetoslav <svetoslav@alkem.io>
2024-05-13 14:54:29 +0300

    migration fix

--- src/migrations/1715575211966-subspaceCredentials.ts
+++ src/migrations/1715575211966-subspaceCredentials.ts
@@ -7,25 +7,25 @@ export class subspaceCredentials1715575211966 implements MigrationInterface {
     // All credentials need to be updated to reflect the new type
     await queryRunner.query(`
       UPDATE credential
-      SET your_field = REPLACE(type, 'subspace-', 'space-')
+      SET type = REPLACE(type, 'subspace-', 'space-')
       WHERE type LIKE '%subspace-%';
     `);
 
     // And the community policies
     await queryRunner.query(`
-      UPDATE credential
+      UPDATE community_policy
       SET member = REPLACE(member, 'subspace-', 'space-')
       WHERE member LIKE '%subspace-%'
     `);
 
     await queryRunner.query(`
-      UPDATE credential
+      UPDATE community_policy
       SET lead = REPLACE(lead, 'subspace-', 'space-')
       WHERE lead LIKE '%subspace-%'
     `);
 
     await queryRunner.query(`
-      UPDATE credential
+      UPDATE community_policy
       SET admin = REPLACE(admin, 'subspace-', 'space-')
       WHERE admin LIKE '%subspace-%'
     `);
