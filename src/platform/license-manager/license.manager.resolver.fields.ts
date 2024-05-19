4b0b3d16cc72e821bcd4d28300dd34669c03d78b
Neil Smyth <neil@thesmyths.eu>
2024-05-19 11:06:11 +0200

    server compiling + starting

--- src/platform/license-manager/license.manager.resolver.fields.ts
+++ src/platform/license-manager/license.manager.resolver.fields.ts
@@ -6,20 +6,29 @@ import { AuthorizationAgentPrivilege } from '@src/common/decorators';
 import { ILicenseManager } from './license.manager.interface';
 import { LicenseManagerService } from './license.manager.service';
 import { ILicensePlan } from '@platform/license-plan/license.plan.interface';
+import { ILicensePolicy } from '@platform/license-policy';
 
 @Resolver(() => ILicenseManager)
 export class LicenseManagerResolverFields {
   constructor(private licenseManagerService: LicenseManagerService) {}
 
   @AuthorizationAgentPrivilege(AuthorizationPrivilege.READ)
-  @ResolveField('licensePlans', () => [ILicensePlan], {
+  @ResolveField('plans', () => [ILicensePlan], {
     nullable: false,
     description: 'The License Plans in use on the platform.',
   })
   @UseGuards(GraphqlGuard)
-  async licensePlans(
+  async plans(
     @Parent() licenseManager: ILicenseManager
   ): Promise<ILicensePlan[]> {
     return await this.licenseManagerService.getLicensePlans(licenseManager.id);
+  }
+
+  @ResolveField('policy', () => ILicensePolicy, {
+    nullable: false,
+    description: 'The LicensePolicy in use by the License Manager.',
+  })
+  policy(@Parent() licenseManager: ILicenseManager): Promise<ILicensePolicy> {
+    return this.licenseManagerService.getLicensePolicy(licenseManager.id);
   }
 }
