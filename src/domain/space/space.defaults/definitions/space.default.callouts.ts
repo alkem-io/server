4d28e4ce0b9c05cb782fcd1c89e59c195320f5c6
Simone <38861315+SimoneZaza@users.noreply.github.com>
2024-05-23 10:35:51 +0200

    Update space.default.callouts.ts

--- src/domain/space/space.defaults/definitions/space.default.callouts.ts
+++ src/domain/space/space.defaults/definitions/space.default.callouts.ts
@@ -16,7 +16,7 @@ export const spaceDefaultCallouts: CreateCalloutInput[] = [
     framing: {
       profile: {
         displayName: '👋 Welcome to your space!',
-        description: 'Take an interactive tour below to discover how our spaces are thoughtfully designed. Alternatively, explore the other tutorials available on this page and beyond. We\'re excited to have you here! \n<div style=\'position: relative; padding-bottom: calc(40% + 41px); height: 0; width: 100%;\'><iframe src=\'https://demo.arcade.software/zVYe3x4PkZjUkMEMP9Kg?embed&show_copy_link=true\' title=\'👋 Welcome to your space\' frameborder=\'0\' loading=\'lazy\' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=\'clipboard-write\' style=\'position: absolute; top: 0; left: 0; width: 100%; height: 100%;color-scheme: light;\'></iframe></div>\n',
+        description: 'Take an interactive tour below to discover how our spaces are designed. We also invite you to explore the other tutorials available on this page and beyond. We\'re excited to have you here! \n<div style=\'position: relative; padding-bottom: calc(40% + 41px); height: 0; width: 100%;\'><iframe src=\'https://demo.arcade.software/zVYe3x4PkZjUkMEMP9Kg?embed&show_copy_link=true\' title=\'👋 Welcome to your space\' frameborder=\'0\' loading=\'lazy\' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=\'clipboard-write\' style=\'position: absolute; top: 0; left: 0; width: 100%; height: 100%;color-scheme: light;\'></iframe></div>\n',
       },
     },
   },
@@ -31,7 +31,7 @@ export const spaceDefaultCallouts: CreateCalloutInput[] = [
     framing: {
       profile: {
         displayName: '⚙️ Set it up your way!',
-        description: 'In this concise guide, you\'ll discover how to customize your Space to suit your needs. Learn whether your Space should be visible to everyone or just its members, how people can join (directly or by applying), and what essential information to include on the about page. Let\'s get started! \n<div style=\'position: relative; padding-bottom: calc(40% + 41px); height: 0; width: 100%;\'><iframe src=\'https://demo.arcade.software/Rbwhpk4zro3Uer61iQKL?embed&show_copy_link=true\' title=\'⚙️ Set it up your way!\' frameborder=\'0\' loading=\'lazy\' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=\'clipboard-write\' style=\'position: absolute; top: 0; left: 0; width: 100%; height: 100%;color-scheme: light;\'></iframe></div>\n',
+        description: 'In this concise guide, you\'ll discover how to customize your Space to suit your needs. Learn more about how to set the visibility of the Space, how people can join, and what essential information to include on the about page. Let\'s get started! \n<div style=\'position: relative; padding-bottom: calc(40% + 41px); height: 0; width: 100%;\'><iframe src=\'https://demo.arcade.software/Rbwhpk4zro3Uer61iQKL?embed&show_copy_link=true\' title=\'⚙️ Set it up your way!\' frameborder=\'0\' loading=\'lazy\' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=\'clipboard-write\' style=\'position: absolute; top: 0; left: 0; width: 100%; height: 100%;color-scheme: light;\'></iframe></div>\n',
       },
     },
   },
@@ -76,7 +76,7 @@ export const spaceDefaultCallouts: CreateCalloutInput[] = [
     framing: {
       profile: {
         displayName: '🤝 Set up your Community',
-        description: 'In this tour, you\'ll discover how to configure your community, define permissions, create guidelines, set up an application process, and send out invitations. Let\'s get started! \n<div style=\'position: relative; padding-bottom: calc(40% + 41px); height: 0; width: 100%;\'><iframe src=\'https://demo.arcade.software/guBQToL8DWsnjCE7GLve?embed&show_copy_link=true\'title=\'🏘️ Set up your Community\' frameborder=\'0\' loading=\'lazy\' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=\'clipboard-write\' style=\'position: absolute; top: 0; left: 0; width: 100%; height: 100%;color-scheme: light;\'></iframe></div>',
+        description: 'In this tour, you\'ll discover how to define permissions, create guidelines, set up an application process, and send out invitations. Let\'s get started! \n<div style=\'position: relative; padding-bottom: calc(40% + 41px); height: 0; width: 100%;\'><iframe src=\'https://demo.arcade.software/guBQToL8DWsnjCE7GLve?embed&show_copy_link=true\'title=\'🏘️ Set up your Community\' frameborder=\'0\' loading=\'lazy\' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=\'clipboard-write\' style=\'position: absolute; top: 0; left: 0; width: 100%; height: 100%;color-scheme: light;\'></iframe></div>',
       },
     },
   },
@@ -106,7 +106,7 @@ export const spaceDefaultCallouts: CreateCalloutInput[] = [
     framing: {
       profile: {
         displayName: '📚 The Knowledge Base',
-        description: 'Welcome to your knowledge base! This page serves as a central repository for valuable information and references. In the tour below, you\'ll discover how to maximize the utility of this resource. \n<div style=\'position: relative; padding-bottom: calc(40% + 41px); height: 0; width: 100%;\'><iframe src=\'https://demo.arcade.software/pSXpCpds3Mcdibk8LhBE?embed&show_copy_link=true\' title=\'Knowledge Base\' frameborder=\'0\' loading=\'lazy\' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=\'clipboard-write\' style=\'position: absolute; top: 0; left: 0; width: 100%; height: 100%;color-scheme: light;\'></iframe></div>\n',
+        description: 'Welcome to your knowledge base! This page serves as a central repository for valuable information and references that are relevant for the entire community.\n<div style=\'position: relative; padding-bottom: calc(40% + 41px); height: 0; width: 100%;\'><iframe src=\'https://demo.arcade.software/pSXpCpds3Mcdibk8LhBE?embed&show_copy_link=true\' title=\'Knowledge Base\' frameborder=\'0\' loading=\'lazy\' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=\'clipboard-write\' style=\'position: absolute; top: 0; left: 0; width: 100%; height: 100%;color-scheme: light;\'></iframe></div>\n',
       },
     },
   },
