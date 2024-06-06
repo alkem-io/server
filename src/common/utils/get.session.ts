1d8e0a9c4b49b0f299b6f57a2abc7a17887a354e
Svetoslav Petkov <svetoslav@alkem.io>
2024-06-06 11:16:10 +0300

    Auth (#4053)
    * file-service through oathkeeper
    
    * whiteboard collab through oathkeeper
    
    * whiteboard image updated
    
    * latest docker images
    
    ---------
    
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/common/utils/get.session.ts
+++ src/common/utils/get.session.ts
@@ -1,18 +1,20 @@
+import { KratosPayload } from '@core/authentication/kratos.payload';
 import { FrontendApi, Session } from '@ory/kratos-client';
+import jwt_decode from 'jwt-decode';
 
+/**
+ * @param kratosClient
+ * @param opts
+ * @throws {Error}
+ */
 export const getSession = async (
   kratosClient: FrontendApi,
   opts: {
     authorization?: string;
     cookie?: string;
-    apiToken?: string;
   }
 ): Promise<Session | never> => {
-  const { cookie, authorization, apiToken } = opts;
-
-  if (apiToken) {
-    return getSessionFromApiToken(kratosClient, apiToken);
-  }
+  const { cookie, authorization } = opts;
 
   if (authorization) {
     return getSessionFromAuthorizationHeader(kratosClient, authorization);
@@ -25,6 +27,11 @@ export const getSession = async (
   throw new Error('Authorization header or cookie not provided');
 };
 
+/**
+ * @param kratosClient
+ * @param cookie
+ * @throws {Error}
+ */
 const getSessionFromCookie = async (
   kratosClient: FrontendApi,
   cookie: string
@@ -39,6 +46,11 @@ const getSessionFromCookie = async (
     throw new Error(e?.message);
   }
 };
+/**
+ * @param kratosClient
+ * @param authorizationHeader
+ * @throws {Error}
+ */
 const getSessionFromAuthorizationHeader = (
   kratosClient: FrontendApi,
   authorizationHeader: string
@@ -46,12 +58,29 @@ const getSessionFromAuthorizationHeader = (
   const [, token] = authorizationHeader.split(' ');
 
   if (!token) {
-    throw new Error('Token not provided in Authorization header');
+    throw new Error('Token not provided in the Authorization header');
   }
 
-  return getSessionFromApiToken(kratosClient, token);
+  try {
+    return getSessionFromJwt(token);
+  } catch (e) {
+    // ...
+  }
+
+  try {
+    return getSessionFromApiToken(kratosClient, token);
+  } catch (e) {
+    // ...
+  }
+
+  throw new Error('Not a valid token provided in the Authorization header');
 };
 
+/**
+ * @param kratosClient
+ * @param apiToken
+ * @throws {Error}
+ */
 const getSessionFromApiToken = async (
   kratosClient: FrontendApi,
   apiToken: string
@@ -76,6 +105,30 @@ const getSessionFromApiToken = async (
 
   if (!session) {
     throw new Error('Kratos session not found for api token');
+  }
+
+  return session;
+};
+
+/**
+ * @param token
+ * @throws {Error}
+ */
+const getSessionFromJwt = (token: string): Session | never => {
+  if (!token) {
+    throw new Error('Token is empty!');
+  }
+
+  let session: Session | null;
+
+  try {
+    session = jwt_decode<KratosPayload>(token).session;
+  } catch (error: any) {
+    throw new Error(error?.message ?? 'Token is not a valid JWT token!');
+  }
+
+  if (!session) {
+    throw new Error('Kratos session not found in token');
   }
 
   return session;
