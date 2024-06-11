27bdcd373b1aba33b020b0dc934c6494dd34b8bb
Neil Smyth <neil@thesmyths.eu>
2024-06-11 08:24:19 +0200

    switch to using tagsets on framing to set flow state properly

--- src/domain/space/space.defaults/definitions/virtual-contributor/space.defaults.innovation.flow.virtual.contributor.ts
+++ src/domain/space/space.defaults/definitions/virtual-contributor/space.defaults.innovation.flow.virtual.contributor.ts
@@ -1,20 +1,26 @@
 import { IInnovationFlowState } from '@domain/collaboration/innovation-flow-states/innovation.flow.state.interface';
 
+export enum FlowStates {
+  GOING_LIVE = 'Going Live',
+  INTRODUCTION = 'Introduction',
+  BODY_OF_KNOWLEDGE = 'Body of Knowledge',
+  LICENSE_SPACE_ENTERPRISE = 'license-space-enterprise',
+}
+
 export const spaceDefaultsInnovationFlowStatesVirtualContributor: IInnovationFlowState[] =
   [
     {
-      displayName: 'Introduction',
+      displayName: FlowStates.INTRODUCTION,
       description:
         'Scroll down to read more about how to get started. Ready to add some knowledge to your Virtual Contributor? Click on Body of Knowledge ⬆️',
     },
     {
-      displayName: 'Body of Knowledge',
+      displayName: FlowStates.BODY_OF_KNOWLEDGE,
       description:
         'Here you can share all relevant information for the Virtual Contributor to know about. To get started, three posts have already been added. Click on the ➕ Collaboration Tool to add more.',
     },
     {
-      displayName: 'Going Live',
-      description:
-        '',
+      displayName: FlowStates.GOING_LIVE,
+      description: '',
     },
   ];
