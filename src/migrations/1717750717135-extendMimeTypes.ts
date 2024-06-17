7587737e58d29b5b30b13f4658bf80dad5551e98
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2024-06-17 18:42:17 +0300

    copy over allowed mime types into the migration

--- src/migrations/1717750717135-extendMimeTypes.ts
+++ src/migrations/1717750717135-extendMimeTypes.ts
@@ -1,5 +1,23 @@
-import { DEFAULT_ALLOWED_MIME_TYPES } from '@common/enums/mime.file.type';
 import { MigrationInterface, QueryRunner } from 'typeorm';
+
+const DEFAULT_ALLOWED_MIME_TYPES = [
+  'application/pdf',
+  'application/vnd.ms-excel',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  'application/vnd.oasis.opendocument.spreadsheet',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.oasis.opendocument.text',
+  'image/bmp',
+  'image/jpg',
+  'image/jpeg',
+  'image/x-png',
+  'image/png',
+  'image/gif',
+  'image/webp',
+  'image/svg+xml',
+  'image/avif',
+];
 
 export class extendMimeTypes1717750717135 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
