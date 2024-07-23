fc97c426ee01b807c5f6430d0d165902033faba9
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2024-07-23 13:27:45 +0300

    Secure elastic connection based on self-signed ssl cert

--- src/services/external/elasticsearch/elasticsearch-client/elasticsearch.client.factory.ts
+++ src/services/external/elasticsearch/elasticsearch-client/elasticsearch.client.factory.ts
@@ -2,17 +2,36 @@ import { LoggerService } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Client } from '@elastic/elasticsearch';
 import { ConfigurationTypes } from '@common/enums';
+import fs from 'fs';
 
-export const elasticSearchClientFactory = (
+export const elasticSearchClientFactory = async (
   logger: LoggerService,
   configService: ConfigService
-): Client | undefined => {
+): Promise<Client | undefined> => {
   const elasticsearch = configService.get(
     ConfigurationTypes.INTEGRATIONS
   )?.elasticsearch;
 
   const { host, retries, timeout, api_key, tls } = elasticsearch;
   const rejectUnauthorized = tls.rejectUnauthorized ?? false;
+  let tlsOptions;
+
+  // Ensure the path to the certificate inside the container is correct
+  if (tls.ca_cert_path === 'none') {
+    tlsOptions = { rejectUnauthorized };
+  } else {
+    // This should match the mountPath in your Kubernetes deployment YAML
+    const certPath = tls.ca_cert_path;
+    if (!fs.existsSync(certPath)) {
+      logger.error(`Certificate not found at path: ${certPath}`);
+      return undefined;
+    }
+    const cert = fs.readFileSync(certPath);
+    tlsOptions = {
+      rejectUnauthorized: false,
+      ca: cert,
+    };
+  }
 
   if (!host) {
     logger.warn('Elasticsearch host URL not provided!');
@@ -30,6 +49,6 @@ export const elasticSearchClientFactory = (
     requestTimeout: timeout,
     resurrectStrategy: 'ping',
     auth: { apiKey: api_key },
-    tls: { rejectUnauthorized },
+    tls: tlsOptions,
   });
 };
