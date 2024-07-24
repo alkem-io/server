1022d8522e5b79c9d4d741fdfa92ecdb1287b6e1
Neil Smyth <neil@thesmyths.eu>
2024-07-24 15:45:32 +0100

    merge from develop mistake

--- src/domain/community/user/user.service.ts
+++ src/domain/community/user/user.service.ts
@@ -422,13 +422,6 @@ export class UserService {
     }
     const { id } = user;
 
-    const isAccountHost = await this.isAccountHost(user);
-    if (isAccountHost) {
-      throw new ForbiddenException(
-        'Unable to delete User: host of one or more accounts',
-        LogContext.SPACES
-      );
-    }
     await this.clearUserCache(user);
 
     if (user.profile) {
