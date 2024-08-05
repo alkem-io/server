a3f50d52287df8a45d326560ca0de9419cc99cc9
Neil Smyth <neil@thesmyths.eu>
2024-08-05 17:17:51 +0100

    fixed mentions to have ids

--- src/domain/communication/room/room.service.mentions.from.text.spec.ts
+++ src/domain/communication/room/room.service.mentions.from.text.spec.ts
@@ -22,74 +22,74 @@ describe('RoomServiceMentions', () => {
     ['no mentions here', []],
     [
       'Hey, [@aleksandar-alex](https://localhost/user/alex123) - this look like you',
-      [{ nameId: 'alex123', type: 'user' }],
+      [{ id: 'alex123', type: 'user' }],
     ],
 
     [
       'Hey, [@aleksandar-alex](http://localhost:3000/user/alex) - this look like you',
-      [{ nameId: 'alex', type: 'user' }],
+      [{ id: 'alex', type: 'user' }],
     ],
     [
       'Hey, [@aleksandar-alex](https://localhost:3000/user/alex) - this look like you',
-      [{ nameId: 'alex', type: 'user' }],
+      [{ id: 'alex', type: 'user' }],
     ],
     [
       'Hey, [@aleksandar](http://localhost:3000/user/alex-alex) - this look like you',
-      [{ nameId: 'alex-alex', type: 'user' }],
+      [{ id: 'alex-alex', type: 'user' }],
     ],
     [
       'Hey, [@aleksandar](https://localhost:3000/user/alex-alex) - this look like you',
-      [{ nameId: 'alex-alex', type: 'user' }],
+      [{ id: 'alex-alex', type: 'user' }],
     ],
 
     [
       'Hey, [@aleksandar-alex](http://localhost:3000/organization/alex) - this look like you',
-      [{ nameId: 'alex', type: 'organization' }],
+      [{ id: 'alex', type: 'organization' }],
     ],
     [
       'Hey, [@aleksandar-alex](https://localhost:3000/organization/alex) - this look like you',
-      [{ nameId: 'alex', type: 'organization' }],
+      [{ id: 'alex', type: 'organization' }],
     ],
     [
       'Hey, [@aleksandar](http://localhost:3000/organization/alex-alex) - this look like you',
-      [{ nameId: 'alex-alex', type: 'organization' }],
+      [{ id: 'alex-alex', type: 'organization' }],
     ],
     [
       'Hey, [@aleksandar](https://localhost:3000/organization/alex-alex) - this look like you',
-      [{ nameId: 'alex-alex', type: 'organization' }],
+      [{ id: 'alex-alex', type: 'organization' }],
     ],
 
     [
       'Hey, [@aleksandar](http://localhost:3000/user/alex) - this look like you',
-      [{ nameId: 'alex', type: 'user' }],
+      [{ id: 'alex', type: 'user' }],
     ],
     [
       'Hey, [@aleksandar](https://localhost:3000/user/alex) - this look like you',
-      [{ nameId: 'alex', type: 'user' }],
+      [{ id: 'alex', type: 'user' }],
     ],
     [
       'Hey, [@aleksandar](http://localhost:3000/organization/alex) - this look like you',
-      [{ nameId: 'alex', type: 'organization' }],
+      [{ id: 'alex', type: 'organization' }],
     ],
     [
       'Hey, [@aleksandar](https://localhost:3000/organization/alex) - this look like you',
-      [{ nameId: 'alex', type: 'organization' }],
+      [{ id: 'alex', type: 'organization' }],
     ],
 
     [
       'Hey, [@aleksandar](http://localhost:3000/user/alex) - this look like you\n' +
         'Hey, [@aleksandar](http://localhost:3000/organization/alex-org) - this look like you',
       [
-        { nameId: 'alex', type: 'user' },
-        { nameId: 'alex-org', type: 'organization' },
+        { id: 'alex', type: 'user' },
+        { id: 'alex-org', type: 'organization' },
       ],
     ],
     [
       'Hey, [@aleksandar](https://localhost:3000/user/alex) - this look like you\n' +
         'Hey, [@aleksandar](https://localhost:3000/organization/alex-org) - this look like you',
       [
-        { nameId: 'alex', type: 'user' },
-        { nameId: 'alex-org', type: 'organization' },
+        { id: 'alex', type: 'user' },
+        { id: 'alex-org', type: 'organization' },
       ],
     ],
   ])('%s -> %j', async (text, expected) => {
