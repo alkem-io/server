04a5b39a281fdbcce8aa61ce8e6576484ad77eb5
Neil Smyth <neil@thesmyths.eu>
2024-08-07 13:48:40 +0100

    removed method not used

--- src/platform/settings/platform.settings.service.ts
+++ src/platform/settings/platform.settings.service.ts
@@ -1,6 +1,4 @@
-import { AuthorizationPrivilege } from '@common/enums';
 import { Inject, Injectable, LoggerService } from '@nestjs/common';
-import { AgentInfo } from '@core/authentication.agent.info/agent.info';
 import { AuthorizationService } from '@core/authorization/authorization.service';
 import { PlatformAuthorizationPolicyService } from '@platform/authorization/platform.authorization.policy.service';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
@@ -31,16 +29,5 @@ export class PlatformSettingsService {
     }
 
     return await this.organizationService.save(organization);
-  }
-
-  public async checkAuthorizationOrFail(agentInfo: AgentInfo, action: string) {
-    const authorizationPolicy =
-      await this.platformAuthorizationService.getPlatformAuthorizationPolicy();
-    this.authorizationService.grantAccessOrFail(
-      agentInfo,
-      authorizationPolicy,
-      AuthorizationPrivilege.PLATFORM_ADMIN,
-      action
-    );
   }
 }
