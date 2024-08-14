b43f3f90e792ecff7395651e2bf4588695eaa9b3
hero101 <svetoslav@alkem.io>
2024-08-14 17:30:14 +0300

    added CreateNameableOptionalInput

--- src/domain/community/contributor/dto/contributor.dto.create.ts
+++ src/domain/community/contributor/dto/contributor.dto.create.ts
@@ -1,19 +1,5 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { NameID } from '@domain/common/scalars/scalar.nameid';
-import { CreateProfileInput } from '@domain/common/profile/dto/profile.dto.create';
-import { ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
+import { InputType } from '@nestjs/graphql';
+import { CreateNameableOptionalInput } from '@domain/common/entity/nameable-entity/dto/nameable.optiona.dto.create';
 
 @InputType()
-export class CreateContributorInput {
-  @Field(() => NameID, {
-    nullable: true,
-    description: 'A readable identifier, unique within the containing scope.',
-  })
-  nameID?: string;
-
-  @Field(() => CreateProfileInput, { nullable: false })
-  @ValidateNested({ each: true })
-  @Type(() => CreateProfileInput)
-  profileData!: CreateProfileInput;
-}
+export class CreateContributorInput extends CreateNameableOptionalInput {}
