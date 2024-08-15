c3a8ed61f6ecf65b6c811047caf89089750be90c
Neil Smyth <neil@thesmyths.eu>
2024-08-15 20:30:55 +0200

    updated migration to cover all authorization policy types

--- src/migrations/1723391280999-authorizationPolicyType.ts
+++ src/migrations/1723391280999-authorizationPolicyType.ts
@@ -8,66 +8,17 @@ export class AuthorizationPolicyType1723121607999
     await queryRunner.query(
       'ALTER TABLE `authorization_policy` ADD `type` varchar(128) NULL'
     );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'account',
-      'account'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'space_defaults',
-      'space-defaults'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'space',
-      'space'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'user',
-      'user'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'organization',
-      'organization'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'organization_verification',
-      'organization-verification'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'platform',
-      'platform'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'profile',
-      'profile'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'preference',
-      'preference'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'reference',
-      'reference'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'tagset',
-      'tagset'
-    );
-    await this.updateAuthorizationPolicyTypeForEntity(
-      queryRunner,
-      'visual',
-      'visual'
-    );
+    for (const key in AuthorizationPolicyType) {
+      if (AuthorizationPolicyType.hasOwnProperty(key)) {
+        const type =
+          AuthorizationPolicyType[key as keyof typeof AuthorizationPolicyType];
+        await this.updateAuthorizationPolicyTypeForEntity(
+          queryRunner,
+          key.toLowerCase(),
+          type
+        );
+      }
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -102,4 +53,60 @@ export class AuthorizationPolicyType1723121607999
       }
     }
   }
+}
+
+export enum AuthorizationPolicyType {
+  AGENT = 'agent',
+  CALLOUT = 'callout',
+  CALLOUT_CONTRIBUTION = 'callout-contribution',
+  CALLOUT_FRAMING = 'callout-framing',
+  COLLABORATION = 'collaboration',
+  INNOVATION_FLOW = 'innovation-flow',
+  INNOVATION_HUB = 'innovation-hub',
+  INNOVATION_PACK = 'innovation-pack',
+  LINK = 'link',
+  POST = 'post',
+  WHITEBOARD = 'whiteboard',
+  PREFERENCE = 'preference',
+  PREFERENCE_SET = 'preference-set',
+  PROFILE = 'profile',
+  REFERENCE = 'reference',
+  TAGSET = 'tagset',
+  VISUAL = 'visual',
+  COMMUNICATION = 'communication',
+  ROOM = 'room',
+  AI_PERSONA = 'ai-persona',
+  APPLICATION = 'application',
+  COMMUNITY = 'community',
+  COMMUNITY_GUIDELINES = 'community-guidelines',
+  INVITATION = 'invitation',
+  FORUM = 'forum',
+  PLATFORM = 'platform',
+  DISCUSSION = 'discussion',
+  USER = 'user',
+  USER_GROUP = 'user-group',
+  ACTOR = 'actor',
+  ACTOR_GROUP = 'actor-group',
+  ORGANIZATION = 'organization',
+  ORGANIZATION_VERIFICATION = 'organization-verification',
+  CONTEXT = 'context',
+  ECOSYSTEM_MODEL = 'ecosystem-model',
+  VIRTUAL_CONTRIBUTOR = 'virtual-contributor',
+  SPACE = 'space',
+  SPACE_DEFAULTS = 'space-defaults',
+  ACCOUNT = 'account',
+  DOCUMENT = 'document',
+  STORAGE_AGGREGATOR = 'storage-aggregator',
+  STORAGE_BUCKET = 'storage-bucket',
+  TEMPLATES_SET = 'templates-set',
+  CALENDAR = 'calendar',
+  CALENDAR_EVENT = 'calendar-event',
+  TIMELINE = 'timeline',
+  WHITEBOARD_TEMPLATE = 'whiteboard-template',
+  POST_TEMPLATE = 'template',
+  CALLOUT_TEMPLATE = 'template',
+  INNOVATION_FLOW_TEMPLATE = 'template',
+  COMMUNITY_GUIDELINES_TEMPLATE = 'template',
+
+  AI_PERSONA_SERVICE = 'ai-persona-service',
 }
