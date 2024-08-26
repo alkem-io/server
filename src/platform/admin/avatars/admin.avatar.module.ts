8890d9b85b0daa67420b1d0a844e3e50ddb0943b
Neil Smyth <neil@thesmyths.eu>
2024-08-26 07:48:02 +0200

    missed commit

--- src/platform/admin/avatars/admin.avatar.module.ts
+++ src/platform/admin/avatars/admin.avatar.module.ts
@@ -3,9 +3,15 @@ import { ContributorModule } from '@domain/community/contributor/contributor.mod
 import { AdminSearchContributorsMutations } from './admin.avatarresolver.mutations';
 import { AuthorizationPolicyModule } from '@domain/common/authorization-policy/authorization.policy.module';
 import { AuthorizationModule } from '@core/authorization/authorization.module';
+import { PlatformAuthorizationPolicyModule } from '@platform/authorization/platform.authorization.policy.module';
 
 @Module({
-  imports: [AuthorizationModule, AuthorizationPolicyModule, ContributorModule],
+  imports: [
+    AuthorizationModule,
+    AuthorizationPolicyModule,
+    PlatformAuthorizationPolicyModule,
+    ContributorModule,
+  ],
   providers: [AdminSearchContributorsMutations],
   exports: [],
 })
