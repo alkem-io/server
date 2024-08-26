fdf19fd5bd0e769273bdac7e56fb92ce9b3da335
Carlos Cano <carlos@alkem.io>
2024-08-26 16:23:57 +0200

    Migration sets the sortOrder of the existing contributions

--- src/migrations/1724319295054-ContributionSortOrder.ts
+++ src/migrations/1724319295054-ContributionSortOrder.ts
@@ -5,8 +5,29 @@ export class ContributionSortOrder1724319295054 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`callout_contribution\` ADD \`sortOrder\` int NOT NULL`
+      `ALTER TABLE \`callout_contribution\` ADD \`sortOrder\` int NOT NULL DEFAULT (0)`
     );
+    // Add a default sort order to all existing contributions:
+    const contributions: {
+      id: string;
+      calloutId: string;
+    }[] = await queryRunner.query(
+      `SELECT \`id\`, \`calloutId\` FROM \`callout_contribution\` ORDER BY \`calloutId\``
+    );
+
+    let lastCalloutId = '';
+    let sortOrder = 1;
+    for (const { id, calloutId } of contributions) {
+      if (lastCalloutId !== calloutId) {
+        sortOrder = 1;
+        lastCalloutId = calloutId;
+      } else {
+        sortOrder++;
+      }
+      await queryRunner.query(
+        `UPDATE \`callout_contribution\` SET \`sortOrder\` = ${sortOrder} WHERE \`id\` = '${id}'`
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
