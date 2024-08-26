dd2791d3b7cbfd3eac9829f5787e0b6d37ee13e4
Neil Smyth <30729240+techsmyth@users.noreply.github.com>
2024-08-26 11:57:35 +0200

    Ensure avatars are stored on Alkemio (#4451)
    * re-enabled avatar upload
    * removed file-type; added comments to explain why the mime type is hard-coded + what is needed to fix it
    * updated avatar creation document logic to specify a User, as opposed to the contributor, ID for the document
    * ensure there is an agentInfo to use when creating entities during bootstrap
    * Added Admin contributors module to the main module; ensure avatar local storage can never throw an exception

--- src/platform/admin/avatars/admin.avatarresolver.mutations.ts
+++ src/platform/admin/avatars/admin.avatarresolver.mutations.ts
@@ -1,18 +1,14 @@
 import { Inject, LoggerService, UseGuards } from '@nestjs/common';
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { CurrentUser } from '@src/common/decorators';
 import { GraphqlGuard } from '@core/authorization/graphql.guard';
-import { AuthorizationPrivilege, LogContext } from '@common/enums';
+import { AuthorizationPrivilege } from '@common/enums';
 import { PlatformAuthorizationPolicyService } from '@platform/authorization/platform.authorization.policy.service';
 import { AgentInfo } from '@core/authentication.agent.info/agent.info';
 import { AuthorizationService } from '@core/authorization/authorization.service';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ContributorService } from '@domain/community/contributor/contributor.service';
-import { InjectEntityManager } from '@nestjs/typeorm';
-import { EntityManager } from 'typeorm';
-import { User } from '@domain/community/user/user.entity';
-import { Organization } from '@domain/community/organization/organization.entity';
-import { VirtualContributor } from '@domain/community/virtual-contributor/virtual.contributor.entity';
+import { UUID } from '@domain/common/scalars/scalar.uuid';
 
 @Resolver()
 export class AdminSearchContributorsMutations {
@@ -20,17 +16,18 @@ export class AdminSearchContributorsMutations {
     private authorizationService: AuthorizationService,
     private platformAuthorizationPolicyService: PlatformAuthorizationPolicyService,
     private contributorService: ContributorService,
-    @InjectEntityManager('default')
-    private entityManager: EntityManager,
     @Inject(WINSTON_MODULE_NEST_PROVIDER) private logger: LoggerService
   ) {}
 
   @UseGuards(GraphqlGuard)
   @Mutation(() => String, {
     description:
-      'Update all contributor avatars to be stored as Documents. This is an admin only operation to be run once.',
+      'Update the Avatar on the Profile with the spedified profileID to be stored as a Document.',
   })
-  async adminUpdateContributorAvatars(@CurrentUser() agentInfo: AgentInfo) {
+  async adminUpdateContributorAvatars(
+    @CurrentUser() agentInfo: AgentInfo,
+    @Args('profileID', { type: () => UUID }) profileID: string
+  ) {
     const platformPolicy =
       await this.platformAuthorizationPolicyService.getPlatformAuthorizationPolicy();
     this.authorizationService.grantAccessOrFail(
@@ -40,53 +37,9 @@ export class AdminSearchContributorsMutations {
       `Update contributor avatars to be stored as Documents: ${agentInfo.email}`
     );
 
-    const users = await this.entityManager.find(User, {
-      relations: {
-        profile: true,
-      },
-    });
-    for (const user of users) {
-      if (user.profile) {
-        await this.contributorService.ensureAvatarIsStoredInLocalStorageBucket(
-          user.profile,
-          user.id
-        );
-      }
-    }
-    const organizations = await this.entityManager.find(Organization, {
-      relations: {
-        profile: true,
-      },
-    });
-    for (const organization of organizations) {
-      if (organization.profile) {
-        await this.contributorService.ensureAvatarIsStoredInLocalStorageBucket(
-          organization.profile,
-          organization.id
-        );
-      }
-    }
-
-    const virtualContributors = await this.entityManager.find(
-      VirtualContributor,
-      {
-        relations: {
-          profile: true,
-        },
-      }
-    );
-    for (const virtualContributor of virtualContributors) {
-      if (virtualContributor.profile) {
-        await this.contributorService.ensureAvatarIsStoredInLocalStorageBucket(
-          virtualContributor.profile,
-          virtualContributor.id
-        );
-      }
-    }
-
-    this.logger.verbose?.(
-      'Completed updating all contributor avatars',
-      LogContext.COMMUNITY
+    await this.contributorService.ensureAvatarIsStoredInLocalStorageBucket(
+      profileID,
+      agentInfo.userID
     );
   }
 }
