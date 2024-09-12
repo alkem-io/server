1b3f0a20152d366317652069ee69e918ab5c0c4e
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2024-09-12 11:25:19 +0300

    Add defensive code in convergence migration

--- src/migrations/1726060322894-indexConvergence.ts
+++ src/migrations/1726060322894-indexConvergence.ts
@@ -1,17 +1,30 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import { safelyDropIndex } from './utils/safely-drop-index';
+import { safelyAddFK } from './utils/safely-add-foreignKey';
 
-export class Test1726060322894 implements MigrationInterface {
-  name = 'Test1726060322894';
+export class IndexConvergence1726060322894 implements MigrationInterface {
+  name = 'IndexConvergence1726060322894';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      'DROP INDEX `FK_3337f26ca267009fcf514e0e726` ON `document`'
+    await safelyDropIndex(
+      queryRunner,
+      'document',
+      'FK_3337f26ca267009fcf514e0e726'
     );
-    await queryRunner.query(
-      'DROP INDEX `FK_6a30f26ca267009fcf514e0e726` ON `calendar_event`'
+    await safelyDropIndex(
+      queryRunner,
+      'calendar_event',
+      'FK_6a30f26ca267009fcf514e0e726'
     );
-    await queryRunner.query(
-      'DROP INDEX `IDX_0f03c61020ea0dfa0198c60304` ON `activity`'
+    await safelyDropIndex(
+      queryRunner,
+      'calendar_event',
+      'FK_6a30f26ca267009fcf514e0e726'
+    );
+    await safelyDropIndex(
+      queryRunner,
+      'activity',
+      'IDX_0f03c61020ea0dfa0198c60304'
     );
     await queryRunner.query(
       'CREATE INDEX `IDX_0f03c61020ea0dfa0198c60304` ON `activity` (`rowId`)'
@@ -28,15 +41,36 @@ export class Test1726060322894 implements MigrationInterface {
     await queryRunner.query(
       'CREATE UNIQUE INDEX `REL_96a8cbe1706f459fd7d883be9b` ON `innovation_flow` (`profileId`)'
     );
-    await queryRunner.query(
-      'ALTER TABLE `innovation_flow` ADD CONSTRAINT `FK_96a8cbe1706f459fd7d883be9bd` FOREIGN KEY (`profileId`) REFERENCES `profile`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION'
+
+    await safelyAddFK(
+      queryRunner,
+      'innovation_flow', // Table name
+      'FK_96a8cbe1706f459fd7d883be9bd', // Foreign key name
+      'profileId', // Column in this table
+      'profile', // Referenced table
+      'id' // Referenced column
     );
 
-    await queryRunner.query(
-      'ALTER TABLE `application_questions` ADD CONSTRAINT `FK_8495fae86f13836b0745642baa8` FOREIGN KEY (`applicationId`) REFERENCES `application`(`id`) ON DELETE CASCADE ON UPDATE CASCADE'
+    await safelyAddFK(
+      queryRunner,
+      'application_questions', // Table name
+      'FK_8495fae86f13836b0745642baa8', // Foreign key name
+      'applicationId', // Column in this table
+      'application', // Referenced table
+      'id', // Referenced column
+      'CASCADE', // ON  DELETE
+      'CASCADE' // ON UPDATE
     );
-    await queryRunner.query(
-      'ALTER TABLE `application_questions` ADD CONSTRAINT `FK_fe50118fd82e7fe2f74f986a195` FOREIGN KEY (`nvpId`) REFERENCES `nvp`(`id`) ON DELETE CASCADE ON UPDATE CASCADE'
+
+    await safelyAddFK(
+      queryRunner,
+      'application_questions', // Table name
+      'FK_fe50118fd82e7fe2f74f986a195', // Foreign key name
+      'nvpId', // Column in this table
+      'nvp', // Referenced table
+      'id', // Referenced column
+      'CASCADE', // ON  DELETE
+      'CASCADE' // ON UPDATE
     );
   }
 
