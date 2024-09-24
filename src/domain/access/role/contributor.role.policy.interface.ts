3e35777b51ea0a112a8243aaafe6e6fc04d582d2
Neil Smyth <neil@thesmyths.eu>
2024-09-24 21:41:06 +0200

    removed enabled on contributor role policy

--- src/domain/access/role/contributor.role.policy.interface.ts
+++ src/domain/access/role/contributor.role.policy.interface.ts
@@ -2,11 +2,6 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType('ContributorRolePolicy')
 export abstract class IContributorRolePolicy {
-  @Field(() => Boolean, {
-    description: 'Is this role enabled for this Contributor',
-  })
-  enabled!: boolean;
-
   @Field(() => Number, {
     description: 'Minimum number of Contributors in this role',
   })
