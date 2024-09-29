fc4f6e306610643700c3a9dce383c140dd2207d3
Neil Smyth <neil@thesmyths.eu>
2024-09-29 21:21:16 +0200

    removed code based addition of callouts, innovation flow states

--- src/domain/space/space.defaults/definitions/challenge/space.defaults.callout.groups.challenge.ts
+++
@@ -1,9 +0,0 @@
-import { CalloutGroupName } from '@common/enums/callout.group.name';
-import { ICalloutGroup } from '@domain/collaboration/callout-groups/callout.group.interface';
-
-export const spaceDefaultsCalloutGroupsChallenge: ICalloutGroup[] = [
-  {
-    displayName: CalloutGroupName.HOME,
-    description: 'The Subspace Home page.',
-  },
-];
