aaed152c32ac48afd5cefef072d31a756e68af8d
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2024-10-04 12:24:35 +0300

    Fixed migrations on clean / prod db schema

--- src/migrations/1727872794564-cleanupRolesIndexes.ts
+++ src/migrations/1727872794564-cleanupRolesIndexes.ts
@@ -1,4 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import { safelyDropFK } from './utils/safely-drop-foreignKey';
+import { safelyDropIndex } from './utils/safely-drop-index';
+import { safelyAddFK } from './utils/safely-add-foreignKey';
 
 export class cleanupRolesIndexes1727872794564 implements MigrationInterface {
   name = 'cleanupRolesIndexes1727872794564';
@@ -6,40 +9,66 @@ export class cleanupRolesIndexes1727872794564 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     // platform_invitation
     // This is the new FK using the old index
-    await queryRunner.query(
-      `ALTER TABLE \`platform_invitation\` DROP FOREIGN KEY \`FK_562dce4a08bb214f08107b3631e\``
+    safelyDropFK(
+      queryRunner,
+      'platform_invitation',
+      'FK_562dce4a08bb214f08107b3631e'
     );
+
     // This is the old index that needs to be removed
-    await queryRunner.query(
-      `DROP INDEX \`FK_b3d3f3c2ce851d1059c4ed26ba2\` ON \`platform_invitation\`
-    `
+    safelyDropIndex(
+      queryRunner,
+      'platform_invitation',
+      'FK_b3d3f3c2ce851d1059c4ed26ba2'
     );
+
     // This will add the index and the FK back
-    await queryRunner.query(
-      `ALTER TABLE \`platform_invitation\` ADD CONSTRAINT \`FK_562dce4a08bb214f08107b3631e\` FOREIGN KEY (\`roleSetId\`) REFERENCES \`role_set\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION
-    `
+    safelyAddFK(
+      queryRunner,
+      'platform_invitation',
+      'FK_562dce4a08bb214f08107b3631e',
+      'roleSetId',
+      'role_set',
+      'id',
+      'CASCADE',
+      'NO ACTION'
     );
 
     // application
-    await queryRunner.query(
-      `ALTER TABLE \`application\` DROP FOREIGN KEY \`FK_8fb220ad1ac1f9c86ec39d134e4\``
+
+    safelyDropFK(queryRunner, 'application', 'FK_8fb220ad1ac1f9c86ec39d134e4');
+    safelyDropIndex(
+      queryRunner,
+      'application',
+      'FK_500cee6f635849f50e19c7e2b76'
     );
-    await queryRunner.query(
-      `DROP INDEX \`FK_500cee6f635849f50e19c7e2b76\` ON \`application\``
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`application\` ADD CONSTRAINT \`FK_8fb220ad1ac1f9c86ec39d134e4\` FOREIGN KEY (\`roleSetId\`) REFERENCES \`role_set\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+    safelyAddFK(
+      queryRunner,
+      'application',
+      'FK_8fb220ad1ac1f9c86ec39d134e4',
+      'roleSetId',
+      'role_set',
+      'id',
+      'CASCADE',
+      'NO ACTION'
     );
 
     // invitation
-    await queryRunner.query(
-      `ALTER TABLE \`invitation\` DROP FOREIGN KEY \`FK_6a3b86c6db10582baae7058f5b9\``
+    safelyDropFK(queryRunner, 'invitation', 'FK_6a3b86c6db10582baae7058f5b9');
+    safelyDropIndex(
+      queryRunner,
+      'invitation',
+      'FK_339c1fe2a9c5caef5b982303fb0'
     );
-    await queryRunner.query(
-      `DROP INDEX \`FK_339c1fe2a9c5caef5b982303fb0\` ON \`invitation\``
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`invitation\` ADD CONSTRAINT \`FK_6a3b86c6db10582baae7058f5b9\` FOREIGN KEY (\`roleSetId\`) REFERENCES \`role_set\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+    safelyAddFK(
+      queryRunner,
+      'invitation',
+      'FK_6a3b86c6db10582baae7058f5b9',
+      'roleSetId',
+      'role_set',
+      'id',
+      'CASCADE',
+      'NO ACTION'
     );
   }
 
