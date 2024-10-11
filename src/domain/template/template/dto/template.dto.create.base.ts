79c08ab111d1ff346a0e259d1f53f8cded94d1d5
Neil Smyth <neil@thesmyths.eu>
2024-10-11 15:24:44 +0200

    fixed groups in bootstrap space template; updated signature for creating callout from collaboration

--- src/domain/template/template/dto/template.dto.create.base.ts
+++ src/domain/template/template/dto/template.dto.create.base.ts
@@ -1,8 +1,6 @@
-import { SMALL_TEXT_LENGTH } from '@common/constants/entity.field.length.constants';
-import { TemplateType } from '@common/enums/template.type';
 import { CreateNameableInput } from '@domain/common/entity/nameable-entity';
 import { Field, InputType } from '@nestjs/graphql';
-import { IsOptional, MaxLength } from 'class-validator';
+import { IsOptional } from 'class-validator';
 
 @InputType()
 export class CreateTemplateBaseInput extends CreateNameableInput {
@@ -13,11 +11,4 @@ export class CreateTemplateBaseInput extends CreateNameableInput {
   @Field(() => String, { nullable: true })
   @IsOptional()
   visualUri?: string;
-
-  @Field(() => TemplateType, {
-    nullable: false,
-    description: 'The type of the Template to be created.',
-  })
-  @MaxLength(SMALL_TEXT_LENGTH)
-  type!: TemplateType;
 }
