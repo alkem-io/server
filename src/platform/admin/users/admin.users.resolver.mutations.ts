7c4d3c719ffdd2112899d4a7fe62fccbaca41b39
Neil Smyth <neil@thesmyths.eu>
2024-10-15 14:59:55 +0200

    add mutation to allow removing of the account associated with a user

---
+++ src/platform/admin/users/admin.users.resolver.mutations.ts
@@ -0,0 +1,51 @@
+import { Inject, LoggerService, UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CurrentUser } from '@src/common/decorators';
+import { GraphqlGuard } from '@core/authorization/graphql.guard';
+import { AuthorizationPrivilege, LogContext } from '@common/enums';
+import { PlatformAuthorizationPolicyService } from '@platform/authorization/platform.authorization.policy.service';
+import { AgentInfo } from '@core/authentication.agent.info/agent.info';
+import { AuthorizationService } from '@core/authorization/authorization.service';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { UUID } from '@domain/common/scalars/scalar.uuid';
+import { KratosService } from '@services/infrastructure/kratos/kratos.service';
+import { UserService } from '@domain/community/user/user.service';
+import { IUser } from '@domain/community/user/user.interface';
+
+@Resolver()
+export class AdminUsersMutations {
+  constructor(
+    private authorizationService: AuthorizationService,
+    private platformAuthorizationPolicyService: PlatformAuthorizationPolicyService,
+    private kratosService: KratosService,
+    private userService: UserService,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private logger: LoggerService
+  ) {}
+
+  @UseGuards(GraphqlGuard)
+  @Mutation(() => IUser, {
+    description:
+      'Remove the Kratos account associated with the specified User. Note: the Users profile on the platform is not deleted.',
+  })
+  async adminUserAccountDelete(
+    @CurrentUser() agentInfo: AgentInfo,
+    @Args('userID', { type: () => UUID }) userID: string
+  ): Promise<IUser> {
+    const platformPolicy =
+      await this.platformAuthorizationPolicyService.getPlatformAuthorizationPolicy();
+    this.authorizationService.grantAccessOrFail(
+      agentInfo,
+      platformPolicy,
+      AuthorizationPrivilege.PLATFORM_ADMIN,
+      `Remove Kratos account for User ${userID}: ${agentInfo.email}`
+    );
+
+    const user = await this.userService.getUserOrFail(userID);
+    await this.kratosService.deleteIdentityByEmail(user.email);
+    this.logger.verbose?.(
+      `Account associated with User ${user.email} has been deleted`,
+      LogContext.AUTH
+    );
+    return user;
+  }
+}
