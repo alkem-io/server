31312ce21cc76aff00b53ec92a2626831dbdac3e
Carlos Cano <carlos@alkem.io>
2024-11-01 14:00:39 +0100

    Fixed nameId generation

--- src/services/infrastructure/naming/naming.service.ts
+++ src/services/infrastructure/naming/naming.service.ts
@@ -23,6 +23,7 @@ import { SpaceReservedName } from '@common/enums/space.reserved.name';
 import { generateNameId } from '@services/infrastructure/naming/generate.name.id';
 import { Template } from '@domain/template/template/template.entity';
 import { IRoleSet } from '@domain/access/role-set';
+import { NAMEID_MAX_LENGTH } from '@common/constants';
 
 export class NamingService {
   constructor(
@@ -232,7 +233,7 @@ export class NamingService {
     let count = 1;
     while (reservedNameIDs.includes(result)) {
       // If the nameID is already reserved, try again with a new random suffix starting from 1 but with two digits
-      result = `${guess}-${count.toString()}`;
+      result = `${guess.substring(0, NAMEID_MAX_LENGTH - 1 - count.toString().length)}-${count.toString()}`;
       count++;
     }
     return result;
