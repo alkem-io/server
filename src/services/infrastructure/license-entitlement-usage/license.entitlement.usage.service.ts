45f23c98359f04f35bfb93dad740f990a76e29fa
Neil Smyth <neil@thesmyths.eu>
2024-11-05 15:59:09 +0100

    extend lookup of entitlement usage to cover new types

--- src/services/infrastructure/license-entitlement-usage/license.entitlement.usage.service.ts
+++ src/services/infrastructure/license-entitlement-usage/license.entitlement.usage.service.ts
@@ -9,6 +9,7 @@ import {
 import { LogContext } from '@common/enums';
 import { LicenseEntitlementType } from '@common/enums/license.entitlement.type';
 import { Account } from '@domain/space/account/account.entity';
+import { ISpace } from '@domain/space/space/space.interface';
 
 export class LicenseEntitlementUsageService {
   constructor(
@@ -30,7 +31,11 @@ export class LicenseEntitlementUsageService {
         },
       },
       relations: {
-        spaces: true,
+        spaces: {
+          license: {
+            entitlements: true,
+          },
+        },
         virtualContributors: true,
         innovationHubs: true,
         innovationPacks: true,
@@ -44,7 +49,20 @@ export class LicenseEntitlementUsageService {
     }
     switch (entitlementType) {
       case LicenseEntitlementType.ACCOUNT_SPACE_FREE:
-        return account.spaces.length;
+        return this.getAccountSpacesTypeCount(
+          account.spaces,
+          LicenseEntitlementType.SPACE_FREE
+        );
+      case LicenseEntitlementType.ACCOUNT_SPACE_PLUS:
+        return this.getAccountSpacesTypeCount(
+          account.spaces,
+          LicenseEntitlementType.SPACE_PLUS
+        );
+      case LicenseEntitlementType.ACCOUNT_SPACE_PREMIUM:
+        return this.getAccountSpacesTypeCount(
+          account.spaces,
+          LicenseEntitlementType.SPACE_PREMIUM
+        );
       case LicenseEntitlementType.ACCOUNT_VIRTUAL_CONTRIBUTOR:
         return account.virtualContributors.length;
       case LicenseEntitlementType.ACCOUNT_INNOVATION_HUB:
@@ -57,5 +75,34 @@ export class LicenseEntitlementUsageService {
           LogContext.LICENSE
         );
     }
+  }
+
+  private getAccountSpacesTypeCount(
+    spaces: ISpace[],
+    entitlementType: LicenseEntitlementType
+  ): number {
+    const matchingSpaces = spaces.filter(space =>
+      this.hasMatchingLicenseEntitlement(space, entitlementType)
+    );
+    return matchingSpaces.length;
+  }
+
+  private hasMatchingLicenseEntitlement(
+    space: ISpace,
+    entitlementType: LicenseEntitlementType
+  ): boolean {
+    const entitlements = space.license?.entitlements;
+    if (!entitlements) {
+      throw new RelationshipNotFoundException(
+        `Unable to load entitlemets for space: ${space.id}`,
+        LogContext.LICENSE
+      );
+    }
+    for (const entitlement of entitlements) {
+      if (entitlement.type === entitlementType) {
+        return entitlement.enabled;
+      }
+    }
+    return false;
   }
 }
