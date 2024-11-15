e884e3c5dd99a0b313f04e6ae0d2f60de802ac6c
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2024-11-15 11:05:48 +0200

    address comments

--- src/services/room-integration/room.controller.service.ts
+++ src/services/room-integration/room.controller.service.ts
@@ -33,7 +33,7 @@ export class RoomControllerService {
       'virtualContributor'
     );
 
-    //TODO fix me with the expert engine
+    //TODO fix me with the openai assistant engine
     // if (vcInteractionID) {
     //   const vcInteraction =
     //     await this.vcInteractionService.getVcInteractionOrFail(vcInteractionID);
