24dde699398829245da54b1c6b4cdba84091d7eb
Carlos Cano <carlos@alkem.io>
2024-12-13 14:11:50 +0100

    7327 Don't allow nameIds on createSubspaceInput (#4785)
    Co-authored-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/domain/space/space/dto/space.dto.create.subspace.ts
+++ src/domain/space/space/dto/space.dto.create.subspace.ts
@@ -1,9 +1,9 @@
-import { UUID_NAMEID } from '@domain/common/scalars';
+import { UUID } from '@domain/common/scalars';
 import { Field, InputType } from '@nestjs/graphql';
 import { CreateSpaceInput } from './space.dto.create';
 
 @InputType()
 export class CreateSubspaceInput extends CreateSpaceInput {
-  @Field(() => UUID_NAMEID, { nullable: false })
+  @Field(() => UUID, { nullable: false })
   spaceID!: string;
 }
