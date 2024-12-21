71ab7c02baff0668571e496434c6734f0356b201
Neil Smyth <neil@thesmyths.eu>
2024-12-21 09:00:43 +0100

    moved migration to last in teh list

--- src/migrations/1734708463666-authAnonymousReadAccess.ts
+++ src/migrations/1734708463666-authAnonymousReadAccess.ts
@@ -1,9 +1,9 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AuthAnonymousReadAccess1733732413177
+export class AuthAnonymousReadAccess1734708463666
   implements MigrationInterface
 {
-  name = 'AuthAnonymousReadAccess1733732413177';
+  name = 'AuthAnonymousReadAccess1734708463666';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
