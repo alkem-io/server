aed9b188209c1aa785457165912e41617c0a7078
Neil Smyth <neil@thesmyths.eu>
2024-12-28 15:01:39 +0100

    prototype logic for adding KnowledgeBase + associated CalloutsSet to VCs

--- src/migrations/1735321009455-calloutsSet.ts
+++ src/migrations/1735321009455-calloutsSet.ts
@@ -25,6 +25,21 @@ export class CalloutsSet1735321009455 implements MigrationInterface {
                                                 UNIQUE INDEX \`REL_211515f7e21e93136a6b905e84\` (\`tagsetTemplateSetId\`),
                                                 PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
 
+    await queryRunner.query(`CREATE TABLE \`knowledge_base\` (\`id\` char(36) NOT NULL,
+                                                \`createdDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                                                \`updatedDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+                                                \`version\` int NOT NULL, \`authorizationId\` char(36) NULL,
+                                                \`profileId\` char(36) NULL,
+                                                \`calloutsSetId\` char(36) NULL,
+                                                UNIQUE INDEX \`REL_610fe23f4b0c4d8e38f0d0fbf3\` (\`authorizationId\`),
+                                                UNIQUE INDEX \`REL_0e8a8e02916c701eeed6bf866a\` (\`profileId\`),
+                                                UNIQUE INDEX \`REL_970b16bb8c1f8daee6135b00c8\` (\`calloutsSetId\`),
+                                                PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+
+    await queryRunner.query(
+      `ALTER TABLE \`virtual_contributor\` ADD \`knowledgeBaseId\` char(36) NOT NULL`
+    );
+
     await queryRunner.query(
       `ALTER TABLE \`collaboration\` ADD \`calloutsSetId\` char(36) NULL`
     );
@@ -32,8 +47,6 @@ export class CalloutsSet1735321009455 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE \`callout\` ADD \`calloutsSetId\` char(36) NULL`
     );
-
-    // TODO: create definitions for KnowledgeBase, and add column for it to VC. Then iterate over all VCs and create empty KBs etc.
 
     // Create calloutsSet for each collaboration
     const collaborations: {
@@ -58,23 +71,86 @@ export class CalloutsSet1735321009455 implements MigrationInterface {
       );
     }
 
-    await queryRunner.query(
-      `ALTER TABLE \`collaboration\` DROP COLUMN \`groupsStr\``
+    // TODO: create definitions for KnowledgeBase, and add column for it to VC. Then iterate over all VCs and create empty KBs etc.
+    const virtualContributors: {
+      id: string;
+      profileId: string;
+    }[] = await queryRunner.query(
+      `SELECT id, profileId FROM \`virtual_contributor\``
     );
-    await queryRunner.query(
-      `ALTER TABLE \`collaboration\` DROP COLUMN \`tagsetTemplateSetId\``
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`callout\` DROP COLUMN \`collaborationId\``
-    );
+    for (const virtualContributor of virtualContributors) {
+      const [vcProfile]: {
+        id: string;
+        storageBucketId: string;
+      }[] = await queryRunner.query(
+        `SELECT id, storageBucketId FROM profile WHERE id = ?`,
+        [virtualContributor.profileId]
+      );
+      const knowledgeBaseID = randomUUID();
+      const knowledgeBaseAuthID = await this.createAuthorizationPolicy(
+        queryRunner,
+        'knowledge-base'
+      );
+      const vcStorageBucketID = vcProfile.storageBucketId;
 
+      const profileID = await this.createProfile(
+        queryRunner,
+        'Knowledge Base',
+        vcStorageBucketID,
+        'kowledge-base'
+      );
+      // Create an empty tagsetTemplateSet
+      const tagsetTemplateSetID = randomUUID();
+      await queryRunner.query(
+        `INSERT INTO tagset_template_set (id, version) VALUES ('${tagsetTemplateSetID}', 1)`
+      );
+      const calloutsSetID = await this.createCalloutsSet(
+        queryRunner,
+        '[]',
+        tagsetTemplateSetID
+      );
+
+      // Create the knowledge base entity
+      await queryRunner.query(
+        `INSERT INTO knowledge_base (id, version, authorizationId, profileId, calloutsSetId) VALUES
+                    ('${knowledgeBaseID}',
+                    1,
+                    '${knowledgeBaseAuthID}',
+                    '${profileID}',
+                    '${calloutsSetID}')`
+      );
+      // and update the VC to point to the new KB
+      await queryRunner.query(
+        `UPDATE \`virtual_contributor\` SET knowledgeBaseId = '${knowledgeBaseID}' WHERE id = '${virtualContributor.id}'`
+      );
+    }
+
+    await queryRunner.query(
+      `ALTER TABLE \`virtual_contributor\` ADD UNIQUE INDEX \`IDX_409cc6ee5429588f868cd59a1d\` (\`knowledgeBaseId\`)`
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX \`REL_409cc6ee5429588f868cd59a1d\` ON \`virtual_contributor\` (\`knowledgeBaseId\`)`
+    );
     await queryRunner.query(
       `ALTER TABLE \`collaboration\` ADD UNIQUE INDEX \`IDX_9e1ebbc0972fa354d33b67a1a0\` (\`calloutsSetId\`)`
     );
-
     await queryRunner.query(
       `CREATE UNIQUE INDEX \`REL_9e1ebbc0972fa354d33b67a1a0\` ON \`collaboration\` (\`calloutsSetId\`)`
     );
+
+    await queryRunner.query(
+      `ALTER TABLE \`virtual_contributor\` ADD CONSTRAINT \`FK_409cc6ee5429588f868cd59a1de\` FOREIGN KEY (\`knowledgeBaseId\`) REFERENCES \`knowledge_base\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`knowledge_base\` ADD CONSTRAINT \`FK_610fe23f4b0c4d8e38f0d0fbf34\` FOREIGN KEY (\`authorizationId\`) REFERENCES \`authorization_policy\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`knowledge_base\` ADD CONSTRAINT \`FK_0e8a8e02916c701eeed6bf866ad\` FOREIGN KEY (\`profileId\`) REFERENCES \`profile\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`knowledge_base\` ADD CONSTRAINT \`FK_970b16bb8c1f8daee6135b00c82\` FOREIGN KEY (\`calloutsSetId\`) REFERENCES \`callouts_set\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+
     await queryRunner.query(
       `ALTER TABLE \`callout\` ADD CONSTRAINT \`FK_08d3fa19eb35058446dafa99192\` FOREIGN KEY (\`calloutsSetId\`) REFERENCES \`callouts_set\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
     );
@@ -86,6 +162,16 @@ export class CalloutsSet1735321009455 implements MigrationInterface {
     );
     await queryRunner.query(
       `ALTER TABLE \`collaboration\` ADD CONSTRAINT \`FK_9e1ebbc0972fa354d33b67a1a02\` FOREIGN KEY (\`calloutsSetId\`) REFERENCES \`callouts_set\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE \`collaboration\` DROP COLUMN \`groupsStr\``
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`collaboration\` DROP COLUMN \`tagsetTemplateSetId\``
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`callout\` DROP COLUMN \`collaborationId\``
     );
   }
 
@@ -124,53 +210,44 @@ export class CalloutsSet1735321009455 implements MigrationInterface {
     return authID;
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE \`collaboration\` DROP FOREIGN KEY \`FK_9e1ebbc0972fa354d33b67a1a02\``
+  private async createProfile(
+    queryRunner: QueryRunner,
+    entityName: string,
+    siblingStorageBucketID: string,
+    profileType: string
+  ): Promise<string> {
+    const profileId = randomUUID();
+    const authID = await this.createAuthorizationPolicy(queryRunner, 'profile');
+    const profileStorageBucketId = await this.createStorageBucket(
+      queryRunner,
+      siblingStorageBucketID
     );
     await queryRunner.query(
-      `ALTER TABLE \`callouts_set\` DROP FOREIGN KEY \`FK_211515f7e21e93136a6b905e84a\``
+      `INSERT INTO profile (id, version, authorizationId, locationId, displayName, tagline, storageBucketId, type) VALUES
+      ('${profileId}', 1, '${authID}', null, '${entityName} Template', '', '${profileStorageBucketId}', '${profileType}')`
     );
-    await queryRunner.query(
-      `ALTER TABLE \`callouts_set\` DROP FOREIGN KEY \`FK_8f3fd7a83451183166aac4ad02f\``
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`callout\` DROP FOREIGN KEY \`FK_08d3fa19eb35058446dafa99192\``
-    );
-    await queryRunner.query(
-      `DROP INDEX \`REL_9e1ebbc0972fa354d33b67a1a0\` ON \`collaboration\``
+
+    return profileId;
+  }
+
+  private async createStorageBucket(
+    queryRunner: QueryRunner,
+    siblingStorageBucketID: string
+  ): Promise<string> {
+    const newStorageBucketId = randomUUID();
+    const authID = await this.createAuthorizationPolicy(
+      queryRunner,
+      'storage-bucket'
     );
 
     await queryRunner.query(
-      `ALTER TABLE \`collaboration\` DROP INDEX \`IDX_9e1ebbc0972fa354d33b67a1a0\``
+      `INSERT INTO storage_bucket (id, version, authorizationId, allowedMimeTypes, maxFileSize, storageAggregatorId)
+        SELECT '${newStorageBucketId}' as id, 1 as version, '${authID}' as authorizationId, allowedMimeTypes, maxFileSize, storageAggregatorId
+          FROM storage_bucket WHERE id = '${siblingStorageBucketID}'`
     );
-    await queryRunner.query(
-      `ALTER TABLE \`collaboration\` DROP COLUMN \`calloutsSetId\``
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`collaboration\` ADD \`tagsetTemplateSetId\` char(36) NULL`
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`collaboration\` ADD \`groupsStr\` text NOT NULL`
-    );
-    await queryRunner.query(
-      `DROP INDEX \`REL_211515f7e21e93136a6b905e84\` ON \`callouts_set\``
-    );
-    await queryRunner.query(
-      `DROP INDEX \`REL_8f3fd7a83451183166aac4ad02\` ON \`callouts_set\``
-    );
-    await queryRunner.query(`DROP TABLE \`callouts_set\``);
-    await queryRunner.query(
-      `ALTER TABLE \`callout\` CHANGE \`calloutsSetId\` \`collaborationId\` char(36) NULL`
-    );
-    await queryRunner.query(
-      `CREATE UNIQUE INDEX \`REL_b7ece56376ac7ca0b9a56c33b3\` ON \`collaboration\` (\`tagsetTemplateSetId\`)`
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`collaboration\` ADD CONSTRAINT \`FK_b7ece56376ac7ca0b9a56c33b3a\` FOREIGN KEY (\`tagsetTemplateSetId\`) REFERENCES \`tagset_template_set\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
-    );
-    await queryRunner.query(
-      `ALTER TABLE \`callout\` ADD CONSTRAINT \`FK_9b1c5ee044611ac78249194ec35\` FOREIGN KEY (\`collaborationId\`) REFERENCES \`collaboration\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
-    );
+
+    return newStorageBucketId;
   }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
