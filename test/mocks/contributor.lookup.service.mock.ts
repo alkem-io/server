a5f72a1451468e16894189b411c1e7df67912227
Neil Smyth <neil@thesmyths.eu>
2024-12-29 11:30:58 +0100

    .

--- test/mocks/contributor.lookup.service.mock.ts
+++ test/mocks/contributor.lookup.service.mock.ts
@@ -1,11 +1,12 @@
-import { MockValueProvider } from '@test/utils/mock.value.provider';
-import { ConfigService } from '@nestjs/config';
+import { ValueProvider } from '@nestjs/common';
 import { ContributorLookupService } from '@services/infrastructure/contributor-lookup/contributor.lookup.service';
+import { PublicPart } from '@test/utils/public-part';
 
-export const MockContributorLookupService: MockValueProvider<ContributorLookupService> =
-  {
-    provide: ConfigService,
-    useValue: {
-      getContributorsManagedByUser: jest.fn(),
-    },
-  };
+export const MockContributorLookupService: ValueProvider<
+  PublicPart<ContributorLookupService>
+> = {
+  provide: ContributorLookupService,
+  useValue: {
+    getContributorsManagedByUser: jest.fn(),
+  },
+};
