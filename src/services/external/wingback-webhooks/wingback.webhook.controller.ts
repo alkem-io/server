a8e4b10cbfb06683d9647e800022aabc315bed16
Svetoslav <svetoslav@alkem.io>
2025-01-09 17:16:27 +0200

    added contract signed endpoint

--- src/services/external/wingback-webhooks/wingback.webhook.controller.ts
+++ src/services/external/wingback-webhooks/wingback.webhook.controller.ts
@@ -3,8 +3,22 @@ import { Headers } from '@common/decorators';
 
 @Controller('rest/wingback')
 export class WingbackWebhookController {
-  @Post('customer')
-  public customer(@Body() payload: any, @Headers() headers: any): void {
-    console.log('Customer webhook received', payload, headers);
+  // v1.contract.change.completed
+  @Post('contract/changed')
+  public contractChanged(@Body() payload: any, @Headers() headers: any): void {
+    console.log(
+      'Wingback "v1.contract.change.completed" event received',
+      payload,
+      headers
+    );
+  }
+  // v1.contract.signature.completed
+  @Post('contract/signed')
+  public newContract(@Body() payload: any, @Headers() headers: any): void {
+    console.log(
+      'Wingback "v1.contract.signature.completed" event received',
+      payload,
+      headers
+    );
   }
 }
