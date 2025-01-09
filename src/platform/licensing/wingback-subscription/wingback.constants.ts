30586a4f847825f186d8a08762f34ffcbb21fb49
Svetoslav Petkov <svetoslav@alkem.io>
2025-01-09 13:16:23 +0200

    entitlements read from wingback (#4804)
    * entitlements read from wingback
    
    * code readability improvements
    
    * better exception handling
    
    * reverted homeserver.yaml
    
    * rabbit comments
    
    * rabbit comments

---
+++ src/platform/licensing/wingback-subscription/wingback.constants.ts
@@ -0,0 +1,22 @@
+import { LicenseEntitlementType } from '@common/enums/license.entitlement.type';
+
+export enum WingbackFeatureNames {
+  ACCOUNT_SPACE_FREE = 'ACCOUNT_SPACE_FREE',
+  ACCOUNT_VIRTUAL_CONTRIBUTOR = 'ACCOUNT_VIRTUAL_CONTRIBUTOR',
+  ACCOUNT_INNOVATION_HUB = 'ACCOUNT_INNOVATION_HUB',
+  ACCOUNT_INNOVATION_PACK = 'ACCOUNT_INNOVATION_PACK',
+}
+
+export const WingbackFeatureMapping: Record<
+  WingbackFeatureNames,
+  LicenseEntitlementType
+> = {
+  [WingbackFeatureNames.ACCOUNT_SPACE_FREE]:
+    LicenseEntitlementType.ACCOUNT_SPACE_FREE,
+  [WingbackFeatureNames.ACCOUNT_VIRTUAL_CONTRIBUTOR]:
+    LicenseEntitlementType.ACCOUNT_VIRTUAL_CONTRIBUTOR,
+  [WingbackFeatureNames.ACCOUNT_INNOVATION_HUB]:
+    LicenseEntitlementType.ACCOUNT_INNOVATION_HUB,
+  [WingbackFeatureNames.ACCOUNT_INNOVATION_PACK]:
+    LicenseEntitlementType.ACCOUNT_INNOVATION_PACK,
+};
