cfdda126ff8b51e65029437cec7603236836ca45
Svetoslav <svetoslav@alkem.io>
2025-01-09 16:31:11 +0200

    added headers

--- src/services/external/wingback-webhooks/wingback.webhook.controller.ts
+++ src/services/external/wingback-webhooks/wingback.webhook.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Headers } from '@common/decorators';
 
 @Controller('rest/wingback')
 export class WingbackWebhookController {
   @Post('customer')
-  public customer(@Body() payload: any) {
-    console.log('Customer webhook received', payload);
+  public customer(@Body() payload: any, @Headers() headers: any): void {
+    console.log('Customer webhook received', payload, headers);
   }
 }
