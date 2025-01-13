2d6692d4d6e8985a52b4d180a9990fe5b049b242
hero101 <svetoslav@alkem.io>
2025-01-13 15:12:18 +0200

    removed unnecessary auth reset on license reset

--- src/services/external/wingback-webhooks/wingback.webhook.service.ts
+++ src/services/external/wingback-webhooks/wingback.webhook.service.ts
@@ -5,8 +5,6 @@ import { InjectEntityManager } from '@nestjs/typeorm';
 import { EntityManager } from 'typeorm';
 import { Account } from '@domain/space/account/account.entity';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import { AccountAuthorizationService } from '@domain/space/account/account.service.authorization';
-import { AuthorizationPolicyService } from '@domain/common/authorization-policy/authorization.policy.service';
 import { AccountLicenseService } from '@domain/space/account/account.service.license';
 import { LicenseService } from '@domain/common/license/license.service';
 import { LogContext } from '@common/enums';
@@ -15,8 +13,6 @@ import { LogContext } from '@common/enums';
 export class WingbackWebhookService {
   constructor(
     private readonly wingbackManager: WingbackManager,
-    private readonly accountAuthorizationService: AccountAuthorizationService,
-    private readonly authorizationPolicyService: AuthorizationPolicyService,
     private readonly accountLicenseService: AccountLicenseService,
     private readonly licenseService: LicenseService,
     @InjectEntityManager() private entityManager: EntityManager,
@@ -59,9 +55,6 @@ export class WingbackWebhookService {
     }
     // 4. since the contract query returns all the features we can either pass the features to the reset,
     // or let the reset query the contract again
-    const accountAuthorizations =
-      await this.accountAuthorizationService.applyAuthorizationPolicy(account);
-    await this.authorizationPolicyService.saveAll(accountAuthorizations);
     const updatedLicenses = await this.accountLicenseService.applyLicensePolicy(
       account.id
     );
