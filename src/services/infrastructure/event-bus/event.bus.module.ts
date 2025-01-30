b0edd5c394c553455af55738001d108a3d94a40a
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2025-01-30 17:32:07 +0200

    libra flow engine POC

--- src/services/infrastructure/event-bus/event.bus.module.ts
+++ src/services/infrastructure/event-bus/event.bus.module.ts
@@ -98,6 +98,12 @@ import amqplib from 'amqplib';
               routingKey: 'openai-assistant',
               durable: true,
             },
+            {
+              name: eventBusConfig.invoke_engine_libra_flow,
+              exchange: eventBusConfig.exchange,
+              routingKey: 'libra-flow',
+              durable: true,
+            },
           ],
         };
       },
