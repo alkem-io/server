ff6702571ed47b58f2fb0506fbff3cf12c088e1f
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2025-02-05 12:14:03 +0200

    fix privilege

--- src/platform/admin/licensing/admin.licensing.resolver.mutations.ts
+++ src/platform/admin/licensing/admin.licensing.resolver.mutations.ts
@@ -48,7 +48,7 @@ export class AdminLicensingResolverMutations {
     this.authorizationService.grantAccessOrFail(
       agentInfo,
       account.authorization,
-      AuthorizationPrivilege.TRANSFER_RESOURCE_ACCEPT,
+      AuthorizationPrivilege.TRANSFER_RESOURCE_OFFER, //toDo - fix with proper privilege
       `create Wingback account for account (${accountID})`
     );
 
