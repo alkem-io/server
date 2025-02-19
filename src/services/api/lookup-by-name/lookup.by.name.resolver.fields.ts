1cfeee47203c91233d6d32972ca6867adcba05df
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2025-02-19 17:30:16 +0200

    uniform lookup behavior for spaces

--- src/services/api/lookup-by-name/lookup.by.name.resolver.fields.ts
+++ src/services/api/lookup-by-name/lookup.by.name.resolver.fields.ts
@@ -84,12 +84,13 @@ export class LookupByNameResolverFields {
     @Args('NAMEID', { type: () => NameID }) nameid: string
   ): Promise<ISpace> {
     const space = await this.spaceService.getSpaceByNameIdOrFail(nameid);
-    this.authorizationService.grantAccessOrFail(
-      agentInfo,
-      space.authorization,
-      AuthorizationPrivilege.READ,
-      `lookup L0 Space by NameID: ${space.id}`
-    );
+    //toDo fix this, but for now that is uniform with lookupByID
+    // this.authorizationService.grantAccessOrFail(
+    //   agentInfo,
+    //   space.authorization,
+    //   AuthorizationPrivilege.READ,
+    //   `lookup L0 Space by NameID: ${space.id}`
+    // );
 
     return space;
   }
