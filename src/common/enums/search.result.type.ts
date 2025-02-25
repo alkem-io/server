7a9f940539e6e86c38c7f3af23b13c1edcbe0e53
hero101 <svetoslav@alkem.io>
2025-02-25 14:55:43 +0200

    removed legacy types from search v1

--- src/common/enums/search.result.type.ts
+++ src/common/enums/search.result.type.ts
@@ -3,11 +3,8 @@ import { registerEnumType } from '@nestjs/graphql';
 export enum SearchResultType {
   SPACE = 'space',
   SUBSPACE = 'subspace',
-  CHALLENGE = 'challenge', // todo remove
-  OPPORTUNITY = 'opportunity', // todo remove
   USER = 'user',
   ORGANIZATION = 'organization',
-  USERGROUP = 'usergroup',
   POST = 'post',
   CALLOUT = 'callout',
   WHITEBOARD = 'whiteboard',
