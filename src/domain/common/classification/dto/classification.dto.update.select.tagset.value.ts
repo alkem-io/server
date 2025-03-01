8a0c4ea4d1d84ba9d0d489fe9cb10d7166ea265c
Neil Smyth <neil@thesmyths.eu>
2025-03-01 23:50:24 +0100

    fixed input type for updating classification tagset

--- src/domain/common/classification/dto/classification.dto.update.select.tagset.value.ts
+++ src/domain/common/classification/dto/classification.dto.update.select.tagset.value.ts
@@ -1,7 +1,14 @@
+import { UUID } from '@domain/common/scalars/scalar.uuid';
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
 export class UpdateClassificationSelectTagsetValueInput {
+  @Field(() => UUID, { nullable: false })
   classificationID!: string;
 
+  @Field(() => String, { nullable: false })
   tagsetName!: string;
 
+  @Field(() => String, { nullable: false })
   selectedValue!: string;
 }
