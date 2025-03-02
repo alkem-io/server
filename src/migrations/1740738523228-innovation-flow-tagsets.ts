82da9de21ec4fc70a410292f8a1093d544df5186
Neil Smyth <neil@thesmyths.eu>
2025-03-02 09:38:13 +0100

    wip

--- src/migrations/1740738523228-innovation-flow-tagsets.ts
+++ src/migrations/1740738523228-innovation-flow-tagsets.ts
@@ -16,6 +16,48 @@ export class InnovationFlowTagsets1740738523228 implements MigrationInterface {
       `ALTER TABLE \`innovation_flow\` ADD UNIQUE INDEX \`IDX_858fd06a671b804765d91251e6\` (\`flowStatesTagsetTemplateId\`)`
     );
 
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX \`REL_858fd06a671b804765d91251e6\` ON \`innovation_flow\` (\`flowStatesTagsetTemplateId\`)`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`innovation_flow\` ADD CONSTRAINT \`FK_858fd06a671b804765d91251e6c\` FOREIGN KEY (\`flowStatesTagsetTemplateId\`) REFERENCES \`tagset_template\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+
+    await queryRunner.query(
+      `CREATE TABLE \`classification\` (\`id\` char(36) NOT NULL,
+                                        \`createdDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                                        \`updatedDate\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+                                        \`version\` int NOT NULL,
+                                        \`authorizationId\` char(36) NULL,
+                                        UNIQUE INDEX \`REL_42422fc4b9dfe4424046f12d8f\` (\`authorizationId\`),
+                                        PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`callouts_set\` DROP COLUMN \`groups\``
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`tagset\` ADD \`classificationId\` char(36) NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`callout\` ADD \`classificationId\` char(36) NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`callout\` ADD UNIQUE INDEX \`IDX_0674c137336c2417df036053b6\` (\`classificationId\`)`
+    );
+
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX \`REL_0674c137336c2417df036053b6\` ON \`callout\` (\`classificationId\`)`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`classification\` ADD CONSTRAINT \`FK_42422fc4b9dfe4424046f12d8fd\` FOREIGN KEY (\`authorizationId\`) REFERENCES \`authorization_policy\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`tagset\` ADD CONSTRAINT \`FK_391d124a58a845b85a047acc9d3\` FOREIGN KEY (\`classificationId\`) REFERENCES \`classification\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`callout\` ADD CONSTRAINT \`FK_0674c137336c2417df036053b65\` FOREIGN KEY (\`classificationId\`) REFERENCES \`classification\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`
+    );
+
     // Add a Classification entity to all callouts. This will hold any tagsets for classification.
     // NOTE: need to do this separately from CalloutsSet as have Callouts in Templates
     const callouts: {
@@ -30,6 +72,8 @@ export class InnovationFlowTagsets1740738523228 implements MigrationInterface {
       );
     }
 
+    throw new Error('Migration not complete');
+
     // Migrate all innovation flows to have the current state and the flow states tagset
     await this.updateInnovationFlows(queryRunner);
 
@@ -39,7 +83,7 @@ export class InnovationFlowTagsets1740738523228 implements MigrationInterface {
       id: string;
       tagsetTemplateSetId: string;
     }[] = await queryRunner.query(
-      `SELECT id, tagsetTemplateSetId, calloutFramingId FROM \`callout_set\``
+      `SELECT id, tagsetTemplateSetId FROM \`callouts_set\``
     );
     for (const calloutsSet of calloutsSets) {
       const callouts: {
@@ -49,20 +93,19 @@ export class InnovationFlowTagsets1740738523228 implements MigrationInterface {
         groupTagsetId: string;
       }[] = await queryRunner.query(
         `SELECT
-          callout.id as id,
-          callout.classificationId as calloutClassificationId,
-          flowTagset.id as flowTagsetId,
-          flowTagset.tags as flowTagsetTags,
-          groupTagset.id as groupTagsetId,
-          groupTagset.tags as groupTagsetTags
-        FROM callout
-          LEFT JOIN calloutFraming ON callout.calloutFramingId = calloutFraming.id
-          LEFT JOIN profile ON calloutFraming.profileId = profile.id
-          LEFT JOIN flowTagset ON tagset.profileId = profile.id AND tagset.name = '${this.TAGSET_FLOW}'
-          LEFT JOIN groupTagset ON tagset.profileId = profile.id AND tagset.name = '${this.TAGSET_GROUP}'
-        WHERE calloutsSetId='${calloutsSet.id}';
-    `
+            callout.id AS id,
+            callout.classificationId AS calloutClassificationId,
+            flowTagset.id AS flowTagsetId,
+            groupTagset.id AS groupTagsetId
+          FROM callout
+            LEFT JOIN callout_framing ON callout.framingId = callout_framing.id
+            LEFT JOIN profile ON callout_framing.profileId = profile.id
+            LEFT JOIN tagset AS flowTagset ON flowTagset.profileId = profile.id AND flowTagset.name = '${this.TAGSET_FLOW}'
+            LEFT JOIN tagset AS groupTagset ON groupTagset.profileId = profile.id AND groupTagset.name = '${this.TAGSET_GROUP}'
+          WHERE callout.calloutsSetId = '${calloutsSet.id}';
+        `
       );
+      console.log(`Migration: ${JSON.stringify(callouts)}`);
       // Move the tagsets to the new classification
       for (const callout of callouts) {
         if (callout.flowTagsetId) {
@@ -341,28 +384,29 @@ export class InnovationFlowTagsets1740738523228 implements MigrationInterface {
       calloutsSetId: string;
       tagsetTemplateSetId: string;
       innovationFlowProfileId: string;
-      innovationFlowStates: Array<{ displayName: string; description: string }>; // json
+      innovationFlowStates: Array<{ displayName: string; description: string }>; // JSON
       innovationFlowTagsetId: string | undefined;
       flowSelectedValue: string | undefined;
       flowTagsetTemplateId: string | undefined;
     }[] = await queryRunner.query(
       `SELECT
-          collaboration.id as collaborationId,
-          collaboration.innovationFlowId as innovationFlowId,
-          collaboration.calloutsSetId as calloutsSetId,
-          calloutsSet.tagsetTemplateSetId as tagsetTemplateSetId,
-          innovationFlow.profileId as innovationFlowProfileId,
-          innovationFlow.states as innovationFlowStates,
-          flowTagset.id as innovationFlowTagsetId,
-          flowTagset.tags as flowSelectedValue,
-          flowTagsetTemplate.id as flowTagsetTemplateId,
+          collaboration.id AS collaborationId,
+          collaboration.innovationFlowId AS innovationFlowId,
+          collaboration.calloutsSetId AS calloutsSetId,
+          callouts_set.tagsetTemplateSetId AS tagsetTemplateSetId,
+          innovation_flow.profileId AS innovationFlowProfileId,
+          innovation_flow.states AS innovationFlowStates,
+          flowTagset.id AS innovationFlowTagsetId,
+          flowTagset.tags AS flowSelectedValue,
+          flowTagsetTemplate.id AS flowTagsetTemplateId
         FROM collaboration
-          LEFT JOIN calloutsSet ON collaboration.calloutsSetId = callouts_set.id
-          LEFT JOIN innovationFlow ON innovation_flow.id = collaboration.innovationFlowId
-          LEFT JOIN profile on innovationFlow.profileId = profile.id
-          LEFT JOIN flowTagset on tagset.profileId = profile.id AND tagset.name = '${this.TAGSET_FLOW}'
-          LEFT JOIN flowTagsetTemplate on tagset_template.tagsetTemplateSetId = calloutsSet.tagsetTemplateSetId AND tagset_template.name = '${this.TAGSET_FLOW}'
-          `
+        LEFT JOIN callouts_set ON collaboration.calloutsSetId = callouts_set.id
+        LEFT JOIN innovation_flow ON innovation_flow.id = collaboration.innovationFlowId
+        LEFT JOIN profile ON innovation_flow.profileId = profile.id
+        LEFT JOIN tagset AS flowTagset ON flowTagset.profileId = profile.id AND flowTagset.name = '${this.TAGSET_FLOW}'
+        LEFT JOIN tagset_template AS flowTagsetTemplate ON flowTagsetTemplate.tagsetTemplateSetId = callouts_set.tagsetTemplateSetId
+            AND flowTagsetTemplate.name = '${this.TAGSET_FLOW}'
+      `
     );
     for (const collaboration of collaborations) {
       if (
@@ -443,13 +487,11 @@ export class InnovationFlowTagsets1740738523228 implements MigrationInterface {
     await queryRunner.query(`INSERT INTO classification (
                                   id,
                                   version,
-                                  name,
                                   authorizationId
                             ) VALUES
                           (
                           '${classificationId}',
                           1,
-                          'flow-state',
                           '${authorizationID}'
                           )`);
     return classificationId;
