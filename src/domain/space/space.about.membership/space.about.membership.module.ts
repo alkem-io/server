655c38e5824d60f8301f0e77c97620264595bea1
Carlos Cano <carlos@alkem.io>
2025-03-18 11:28:43 +0100

    Added communityGuidelines to the about

--- src/domain/space/space.about.membership/space.about.membership.module.ts
+++ src/domain/space/space.about.membership/space.about.membership.module.ts
@@ -5,9 +5,15 @@ import { SpaceAboutMembershipService } from './space.about.membership.service';
 import { RoleSetModule } from '@domain/access/role-set/role.set.module';
 import { SpaceAboutMembershipResolverFields } from './space.about.membership.resolver.fields';
 import { RoleSetMembershipStatusDataLoader } from '@domain/access/role-set/role.set.data.loader.membership.status';
+import { CommunityModule } from '@domain/community/community/community.module';
 
 @Module({
-  imports: [AuthorizationPolicyModule, AuthorizationModule, RoleSetModule],
+  imports: [
+    AuthorizationPolicyModule,
+    AuthorizationModule,
+    RoleSetModule,
+    CommunityModule,
+  ],
   providers: [
     SpaceAboutMembershipResolverFields,
     SpaceAboutMembershipService,
