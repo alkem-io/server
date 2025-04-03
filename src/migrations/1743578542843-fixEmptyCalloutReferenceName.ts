208d537431b86fb83ad17681937ccecd74b6c89e
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2025-04-03 10:54:04 +0300

    address pr comments

--- src/migrations/1743578542843-fixEmptyCalloutReferenceName.ts
+++ src/migrations/1743578542843-fixEmptyCalloutReferenceName.ts
@@ -6,25 +6,12 @@ export class FixEmptyCalloutReferenceName1743578542843
   private readonly DEFAULT_NAME = 'default'; // Define your default name here
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // First, find all references that match the criteria
-    const referencesToUpdate: { id: string }[] = await queryRunner.query(
-      `SELECT r.id as id
-         FROM reference AS r
-         JOIN profile AS p ON r.profileId = p.id
-         JOIN callout_framing AS cf ON cf.profileId = p.id
-         WHERE r.name = ''`
-    );
-
-    if (referencesToUpdate && referencesToUpdate.length > 0) {
-      const referenceIDsToUpdate = referencesToUpdate.map(ref => ref.id);
-      // Update the references with the default name
-      await queryRunner.query(
-        `UPDATE reference
+    await queryRunner.query(
+      `UPDATE reference
              SET name = ?
-             WHERE name = '' AND id IN (?)`,
-        [this.DEFAULT_NAME, referenceIDsToUpdate]
-      );
-    }
+             WHERE name = ''`,
+      [this.DEFAULT_NAME]
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
