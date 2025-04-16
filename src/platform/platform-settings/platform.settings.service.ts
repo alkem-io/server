bf623d224bc476149b231b7cd3b2978ed9d0a15c
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2025-04-16 17:30:03 +0300

    Guards against uninitialized integration settings
    Adds a check to ensure that integration settings are initialized
    before attempting to access them. This prevents potential errors
    and ensures the application behaves predictably.

--- src/platform/platform-settings/platform.settings.service.ts
+++ src/platform/platform-settings/platform.settings.service.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { IPlatformSettings } from './platform.settings.interface';
 import { UpdatePlatformSettingsInput } from './dto/platform.settings.dto.update';
+import { EntityNotInitializedException } from '@common/exceptions';
+import { LogContext } from '@common/enums';
 
 @Injectable()
 export class PlatformSettingsService {
@@ -32,7 +34,12 @@ export class PlatformSettingsService {
     settings: IPlatformSettings,
     iframeAllowedURL: string
   ): string[] {
-    const currentUrls = settings.integration?.iframeAllowedUrls || [];
+    if (!settings.integration)
+      throw new EntityNotInitializedException(
+        'Settings  not initialized',
+        LogContext.PLATFORM
+      );
+    const currentUrls = settings.integration?.iframeAllowedUrls;
 
     // Only add if not already present
     if (!currentUrls.includes(iframeAllowedURL)) {
@@ -46,7 +53,12 @@ export class PlatformSettingsService {
     settings: IPlatformSettings,
     iframeAllowedURL: string
   ): string[] {
-    const currentUrls = settings.integration?.iframeAllowedUrls || [];
+    if (!settings.integration)
+      throw new EntityNotInitializedException(
+        'Settings  not initialized',
+        LogContext.PLATFORM
+      );
+    const currentUrls = settings.integration?.iframeAllowedUrls;
 
     const updatedUrls = currentUrls.filter(url => url !== iframeAllowedURL);
 
