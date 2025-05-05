8711d5793fb342510a9add5b0de6f1726e96dc7e
Neil Smyth <neil@thesmyths.eu>
2025-05-05 07:30:48 +0200

    moved migration to be latest

--- src/migrations/1745836284799-paginatedVCs.ts
+++ src/migrations/1745836284799-paginatedVCs.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class PaginatedVCs1743965565677 implements MigrationInterface {
-  name = 'PaginatedVCs1743965565677';
+export class PaginatedVCs1745836284799 implements MigrationInterface {
+  name = 'PaginatedVCs1745836284799';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
