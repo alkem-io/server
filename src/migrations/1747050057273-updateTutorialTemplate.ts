07658131be2741f9d71068dca2c2b0d2bd561b50
Carlos Cano <carlos@alkem.io>
2025-05-12 15:03:23 +0200

    addressed comments

--- src/migrations/1747050057273-updateTutorialTemplate.ts
+++ src/migrations/1747050057273-updateTutorialTemplate.ts
@@ -5,8 +5,8 @@ const PLATFORM_SPACE_TUTORIALS = 'platform-space-tutorials'; // = enum at src/co
 export class UpdateTutorialTemplate1747050057273 implements MigrationInterface {
   private findTemplateProfileId = async (
     queryRunner: QueryRunner
-  ): Promise<string | null> => {
-    const templates: { id: string }[] = await queryRunner.query(`
+  ): Promise<string> => {
+    const templateProfileIds: { id: string }[] = await queryRunner.query(`
       SELECT profile.id FROM template
         JOIN collaboration ON collaboration.id = template.collaborationId
         JOIN callout ON callout.calloutsSetId = collaboration.calloutsSetId
@@ -19,15 +19,12 @@ export class UpdateTutorialTemplate1747050057273 implements MigrationInterface {
       )
       AND profile.description LIKE 'Done with the tutorials and ready to%';
     `);
-    if (templates.length === 0) {
-      console.warn('No templates to update');
-      return null;
-    } else if (templates.length > 1) {
+    if (templateProfileIds.length !== 1) {
       throw new Error(
-        `Found ${templates.length} templates to update, expected only one.`
+        `Found ${templateProfileIds.length} templates to update, expected one.`
       );
     } else {
-      return templates[0].id;
+      return templateProfileIds[0].id;
     }
   };
 
@@ -47,23 +44,19 @@ export class UpdateTutorialTemplate1747050057273 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     // Updates the text of certain callout in the platform template to create a space with tutorials
     const profileId = await this.findTemplateProfileId(queryRunner);
-    if (profileId) {
-      await this.updateDescription(
-        queryRunner,
-        profileId,
-        'Done with the tutorials and ready to build up this Space/Subspace your way? You can move the tutorials or delete them completely. \n\n*   To move in a Space:\n\n    *   Go to the settings using the ⚙️ icon on the top right of the space \n    *   Go to the LAYOUT tab \n    *   Drag the tool to the page you want\n\n* To move in a Subspace:\n\n    *   Go to the innovation flow and click on the icon to manage the flow \n    *   Drag the tool to the phase you want\n\n*   To remove:\n\n    *   Click on the ⚙️ icon on the block with the tutorial > Delete\n    *   Confirm \n\n You can always find the tutorials in the [tutorials template pack](https://alkem.io/innovation-packs/newspace) and in the [documentation](https://alkem.io/docs/how-to/tutorials.en-US).'
-      );
-    }
+    await this.updateDescription(
+      queryRunner,
+      profileId,
+      'Done with the tutorials and ready to build up this Space/Subspace your way? You can move the tutorials or delete them completely. \n\n*   To move in a Space:\n\n    *   Go to the settings using the ⚙️ icon on the top right of the space \n    *   Go to the LAYOUT tab \n    *   Drag the tool to the page you want\n\n* To move in a Subspace:\n\n    *   Go to the innovation flow and click on the icon to manage the flow \n    *   Drag the tool to the phase you want\n\n*   To remove:\n\n    *   Click on the ⚙️ icon on the block with the tutorial > Delete\n    *   Confirm \n\n You can always find the tutorials in the [tutorials template pack](https://alkem.io/innovation-packs/newspace) and in the [documentation](https://alkem.io/docs/how-to/tutorials.en-US).'
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     const profileId = await this.findTemplateProfileId(queryRunner);
-    if (profileId) {
-      await this.updateDescription(
-        queryRunner,
-        profileId,
-        "Done with the tutorials and ready to build up this Space your way? You can move the tutorials to your knowledge base or delete them completely.\n\n*   To move:\n\n    *   Click on the ⚙️ icon on the block with the tutorial > Edit\n    *   Scroll down to 'Location'\n    *   Select 'Knowledge Base' or any other page\n\n*   To remove:\n\n    *   Click on the ⚙️ icon on the block with the tutorial > Delete\n    *   Confirm"
-      );
-    }
+    await this.updateDescription(
+      queryRunner,
+      profileId,
+      "Done with the tutorials and ready to build up this Space your way? You can move the tutorials to your knowledge base or delete them completely.\n\n*   To move:\n\n    *   Click on the ⚙️ icon on the block with the tutorial > Edit\n    *   Scroll down to 'Location'\n    *   Select 'Knowledge Base' or any other page\n\n*   To remove:\n\n    *   Click on the ⚙️ icon on the block with the tutorial > Delete\n    *   Confirm"
+    );
   }
 }
