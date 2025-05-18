ebb48894b12e2b133da369f73b58704de2eb1b88
Neil Smyth <neil@thesmyths.eu>
2025-05-18 09:40:36 +0200

    added model card module to ai persona

--- src/domain/community/ai-persona-model-card/ai.persona.model.card.module.ts
+++ src/domain/community/ai-persona-model-card/ai.persona.model.card.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthorizationModule } from '@core/authorization/authorization.module';
 import { AuthorizationPolicyModule } from '@domain/common/authorization-policy/authorization.policy.module';
-import { AiPersonaModelCardService } from './ai.persona.model.card.service';
 import { AiPersonaModelCardResolverFields } from './ai.persona.model.card.resolver.fields';
 
 @Module({
   imports: [AuthorizationPolicyModule, AuthorizationModule],
-  providers: [AiPersonaModelCardResolverFields, AiPersonaModelCardService],
-  exports: [AiPersonaModelCardService],
+  providers: [AiPersonaModelCardResolverFields],
+  exports: [],
 })
 export class AiPersonaModelCardModule {}
