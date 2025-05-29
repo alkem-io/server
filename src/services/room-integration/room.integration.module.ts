8ff670da64f05652912f6ac724a964bb86b7bb9d
Neil Smyth <neil@thesmyths.eu>
2025-05-29 19:59:30 +0200

    removed existing forwardRefs on way to tracking the circular dependency down

--- src/services/room-integration/room.integration.module.ts
+++ src/services/room-integration/room.integration.module.ts
@@ -1,17 +1,11 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { RoomControllerService } from './room.controller.service';
 import { SubscriptionServiceModule } from '@services/subscriptions/subscription-service';
 import { VcInteractionModule } from '@domain/communication/vc-interaction/vc.interaction.module';
 import { RoomLookupModule } from '@domain/communication/room-lookup/room.lookup.module';
-import { RoomModule } from '@domain/communication/room/room.module';
 
 @Module({
-  imports: [
-    RoomLookupModule,
-    VcInteractionModule,
-    SubscriptionServiceModule,
-    forwardRef(() => RoomModule),
-  ],
+  imports: [RoomLookupModule, VcInteractionModule, SubscriptionServiceModule],
   providers: [RoomControllerService],
   exports: [RoomControllerService],
 })
