8923e808438264166d865ef9844259f56cda3427
Neil Smyth <neil@thesmyths.eu>
2025-05-29 20:02:24 +0200

    tidy up imports on template content space

--- src/domain/template/template-content-space/template.content.space.module.ts
+++ src/domain/template/template-content-space/template.content.space.module.ts
@@ -1,16 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollaborationModule } from '@domain/collaboration/collaboration/collaboration.module';
-import { ProfileModule } from '@domain/common/profile/profile.module';
-import { CommunityModule } from '@domain/community/community/community.module';
-import { PlatformAuthorizationPolicyModule } from '@platform/authorization/platform.authorization.policy.module';
-import { NamingModule } from '@services/infrastructure/naming/naming.module';
-import { LicensingCredentialBasedModule } from '@platform/licensing/credential-based/licensing-credential-based-entitlements-engine/licensing.credential.based.module';
-import { LicenseIssuerModule } from '@platform/licensing/credential-based/license-credential-issuer/license.issuer.module';
-import { InputCreatorModule } from '@services/api/input-creator/input.creator.module';
-import { LicensingFrameworkModule } from '@platform/licensing/credential-based/licensing-framework/licensing.framework.module';
-import { LicenseModule } from '@domain/common/license/license.module';
-import { SpaceSettingsModule } from '@domain/space/space.settings/space.settings.module';
 import { TemplateContentSpace } from './template.content.space.entity';
 import { AuthorizationModule } from '@core/authorization/authorization.module';
 import { SpaceAboutModule } from '@domain/space/space.about/space.about.module';
@@ -19,21 +9,13 @@ import { TemplateContentSpaceAuthorizationService } from './template.content.spa
 import { TemplateContentSpaceLicenseService } from './template.content.space.service.license';
 import { TemplateContentSpaceResolverFields } from './template.content.space.resolver.fields';
 import { TemplateContentSpaceResolverMutations } from './template.content.space.resolver.mutations';
+import { LicenseModule } from '@domain/common/license/license.module';
 
 @Module({
   imports: [
     AuthorizationModule,
     SpaceAboutModule,
-    CommunityModule,
-    ProfileModule,
-    LicensingFrameworkModule,
-    LicenseIssuerModule,
-    LicensingCredentialBasedModule,
-    NamingModule,
-    PlatformAuthorizationPolicyModule,
-    SpaceSettingsModule,
     CollaborationModule,
-    InputCreatorModule,
     LicenseModule,
     TypeOrmModule.forFeature([TemplateContentSpace]),
   ],
