7fb27d264eb96a952db466d5dbf452ff2d44a377
Neil Smyth <neil@thesmyths.eu>
2025-06-12 17:37:27 +0200

    migration to add field

---
+++ src/migrations/1749742549395-geoLocation.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class GeoLocation1749742549395 implements MigrationInterface {
+  name = 'GeoLocation1749742549395';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`location\` ADD \`geoLocation\` json NOT NULL`
+    );
+    await queryRunner.query(
+      `UPDATE \`location\` SET geoLocation = '{"latitude": 0, "longitude": 0, "isValid": false}'`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`location\` DROP COLUMN \`geoLocation\``
+    );
+  }
+}
