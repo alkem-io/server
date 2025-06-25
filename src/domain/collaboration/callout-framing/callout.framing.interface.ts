625e1d611dc4d9dd076414c5cb47148444b6012b
Carlos Cano <carlos@alkem.io>
2025-06-25 15:30:39 +0200

    Add missing graphql field. CalloutFraming.type

--- src/domain/collaboration/callout-framing/callout.framing.interface.ts
+++ src/domain/collaboration/callout-framing/callout.framing.interface.ts
@@ -1,6 +1,6 @@
 import { IAuthorizable } from '@domain/common/entity/authorizable-entity/authorizable.interface';
 import { IProfile } from '@domain/common/profile/profile.interface';
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { IWhiteboard } from '@domain/common/whiteboard/whiteboard.interface';
 import { CalloutFramingType } from '@common/enums/callout.framing.type';
 
@@ -8,6 +8,10 @@ import { CalloutFramingType } from '@common/enums/callout.framing.type';
 export abstract class ICalloutFraming extends IAuthorizable {
   profile!: IProfile;
 
+  @Field(() => CalloutFramingType, {
+    description:
+      'The type of the Callout Framing, the additional content attached to this callout',
+  })
   type!: CalloutFramingType;
 
   whiteboard?: IWhiteboard;
