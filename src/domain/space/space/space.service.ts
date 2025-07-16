3501c1db13cdb77190f85f2c44a4bb7b29801989
Carlos Cano <carlos@alkem.io>
2025-07-16 21:36:42 +0200

    Fix callouts in subspaces in the template not being created on subspaces when using a template (#5241)

--- src/domain/space/space/space.service.ts
+++ src/domain/space/space/space.service.ts
@@ -246,6 +246,7 @@ export class SpaceService {
             },
           },
           collaborationData: {
+            addCallouts: spaceData.collaborationData.addCallouts,
             calloutsSetData: {},
           },
         };
