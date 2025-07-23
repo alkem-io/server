b495040886ae843b7b511c2415095f1f80605790
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2025-07-23 16:20:45 +0300

    fix asyncFilter to handle rejections
    Signed-off-by: Valentin Yanakiev <valentin.yanakiev@gmail.com>

--- src/common/utils/async.filter.ts
+++ src/common/utils/async.filter.ts
@@ -1,14 +1,7 @@
-export const asyncFilter = <T>(
+export const asyncFilter = async <T>(
   originalArray: Array<T>,
   predicate: (value: T) => Promise<boolean>
 ): Promise<Array<T>> => {
-  return new Promise(resolve => {
-    const booleanArr: Array<Promise<boolean>> = [];
-    originalArray.forEach(e => booleanArr.push(predicate(e)));
-
-    Promise.all(booleanArr).then(booleanArr => {
-      const filteredArray = originalArray.filter((e, i) => booleanArr[i]);
-      resolve(filteredArray);
-    });
-  });
+  const results = await Promise.all(originalArray.map(predicate));
+  return originalArray.filter((_, i) => results[i]);
 };
