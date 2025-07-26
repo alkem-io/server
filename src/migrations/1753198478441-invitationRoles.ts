64ee5f5d3cfc005bbe705ddfca8257919e41c657
Neil Smyth <neil@thesmyths.eu>
2025-07-26 09:33:59 +0200

    migration to update data

---
+++ src/migrations/1753198478441-invitationRoles.ts
@@ -0,0 +1,71 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InvitationRoles1753198478441 implements MigrationInterface {
+  name = 'InvitationRoles1753198478441';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`invitation\` ADD \`extraRoles\` text NOT NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`platform_invitation\` ADD \`roleSetExtraRoles\` text NOT NULL`
+    );
+
+    // Iterate over all invitations and migrate the extra role specified to the array
+    const invitations: {
+      id: string;
+      extraRole: string | null;
+    }[] = await queryRunner.query(`SELECT id, extraRole FROM \`invitation\``);
+    for (const invitation of invitations) {
+      let extraRoles: string[] = [];
+      if (invitation.extraRole) {
+        extraRoles = [invitation.extraRole];
+      }
+      await queryRunner.query(
+        `UPDATE \`invitation\` SET \`extraRoles\` = '${extraRoles}' WHERE \`id\` = ?`,
+        [invitation.id]
+      );
+    }
+
+    // Iterate over all platform invitations + migrate the data
+    const platformInvitations: {
+      id: string;
+      extraRole: string | null;
+    }[] = await queryRunner.query(
+      `SELECT id, roleSetExtraRole FROM \`platform_invitation\``
+    );
+    for (const platformInvitation of platformInvitations) {
+      let extraRoles: string[] = [];
+      if (platformInvitation.extraRole) {
+        extraRoles = [platformInvitation.extraRole];
+      }
+      await queryRunner.query(
+        `UPDATE \`platform_invitation\` SET \`roleSetExtraRoles\` = '${extraRoles}' WHERE \`id\` = ?`,
+        [platformInvitation.id]
+      );
+    }
+
+    await queryRunner.query(
+      `ALTER TABLE \`platform_invitation\` DROP COLUMN \`roleSetExtraRoles\``
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`invitation\` DROP COLUMN \`extraRoles\``
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`platform_invitation\` ADD \`roleSetExtraRoles\` varchar(128) NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`invitation\` ADD \`extraRoles\` varchar(128) NULL`
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE \`invitation\` DROP COLUMN \`extraRoles\``
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`platform_invitation\` DROP COLUMN \`roleSetExtraRoles\``
+    );
+  }
+}
