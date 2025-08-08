72932276d51176979a14506acf175d17ddc79e5b
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2025-08-08 16:17:33 +0300

    Updates GraphQL field types to Int
    Ensures that fields representing counts or quantities
    are correctly represented as integers in the GraphQL schema.

--- src/domain/space/account.license.plan/account.license.plan.interface.ts
+++ src/domain/space/account.license.plan/account.license.plan.interface.ts
@@ -1,37 +1,37 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, Int } from '@nestjs/graphql';
 @ObjectType('AccountLicensePlan')
 export abstract class IAccountLicensePlan {
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: false,
     description: 'The number of Free Spaces allowed.',
   })
   spaceFree!: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: false,
     description: 'The number of Plus Spaces allowed.',
   })
   spacePlus!: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: false,
     description: 'The number of Premium Spaces allowed.',
   })
   spacePremium!: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: false,
     description: 'The number of Virtual Contributors allowed.',
   })
   virtualContributor!: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: false,
     description: 'The number of Innovation Packs allowed.',
   })
   innovationPacks!: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: false,
     description: 'The number of Starting Pages allowed.',
   })
