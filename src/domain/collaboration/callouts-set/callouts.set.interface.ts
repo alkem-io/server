0df7b459379dced576baa8d39817d98341ca581a
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2025-08-29 09:50:59 +0300

    use Collaboration interface for CalloutSet interface

--- src/domain/collaboration/callouts-set/callouts.set.interface.ts
+++ src/domain/collaboration/callouts-set/callouts.set.interface.ts
@@ -3,11 +3,11 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { ICallout } from '../callout/callout.interface';
 import { ITagsetTemplateSet } from '@domain/common/tagset-template-set/tagset.template.set.interface';
 import { CalloutsSetType } from '@common/enums/callouts.set.type';
-import { Collaboration } from '../collaboration';
+import { ICollaboration } from '../collaboration';
 
 @ObjectType('CalloutsSet')
 export abstract class ICalloutsSet extends IAuthorizable {
-  collaboration?: Collaboration;
+  collaboration?: ICollaboration;
 
   callouts!: ICallout[];
 
