b30036f82b519a6d52d9bba68831d1ec7c852cf4
Neil Smyth <neil@thesmyths.eu>
2025-08-29 17:15:58 +0200

    fixed typo in migration file

--- src/migrations/1756479201057-removeMentionInApp.ts
+++ src/migrations/1756479201057-removeMentionInApp.ts
@@ -2,9 +2,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class RemoveMentionInApp1756479201057 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    /// delete all in app notifications where the type is 'USER_MENTION'
+    /// delete all in app notifications where the type is 'USER_MENTIONED'
     await queryRunner.query(
-      `DELETE FROM \`in_app_notification\` WHERE \`type\` = 'USER_MENTION'`
+      `DELETE FROM \`in_app_notification\` WHERE \`type\` = 'USER_MENTIONED'`
     );
   }
 
