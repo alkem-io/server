da296dd0ac07ca90b1c75b49097836868a3f4c6a
Svetoslav <svetoslav@alkem.io>
2025-08-29 15:57:57 +0300

    null result for non existent data

--- src/platform/in-app-notification-payload/field-resolvers/space/in.app.notification.payload.space.community.contributor.resolver.fields.ts
+++ src/platform/in-app-notification-payload/field-resolvers/space/in.app.notification.payload.space.community.contributor.resolver.fields.ts
@@ -10,7 +10,7 @@ import { InAppNotificationPayloadSpaceCommunityContributor } from '@platform/in-
 @Resolver(() => InAppNotificationPayloadSpaceCommunityContributor)
 export class InAppNotificationPayloadSpaceCommunityContributorResolverFields {
   @ResolveField(() => IContributor, {
-    nullable: false,
+    nullable: true,
     description: 'The Contributor that joined.',
   })
   public contributor(
@@ -22,7 +22,7 @@ export class InAppNotificationPayloadSpaceCommunityContributorResolverFields {
   }
 
   @ResolveField(() => ISpace, {
-    nullable: false,
+    nullable: true,
     description: 'The Space that was joined.',
   })
   public space(
