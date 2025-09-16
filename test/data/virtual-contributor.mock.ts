df62bef8d7083394501b3fd3d668f5f32ee773fe
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2025-09-16 10:47:54 +0300

    fix build

--- test/data/virtual-contributor.mock.ts
+++ test/data/virtual-contributor.mock.ts
@@ -1,5 +1,8 @@
 import { IVirtualContributor } from '@domain/community/virtual-contributor/virtual.contributor.interface';
 import { AgentType } from '@common/enums/agent.type';
+import { VirtualContributorBodyOfKnowledgeType } from '@common/enums/virtual.contributor.body.of.knowledge.type';
+import { VirtualContributorDataAccessMode } from '@common/enums/virtual.contributor.data.access.mode';
+import { VirtualContributorInteractionMode } from '@common/enums/virtual.contributor.interaction.mode';
 
 export const virtualContributorData: {
   virtualContributor: IVirtualContributor;
@@ -8,6 +11,10 @@ export const virtualContributorData: {
     rowId: 1,
     id: '08a43f9f-58e7-4c65-bf38-be283a548b3b',
     nameID: 'bridgeuniandbusiness',
+    bodyOfKnowledgeType: VirtualContributorBodyOfKnowledgeType.ALKEMIO_SPACE,
+    aiPersonaID: 'ai-persona-08a43f9f-58e7-4c65-bf38-be283a548b3b',
+    dataAccessMode: VirtualContributorDataAccessMode.SPACE_PROFILE,
+    interactionModes: [VirtualContributorInteractionMode.DISCUSSION_TAGGING],
     agent: {
       type: AgentType.SPACE,
       // Add other required IAgent fields as needed for type safety
