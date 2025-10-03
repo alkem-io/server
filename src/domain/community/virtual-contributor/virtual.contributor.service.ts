d5a905231abf86023357d781f021ff200bddf404
bobbykolev <bobbykolev@abv.bg>
2025-10-03 11:19:10 +0300

    pr fix

--- src/domain/community/virtual-contributor/virtual.contributor.service.ts
+++ src/domain/community/virtual-contributor/virtual.contributor.service.ts
@@ -437,8 +437,25 @@ export class VirtualContributorService {
       LogContext.VIRTUAL_CONTRIBUTOR
     );
 
+    // no refresh needed for these types
+    if (
+      [
+        VirtualContributorBodyOfKnowledgeType.NONE,
+        VirtualContributorBodyOfKnowledgeType.OTHER,
+      ].includes(virtualContributor.bodyOfKnowledgeType)
+    ) {
+      return Promise.resolve(false);
+    }
+
+    if (!virtualContributor.bodyOfKnowledgeID) {
+      throw new ValidationException(
+        `Virtual Contributor Body of Knowledge ID missing: ${virtualContributor.id}`,
+        LogContext.VIRTUAL_CONTRIBUTOR
+      );
+    }
+
     return this.aiServerAdapter.refreshBodyOfKnowledge(
-      virtualContributor.bodyOfKnowledgeID ?? '',
+      virtualContributor.bodyOfKnowledgeID,
       virtualContributor.bodyOfKnowledgeType,
       virtualContributor.aiPersonaID
     );
