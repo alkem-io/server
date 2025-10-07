c5baa965e1f5ce6550a13731e8657473d3ed20d2
Bobby Kolev <bobbykolev@abv.bg>
2025-10-07 09:17:47 +0300

    clean up community leads sender template (#5464)

--- src/services/api/notification-recipients/notification.recipients.service.ts
+++ src/services/api/notification-recipients/notification.recipients.service.ts
@@ -388,7 +388,6 @@ export class NotificationRecipientsService {
       case NotificationEvent.USER_MESSAGE:
       case NotificationEvent.USER_MESSAGE_SENDER:
       case NotificationEvent.ORGANIZATION_MESSAGE_SENDER:
-      case NotificationEvent.SPACE_COMMUNICATION_MESSAGE_SENDER:
       case NotificationEvent.PLATFORM_FORUM_DISCUSSION_COMMENT: {
         // For mentions, no privilege check is needed - mentions are direct notifications to specific users
         credentialCriteria = this.getUserSelfCriteria(userID);
@@ -484,7 +483,6 @@ export class NotificationRecipientsService {
       case NotificationEvent.USER_MESSAGE:
       case NotificationEvent.USER_MESSAGE_SENDER:
       case NotificationEvent.ORGANIZATION_MESSAGE_SENDER:
-      case NotificationEvent.SPACE_COMMUNICATION_MESSAGE_SENDER:
       case NotificationEvent.PLATFORM_FORUM_DISCUSSION_COMMENT:
       case NotificationEvent.USER_COMMENT_REPLY:
       case NotificationEvent.USER_SPACE_COMMUNITY_JOINED:
