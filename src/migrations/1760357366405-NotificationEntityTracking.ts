ed6059ddb185cb35611ba9a1ea1a4fbf3e097934
Svetoslav <svetoslav@alkem.io>
2025-10-20 14:33:14 +0300

    Fix: Update query to correctly check for non-null callout_contribution.id in notification tracking

--- src/migrations/1760357366405-NotificationEntityTracking.ts
+++ src/migrations/1760357366405-NotificationEntityTracking.ts
@@ -45,7 +45,7 @@ export class NotificationEntityTracking1760357366405 implements MigrationInterfa
       const data: { id: string; contributionParentID: string; }[] = await queryRunner.query(`
           SELECT in_app_notification.id, callout_contribution.id as contributionParentID from in_app_notification
           LEFT JOIN callout_contribution ON callout_contribution.postId = JSON_UNQUOTE(in_app_notification.payload->'$.contributionID')
-          WHERE JSON_UNQUOTE(payload->'$.type') = 'SPACE_COLLABORATION_CALLOUT_POST_COMMENT' AND contributionParentID IS NOT NULL;
+          WHERE JSON_UNQUOTE(payload->'$.type') = 'SPACE_COLLABORATION_CALLOUT_POST_COMMENT' AND callout_contribution.id IS NOT NULL;
       `);
       for (const { id, contributionParentID } of data) {
         // payload.contributionID to be the ACTUAL contributionID instead of the postID
