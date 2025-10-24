ddd09dc63f222f03be9e07ef3482348f050cfd2f
Svetoslav <svetoslav@alkem.io>
2025-10-24 11:06:00 +0300

    refactor: update InAppNotificationPayloadSpaceCommunityApplicationDeclined to extend InAppNotificationPayloadSpaceBase

--- src/platform/in-app-notification-payload/dto/space/notification.in.app.payload.space.community.application.declined.ts
+++ src/platform/in-app-notification-payload/dto/space/notification.in.app.payload.space.community.application.declined.ts
@@ -1,8 +1,10 @@
 import { IInAppNotificationPayload } from '@platform/in-app-notification-payload/in.app.notification.payload.interface';
-import { InAppNotificationPayloadSpace } from './notification.in.app.payload.space.base';
+import {
+  InAppNotificationPayloadSpaceBase,
+} from './notification.in.app.payload.space.base';
 import { ObjectType } from '@nestjs/graphql';
 
 @ObjectType('InAppNotificationPayloadSpaceCommunityApplicationDeclined', {
   implements: () => IInAppNotificationPayload,
 })
-export abstract class InAppNotificationPayloadSpaceCommunityApplicationDeclined extends InAppNotificationPayloadSpace {}
+export abstract class InAppNotificationPayloadSpaceCommunityApplicationDeclined extends InAppNotificationPayloadSpaceBase {}
