ce19268fe700d2b09ee9c93af02d3c5745e813e6
Neil Smyth <neil@thesmyths.eu>
2025-11-01 06:39:16 +0100

    added logic to check if receiving user is open to receiving messages

--- src/domain/communication/conversations-set/conversations.set.resolver.mutations.ts
+++ src/domain/communication/conversations-set/conversations.set.resolver.mutations.ts
@@ -15,6 +15,8 @@ import { ForbiddenException } from '@common/exceptions/forbidden.exception';
 import { LogContext } from '@common/enums/logging.context';
 import { UserLookupService } from '@domain/community/user-lookup/user.lookup.service';
 import { CreateConversationInput } from '../conversation/dto/conversation.dto.create';
+import { MessagingNotEnabledException } from '@common/exceptions/messaging.not.enabled.exception';
+import { CommunicationConversationType } from '@common/enums/communication.conversation.type';
 
 @InstrumentResolver()
 @Resolver()
@@ -57,9 +59,18 @@ export class ConversationsSetResolverMutations {
     this.authorizationService.grantAccessOrFail(
       agentInfo,
       conversationsSet.authorization,
-      AuthorizationPrivilege.CREATE, // TODO: tie this one down more
+      AuthorizationPrivilege.CREATE,
       `create conversation on conversations Set: ${conversationsSet.id}`
     );
+
+    // Also check if the receiving user wants to accept conversations
+    if (conversationData.type === CommunicationConversationType.USER_USER) {
+      await this.checkReceivingUserAccessAndSettings(
+        agentInfo,
+        conversationData.userID
+      );
+    }
+
     const conversation =
       await this.conversationsSetService.createConversationOnConversationsSet(
         conversationData,
@@ -80,5 +91,37 @@ export class ConversationsSetResolverMutations {
     return await this.conversationService.getConversationOrFail(
       conversation.id
     );
+  }
+
+  private async checkReceivingUserAccessAndSettings(
+    agentInfo: AgentInfo,
+    receivingUserID: string
+  ) {
+    const receivingUser = await this.userLookupService.getUserOrFail(
+      receivingUserID,
+      {
+        relations: {
+          settings: true,
+        },
+      }
+    );
+    this.authorizationService.grantAccessOrFail(
+      agentInfo,
+      receivingUser.authorization,
+      AuthorizationPrivilege.READ,
+      `user ${agentInfo.userID} starting conversation with: ${receivingUser.id}`
+    );
+
+    // Check if the user is willing to receive messages
+    if (!receivingUser.settings.communication.allowOtherUsersToSendMessages) {
+      throw new MessagingNotEnabledException(
+        'User is not open to receiving messages',
+        LogContext.USER,
+        {
+          userId: receivingUser.id,
+          senderId: agentInfo.userID,
+        }
+      );
+    }
   }
 }
