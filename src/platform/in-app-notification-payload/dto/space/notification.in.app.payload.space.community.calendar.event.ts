758e7a90a3f2fe92a7273afd01b15659f22783a9
Svetoslav <svetoslav@alkem.io>
2025-11-05 18:05:14 +0200

    refactor: specify notification event payload type for community calendar event

--- src/platform/in-app-notification-payload/dto/space/notification.in.app.payload.space.community.calendar.event.ts
+++ src/platform/in-app-notification-payload/dto/space/notification.in.app.payload.space.community.calendar.event.ts
@@ -3,11 +3,14 @@ import { InAppNotificationPayloadSpaceBase } from './notification.in.app.payload
 import { Field, ObjectType } from '@nestjs/graphql';
 import { CalendarEventType } from '@common/enums/calendar.event.type';
 import { UUID } from '@domain/common/scalars/scalar.uuid';
+import { NotificationEventPayload } from '@common/enums/notification.event.payload';
 
 @ObjectType('InAppNotificationPayloadSpaceCommunityCalendarEvent', {
   implements: () => IInAppNotificationPayload,
 })
 export abstract class InAppNotificationPayloadSpaceCommunityCalendarEvent extends InAppNotificationPayloadSpaceBase {
+  declare type: NotificationEventPayload.SPACE_COMMUNITY_CALENDAR_EVENT;
+
   @Field(() => UUID, {
     description: 'ID of the calendar event.',
   })
