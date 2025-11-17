c493f454d2f94362e0e1efa5a3f6a04077082e5a
Polina Boneva <13227501+polibb@users.noreply.github.com>
2025-11-17 13:49:46 +0200

    Space: Whiteboards new privileges for creators/admins (#5573)

--- src/domain/collaboration/callout-contribution/callout.contribution.service.authorization.ts
+++ src/domain/collaboration/callout-contribution/callout.contribution.service.authorization.ts
@@ -21,6 +21,7 @@ import { IRoleSet } from '@domain/access/role-set/role.set.interface';
 import { RoleSetService } from '@domain/access/role-set/role.set.service';
 import { IPlatformRolesAccess } from '@domain/access/platform-roles-access/platform.roles.access.interface';
 import { PlatformRolesAccessService } from '@domain/access/platform-roles-access/platform.roles.access.service';
+import { ISpaceSettings } from '@domain/space/space.settings/space.settings.interface';
 import { MemoAuthorizationService } from '@domain/common/memo/memo.service.authorization';
 
 @Injectable()
@@ -40,7 +41,8 @@ export class CalloutContributionAuthorizationService {
     contributionID: string,
     parentAuthorization: IAuthorizationPolicy | undefined,
     platformRolesAccess: IPlatformRolesAccess,
-    roleSet?: IRoleSet
+    roleSet?: IRoleSet,
+    spaceSettings?: ISpaceSettings
   ): Promise<IAuthorizationPolicy[]> {
     const contribution =
       await this.contributionService.getCalloutContributionOrFail(
@@ -148,7 +150,8 @@ export class CalloutContributionAuthorizationService {
       const whiteboardAuthorizations =
         await this.whiteboardAuthorizationService.applyAuthorizationPolicy(
           contribution.whiteboard.id,
-          contribution.authorization
+          contribution.authorization,
+          spaceSettings
         );
       updatedAuthorizations.push(...whiteboardAuthorizations);
     }
