946064b1c03fcec78d55bd74cf4e25789bb3c700
Neil Smyth <neil@thesmyths.eu>
2025-12-12 19:01:50 +0100

    removed back one-one relation

--- src/domain/communication/messaging/messaging.entity.ts
+++ src/domain/communication/messaging/messaging.entity.ts
@@ -1,24 +1,13 @@
-import { Entity, OneToMany, OneToOne } from 'typeorm';
+import { Entity, OneToMany } from 'typeorm';
 import { AuthorizableEntity } from '@domain/common/entity/authorizable-entity';
 import { Conversation } from '../conversation/conversation.entity';
 import { IMessaging } from './messaging.interface';
-import { Platform } from '@platform/platform/platform.entity';
 
 @Entity('messaging')
-export class Messaging
-  extends AuthorizableEntity
-  implements IMessaging
-{
-  @OneToMany(
-    () => Conversation,
-    conversation => conversation.messaging,
-    {
-      eager: false,
-      cascade: true,
-    }
-  )
+export class Messaging extends AuthorizableEntity implements IMessaging {
+  @OneToMany(() => Conversation, conversation => conversation.messaging, {
+    eager: false,
+    cascade: true,
+  })
   conversations!: Conversation[];
-
-  @OneToOne(() => Platform, platform => platform.messaging)
-  platform?: Platform;
 }
