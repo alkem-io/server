5d5b53bb3c5fb174c9672628e3286e6e9e5afb0e
Carlos Cano <carlos@alkem.io>
2026-06-24 14:27:11 +0200

    Merge pull request #6176 from alkem-io/feat/006-u2u-messaging
    Feat/006 u2u messaging

--- src/domain/community/user-settings/user.settings.communications.interface.ts
+++ src/domain/community/user-settings/user.settings.communications.interface.ts
@@ -7,4 +7,11 @@ export abstract class IUserSettingsCommunication {
     description: 'Allow Users to send messages to this User.',
   })
   allowOtherUsersToSendMessages!: boolean;
+
+  @Field(() => Boolean, {
+    nullable: false,
+    description:
+      'Allow other Users to be offered an email contact route to this User (using the account email; the address is never exposed). Default false.',
+  })
+  allowOtherUsersToContactViaEmail!: boolean;
 }
